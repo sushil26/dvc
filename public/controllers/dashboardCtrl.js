27b80c457f55ef0f9ff621497a3cb4a4f97feb1e
LogeswariGuru <logeswari.careator@gmail.com>
2018-04-14 11:25:00 +0530

    userName added

--- public/controllers/dashboardCtrl.js
+++ public/controllers/dashboardCtrl.js
@@ -1,6 +1,7 @@
 app.controller('dashboardController', function ($scope, $window, httpFactory) {
     console.log("dashboardController==>");
-
+    var id = localStorage.getItem("id");
+    $scope.userName = localStorage.getItem("userName");
 
    
   

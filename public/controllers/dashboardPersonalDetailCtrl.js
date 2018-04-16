f70b83a8da4ca28e47658638a004c619e9eee3fa
LogeswariGuru <logeswari.careator@gmail.com>
2018-04-16 07:15:47 +0530

    loged in api

--- public/controllers/dashboardPersonalDetailCtrl.js
+++ public/controllers/dashboardPersonalDetailCtrl.js
@@ -1,12 +1,27 @@
 app.controller('dashboardPersonalDetailController', function ($scope, $window, httpFactory) {
     console.log("dashboardController==>");
 
+    var id = localStorage.getItem("id");
 
-   
-  
+    $scope.getDetails = function (id) {
+        console.log("getDetails-->");
+        var api = "https://norecruits.com/vc/getLoginData" + "/" + id;
 
+        httpFactory.get(api).then(function (data) {
+            var checkStatus = httpFactory.dataValidation(data);
+            console.log("data--" + JSON.stringify(data.data));
+            if (checkStatus) {
+                $scope.userData = data.data.data;
+                console.log(" $scope.userData: " + $scope.userData);
+            }
+            else {
+                console.log("sorry");
+            }
+            console.log("<--getDetails");
+        })
+    }
 
-
+    $scope.getDetails(id);
 
 
 })

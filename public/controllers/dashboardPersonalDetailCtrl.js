a7f9fcffa3a6041674ec3751f329b19eeb485c49
LogeswariGuru <logeswari.careator@gmail.com>
2018-04-16 07:50:55 +0530

    profile view value matching

--- public/controllers/dashboardPersonalDetailCtrl.js
+++ public/controllers/dashboardPersonalDetailCtrl.js
@@ -2,66 +2,66 @@ app.controller('dashboardPersonalDetailController', function ($scope, $window, h
     console.log("dashboardController==>");
 
     var id = localStorage.getItem("id");
+    $scope.loginType = localStorage.getItem("loginType");
 
+    $scope.getTeacherDetails = function (id) {
+        console.log("getTeacherData-->");
+        var id = localStorage.getItem("id");
 
-$scope.getTeacherDetails = function (id) {
-    console.log("getTeacherData-->");
-    var id = localStorage.getItem("id");
+        var api = "https://norecruits.com/vc/teacherDetail" + "/" + id;
+        //var api = "http://localhost:5000/vc/teacherDetail" + "/" + id;
+        //var api = "http://localhost:5000/vc/eventGet";
+        console.log("api: " + api);
+        httpFactory.get(api).then(function (data) {
+            var checkStatus = httpFactory.dataValidation(data);
+            console.log("data--" + JSON.stringify(data.data));
+            if (checkStatus) {
+                $scope.userData = data.data.data;
+                console.log("teacherData: " + JSON.stringify($scope.teacherData));
+                //   $scope.css = $scope.teacherData[0].css;
+                //   console.log("$scope.css: " + JSON.stringify($scope.css));
+            }
+            else {
 
-    var api = "https://norecruits.com/vc/teacherDetail" + "/" + id;
-    //var api = "http://localhost:5000/vc/teacherDetail" + "/" + id;
-    //var api = "http://localhost:5000/vc/eventGet";
-    console.log("api: " + api);
-    httpFactory.get(api).then(function (data) {
-        var checkStatus = httpFactory.dataValidation(data);
-        console.log("data--" + JSON.stringify(data.data));
-        if (checkStatus) {
-            $scope.teacherData = data.data.data;
-            console.log("teacherData: " + JSON.stringify($scope.teacherData));
-            //   $scope.css = $scope.teacherData[0].css;
-            //   console.log("$scope.css: " + JSON.stringify($scope.css));
-        }
-        else {
+            }
 
-        }
-        
-    })
-    console.log("<--getTeacherData");
-}
+        })
+        console.log("<--getTeacherData");
+    }
 
-$scope.getStudentDetails = function (id) {
-    console.log("getTeacherData-->");
-    var id = localStorage.getItem("id");
-    var api = "https://norecruits.com/vc/studentDetail" + "/" + id;
-    console.log("api: " + api);
-    $scope.teacherList = [];
-    httpFactory.get(api).then(function (data) {
-      var checkStatus = httpFactory.dataValidation(data);
-      console.log("data--" + JSON.stringify(data.data));
-      if (checkStatus) {
-        $scope.studentData = data.data.data;
-        console.log("studentData: " + JSON.stringify($scope.studentData));
-      }
-      else{
+    $scope.getStudentDetails = function (id) {
+        console.log("getTeacherData-->");
+        var id = localStorage.getItem("id");
+        var api = "https://norecruits.com/vc/studentDetail" + "/" + id;
+        console.log("api: " + api);
+        $scope.teacherList = [];
+        httpFactory.get(api).then(function (data) {
+            var checkStatus = httpFactory.dataValidation(data);
+            console.log("data--" + JSON.stringify(data.data));
+            if (checkStatus) {
+                $scope.userData = data.data.data;
+                console.log("studentData: " + JSON.stringify($scope.studentData));
+            }
+            else {
 
-      }
-    })
-}
+            }
+        })
+    }
 
     if (localStorage.getItem("loginType") == 'teacher') {
         $scope.getTeacherDetails(id);
         console.log("teacher login");
-       
+
     }
     else if (localStorage.getItem("loginType") == 'studParent') {
         $scope.getStudentDetails(id);
         console.log("studParent login");
     }
-    else if(localStorage.getItem("loginType") == 'admin'){
-console.log("admin Login");
+    else if (localStorage.getItem("loginType") == 'admin') {
+        console.log("admin Login");
     }
 
-       
+
 
 
 

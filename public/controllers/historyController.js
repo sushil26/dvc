20ccc7206eb46e1f86387df0e4288a5d7b214c97
LogeswariGuru <logeswari.careator@gmail.com>
2018-04-17 11:41:29 +0530

    sc

--- public/controllers/historyController.js
+++ public/controllers/historyController.js
@@ -1,56 +1,56 @@
 app.controller('historyController', function ($scope, $window, httpFactory) {
     console.log("historyController==>");
     vm.events = [];
-    // $scope.eventGet = function () {
-    //     console.log("eventGet-->");
-    //     var id = localStorage.getItem("id");
-    //     var api = "https://norecruits.com/vc/eventGet" + "/" + id;
-    //     //var api = "http://localhost:5000/vc/eventGet"+ "/" + id;;
-    //     $scope.calendarOwner = "Your";
+    $scope.eventGet = function () {
+        console.log("eventGet-->");
+        var id = localStorage.getItem("id");
+        var api = "https://norecruits.com/vc/eventGet" + "/" + id;
+        //var api = "http://localhost:5000/vc/eventGet"+ "/" + id;;
+        $scope.calendarOwner = "Your";
 
-    //     httpFactory.get(api).then(function (data) {
-    //         var checkStatus = httpFactory.dataValidation(data);
-    //         console.log("data--" + JSON.stringify(data.data));
-    //         if (checkStatus) {
-    //             $scope.eventData = data.data.data;
+        httpFactory.get(api).then(function (data) {
+            var checkStatus = httpFactory.dataValidation(data);
+            console.log("data--" + JSON.stringify(data.data));
+            if (checkStatus) {
+                $scope.eventData = data.data.data;
                 
-    //             // ownerEvents = [];
-    //             for (var x = 0; x < $scope.eventData.length; x++) {
-    //                 console.log("$scope.eventData[" + x + "]: " + JSON.stringify($scope.eventData[x]));
-    //                 var obj = {
-    //                     'id': $scope.eventData[x]._id,
-    //                     'title': $scope.eventData[x].title,
-    //                     'color': $scope.eventData[x].primColor,
-    //                     'startsAt': new Date($scope.eventData[x].start),
-    //                     'endsAt': new Date($scope.eventData[x].end),
-    //                     'draggable': true,
-    //                     'resizable': true,
-    //                     'actions': actions,
-    //                     'url': $scope.eventData[x].url,
-    //                     "senderName": $scope.eventData[x].senderName,
-    //                     "senderId": $scope.eventData[x].senderId,
-    //                     "senderMN": $scope.eventData[x].senderMN,
-    //                     "senderLoginType": $scope.eventData[x].senderLoginType,
-    //                     "title": $scope.eventData[x].title,
-    //                     "reason": $scope.eventData[x].reason,
-    //                     "receiverEmail": $scope.eventData[x].receiverEmail,
-    //                     "receiverName": $scope.eventData[x].receiverName,
-    //                     "receiverId": $scope.eventData[x].receiverId,
-    //                     "receiverMN": $scope.eventData[x].receiverMN,
-    //                     "remoteCalendarId": $scope.eventData[x].remoteCalendarId
-    //                 }
-    //                 console.log(" obj" + JSON.stringify(obj))
-    //                 // ownerEvents.push(obj);
-    //                 vm.events.push(obj);
+                // ownerEvents = [];
+                for (var x = 0; x < $scope.eventData.length; x++) {
+                    console.log("$scope.eventData[" + x + "]: " + JSON.stringify($scope.eventData[x]));
+                    var obj = {
+                        'id': $scope.eventData[x]._id,
+                        'title': $scope.eventData[x].title,
+                        'color': $scope.eventData[x].primColor,
+                        'startsAt': new Date($scope.eventData[x].start),
+                        'endsAt': new Date($scope.eventData[x].end),
+                        'draggable': true,
+                        'resizable': true,
+                        'actions': actions,
+                        'url': $scope.eventData[x].url,
+                        "senderName": $scope.eventData[x].senderName,
+                        "senderId": $scope.eventData[x].senderId,
+                        "senderMN": $scope.eventData[x].senderMN,
+                        "senderLoginType": $scope.eventData[x].senderLoginType,
+                        "title": $scope.eventData[x].title,
+                        "reason": $scope.eventData[x].reason,
+                        "receiverEmail": $scope.eventData[x].receiverEmail,
+                        "receiverName": $scope.eventData[x].receiverName,
+                        "receiverId": $scope.eventData[x].receiverId,
+                        "receiverMN": $scope.eventData[x].receiverMN,
+                        "remoteCalendarId": $scope.eventData[x].remoteCalendarId
+                    }
+                    console.log(" obj" + JSON.stringify(obj))
+                    // ownerEvents.push(obj);
+                    vm.events.push(obj);
 
-    //             }
-    //         }
-    //         else {
-    //             //alert("Event get Failed");
-    //         }
-    //     })
-    // }
-    // $scope.eventGet();
+                }
+            }
+            else {
+                //alert("Event get Failed");
+            }
+        })
+    }
+    $scope.eventGet();
 
 
 })

2ad529405c6d8ac5c7157c55be3af8795a9d5f0a
LogeswariGuru <logeswari.careator@gmail.com>
2018-04-19 14:26:51 +0530

    index corrected by 1 in upcomngevent

--- config/controllers/event.js
+++ config/controllers/event.js
@@ -125,7 +125,7 @@ module.exports.eventGet = function (req, res) {
     console.log("getEvent-->");
     var responseData;
      console.log("req.params.id: "+req.params.id);
-     console.log("DateTime: "+new Date());
+     console.log("DateTime: "+new Date().toISOString());
        if (general.emptyCheck(req.params.id)) {
            event.find({ $or: [ { "userId": req.params.id }, { "remoteCalendarId": req.params.id } ] }).sort({"startAt":1}).toArray(function (err, listOfevents) {
            // console.log("listOfevents: "+JSON.stringify(listOfevents))

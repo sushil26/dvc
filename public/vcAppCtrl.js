5fae56cd5b86ce3e86c57af1be25e2e3739861f2
LogeswariGuru <logeswari.careator@gmail.com>
2018-04-20 11:47:00 +0530

    made index.html page as angularjs

--- public/vcAppCtrl.js
+++ public/vcAppCtrl.js
@@ -1,14 +1,23 @@
-app.controller("vcAppCtrl", function(
-  $scope,$rootScope,
-  httpFactory,
-  $window,
-  $timeout,
-  $state,
-  $http
-) {
+app.controller("vcAppCtrl", function ($scope, $rootScope, httpFactory, $window, $timeout, $state, $http, $uibModal) {
   console.log("controller==>");
 
-  $scope.vcLogout = function() {
+  $scope.loginClick = function () {
+    console.log("loginClick-->");
+    var dayEventmodal = $uibModal.open({
+      scope: $scope,
+      templateUrl: '/html/templates/loginPopup.html',
+      windowClass: 'show',
+      backdropClass: 'show',
+      controller: function ($scope, $uibModalInstance) {
+       
+        console.log("<--loginClick");
+
+      }
+    })
+
+  }
+
+  $scope.vcLogout = function () {
     console.log("vcLogout");
     window.location = "https://norecruits.com/client";
     localStorage.removeItem("userData");
@@ -25,12 +34,12 @@ app.controller("vcAppCtrl", function(
   };
 
   $rootScope.TimeTable_timing = [
-    {"startsAt":"09:00","endsAt":"09:45", "meridian":'AM'},
-  {"startsAt":"9:45","endsAt":"10:30", "meridian":'AM'},
-  {"startsAt":"10:30", "endsAt":"11:15", "meridian":'AM'},
-  {"startsAt":"11:15","endsAt":"12:00", "meridian":'AM'},
-  {"startsAt":"01:00", "endsAt":"01:45", "meridian":'PM'},
-  {"startsAt":"01:45", "endsAt":"02:30", "meridian":'PM'},
-  {"startsAt":"02:30","endsAt":"03:15", "meridian":'PM'},
-  {"startsAt":"03:15", "endsAt":"04:00", "meridian":'PM'}]
+    { "startsAt": "09:00", "endsAt": "09:45", "meridian": 'AM' },
+    { "startsAt": "9:45", "endsAt": "10:30", "meridian": 'AM' },
+    { "startsAt": "10:30", "endsAt": "11:15", "meridian": 'AM' },
+    { "startsAt": "11:15", "endsAt": "12:00", "meridian": 'AM' },
+    { "startsAt": "01:00", "endsAt": "01:45", "meridian": 'PM' },
+    { "startsAt": "01:45", "endsAt": "02:30", "meridian": 'PM' },
+    { "startsAt": "02:30", "endsAt": "03:15", "meridian": 'PM' },
+    { "startsAt": "03:15", "endsAt": "04:00", "meridian": 'PM' }]
 });

7a97d2aace64d61fd671b17f3c307bd9593e0470
LogeswariGuru <logeswari.careator@gmail.com>
2018-04-25 09:57:00 +0530

    file custome directory added

--- public/common/fileModel.js
+++ public/common/fileModel.js
@@ -9,10 +9,10 @@ app.directive('fileModel', ['$parse', function ($parse) {
                 scope.$apply(function () {
                     console.log("file from filemodel cutom drctve: " + element.files);
                     console.log("element[0].files[0] from filemodel cutom drctve: " + element.file);
-                    console.log("element[0].files[0] from filemodel cutom drctve: " + JSON.stringify(element));
-                    console.log("element[0].files[0] from filemodel cutom drctve: " + JSON.stringify(element[0].files));
-                    console.log("element[0].files[0] from filemodel cutom drctve: " + JSON.stringify(element[0].files[0]));
-                    modelSetter(scope, element[0].files[0]);
+                    // console.log("element[0].files[0] from filemodel cutom drctve: " + JSON.stringify(element));
+                    // console.log("element[0].files[0] from filemodel cutom drctve: " + JSON.stringify(element[0].files));
+                    // console.log("element[0].files[0] from filemodel cutom drctve: " + JSON.stringify(element[0].files[0]));
+                    // modelSetter(scope, element[0].files[0]);
                 });
             });
         }

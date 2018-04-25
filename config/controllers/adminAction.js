928fb9923bb9938e71b5454aef508bf4e8449f28
LogeswariGuru <logeswari.careator@gmail.com>
2018-04-25 12:02:28 +0530

    sc

--- config/controllers/adminAction.js
+++ config/controllers/adminAction.js
@@ -9,33 +9,33 @@ module.exports.getAllClass = function (req, res) {
     console.log("getAllClass-->");
     var responseData;
     stud.find().toArray(function (err, studentList) {
-      if (err) {
-        responseData = {
-          status: false,
-          message: "Failed to get Data",
-          data: data
-        };
-        res.status(400).send(responseData);
-      } else {
-var allClass=[];
-        // console.log("studentList: "+JSON.stringify(studentList));
-        console.log("studentList.length: "+studentList.length);
-        for(var len=0;len<studentList.length;len++){
-            var cls = studentList[len].cs[0].class;
-            console.log("cls: "+cls);
-            allClass.push(cls);
+        if (err) {
+            responseData = {
+                status: false,
+                message: "Failed to get Data",
+                data: data
+            };
+            res.status(400).send(responseData);
+        } else {
+            var allClass = [];
+            // console.log("studentList: "+JSON.stringify(studentList));
+            console.log("studentList.length: " + studentList.length);
+            for (var len = 0; len < studentList.length; len++) {
+                var cls = studentList[len].cs[0].class;
+                var sec = studentList[len].cs[0].section;
+                console.log("cls: " + cls);
+                allClass.push({ "class": cls, "sec": section });
+            }
 
+            responseData = {
+                status: true,
+                message: "Successfull retrived data",
+                data: allClass
+            };
+
+            res.status(200).send(responseData);
         }
-
-        responseData = {
-          status: true,
-          message: "Successfull retrived data",
-          data: allClass
-        };
-  
-        res.status(200).send(responseData);
-      }
     });
-  
+
     console.log("<--getAllClass");
-  };
+};

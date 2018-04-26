cce76a38524d8e638dd015d03a956be6313cfd86
LogeswariGuru <logeswari.careator@gmail.com>
2018-04-26 16:48:10 +0530

    addUploadR added

--- config/controllers/adminAction.js
+++ config/controllers/adminAction.js
@@ -60,6 +60,7 @@ module.exports.uploadAttendance = function (req, res) {
         ignoreEmpty: true
     }).on("data", function (data) {
         console.log("data: " + JSON.stringify(data));
+        console.log("today data: " + d);
         //parser.pause();
         // var studId = {
         //     "studId": data.studId

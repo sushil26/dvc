684975edbee5b794b0e2c09b8036a28b154bd006
LogeswariGuru <logeswari.careator@gmail.com>
2018-04-26 18:04:26 +0530

    api

--- config/controllers/adminAction.js
+++ config/controllers/adminAction.js
@@ -78,7 +78,7 @@ module.exports.uploadAttendance = function (req, res) {
         }]
         // console.log("attendance: " + JSON.stringify(attendance));
 
-        stud.find(studId, { "attendance": { $exists: false } }), function (err, data) {
+        stud.find(studId, { "attendance": { $exists: true } }), function (err, data) {
 console.log("query started: "+JSON.stringify(data));
             if (err) {
                 console.log("err");

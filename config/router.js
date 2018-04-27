55a8aa73dc9b2bd0d760674589cfacdca908d6fa
LogeswariGuru <logeswari.careator@gmail.com>
2018-04-27 10:41:06 +0530

    api

--- config/router.js
+++ config/router.js
@@ -26,7 +26,7 @@ module.exports = function (app) {
      app.get('/vc/getAllClass', adminAction.getAllClass);
     // app.post('/vc/uploadStudentMaster', adminAction.uploadStudentMaster);
      app.post('/vc/uploadMark', adminAction.uploadMark);
-     app.post('/vc/uploadAttendance', adminAction.uploadAttendance);
+    //  app.post('/vc/uploadAttendance', adminAction.uploadAttendance);
      app.post('/vc/uploadPayment', adminAction.uploadPayment);
     
     // app.post('/vc/atte', adminAction.getAllClass);

756905dcc37d976ff9d7ff29f8c7d06c3cfb03bc
LogeswariGuru <logeswari.careator@gmail.com>
2018-05-01 14:58:37 +0530

    getallclass api correctin

--- config/controllers/school.js
+++ config/controllers/school.js
@@ -5,13 +5,15 @@ var school = db.collection("school"); /* ### school collection  ### */
 var general = require("../general.js");
 var ObjectId = require("mongodb").ObjectID;
 var bodyParser = require('body-parser');
+
 module.exports.getAllClass = function (req, res) {
     console.log("getAllClass-->");
     var responseData;
-    school.find({"schoolName":req.params.schoolName}).toArray(function (err, data) {
-        console.log("data: "+JSON.stringify(data));
-
-        allClass=data.cs;
+    var allClass = [];
+    school.find({ "schoolName": req.params.schoolName }).toArray(function (err, data) {
+        console.log("data: " + JSON.stringify(data));
+        allClass = data.cs;
+        console.log("allClass: " + JSON.stringify(allClass));
         if (err) {
             responseData = {
                 status: false,
@@ -20,35 +22,35 @@ module.exports.getAllClass = function (req, res) {
             };
             res.status(400).send(responseData);
         } else {
-            if(data[0].cs.length>0){
-                
-           
-            // console.log("studentList: "+JSON.stringify(studentList));
-            console.log("allClass.length: " + allClass.length);
-            // for (var len = 0; len < csList.length; len++) {
-            //     var cls = studentList[len].cs[0].class;
-            //     var sec = studentList[len].cs[0].section;
-            //     console.log("cls: " + cls);
-            //     allClass.push({ "class": cls, "section": sec });
-            // }
+            if (data[0].cs.length > 0) {
 
-            responseData = {
-                status: true,
-                message: "Successfull retrived data",
-                data: allClass
-            };
 
-            res.status(200).send(responseData);
-        }
-        else{
-            responseData = {
-                status: true,
-                message: "There is no class",
-                data: allClass                
-            };
+                // console.log("studentList: "+JSON.stringify(studentList));
+                console.log("allClass.length: " + allClass.length);
+                // for (var len = 0; len < csList.length; len++) {
+                //     var cls = studentList[len].cs[0].class;
+                //     var sec = studentList[len].cs[0].section;
+                //     console.log("cls: " + cls);
+                //     allClass.push({ "class": cls, "section": sec });
+                // }
 
-            res.status(200).send(responseData);
-        }
+                responseData = {
+                    status: true,
+                    message: "Successfull retrived data",
+                    data: allClass
+                };
+
+                res.status(200).send(responseData);
+            }
+            else {
+                responseData = {
+                    status: true,
+                    message: "There is no class",
+                    data: allClass
+                };
+
+                res.status(200).send(responseData);
+            }
         }
     });
 

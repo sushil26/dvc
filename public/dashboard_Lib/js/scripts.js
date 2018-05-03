f1b4f694c4508008946b29d47f95fb69ccd1d893
LogeswariGuru <logeswari.careator@gmail.com>
2018-05-03 15:04:56 +0530

    login api check

--- public/dashboard_Lib/js/scripts.js
+++ public/dashboard_Lib/js/scripts.js
@@ -30,27 +30,28 @@ function initializeJS() {
 // }
 
     //sidebar dropdown menu
-    // jQuery('#sidebar .sub-menu > a').click(function () {
-    //     var last = jQuery('.sub-menu.open', jQuery('#sidebar'));   
+    jQuery('#sidebar .sub-menu > a').click(function () {
+        var last = jQuery('.sub-menu.open', jQuery('#sidebar'));   
 
-    //     jQuery('.menu-arrow').removeClass('arrow_carrot-right');
-    //     jQuery('.sub', last).slideUp(200);
-    //     var sub = jQuery(this).next();
-    //     if (sub.is(":visible")) {
-    //         jQuery('.menu-arrow').addClass('arrow_carrot-right');            
-    //         sub.slideUp(200);
-    //     } else {
+        jQuery('.menu-arrow').removeClass('arrow_carrot-right');
+        jQuery('.sub', last).slideUp(200);
+        var sub = jQuery(this).next();
+        console.log("*sub: "+sub.is(":visible"));
+        if (sub.is(":visible")) {
+            jQuery('.menu-arrow').addClass('arrow_carrot-right');            
+            sub.slideUp(200);
+        } else {
             
-    //         jQuery('.menu-arrow').addClass('arrow_carrot-down');            
-    //         sub.slideDown(200);
-    //     }
-    //     var o = (jQuery(this).offset());
-    //     diff = 200 - o.top;
-    //     if(diff>0)
-    //         jQuery("#sidebar").scrollTo("-="+Math.abs(diff),500);
-    //     else
-    //         jQuery("#sidebar").scrollTo("+="+Math.abs(diff),500);
-    // });
+            jQuery('.menu-arrow').addClass('arrow_carrot-down');            
+            sub.slideDown(200);
+        }
+        var o = (jQuery(this).offset());
+        diff = 200 - o.top;
+        if(diff>0)
+            jQuery("#sidebar").scrollTo("-="+Math.abs(diff),500);
+        else
+            jQuery("#sidebar").scrollTo("+="+Math.abs(diff),500);
+    });
 
     // sidebar menu toggle
     jQuery(function() {

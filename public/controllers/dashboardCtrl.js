53254e285cf01ea9439274ed763ad0265ece4c21
LogeswariGuru <logeswari.careator@gmail.com>
2018-05-04 11:29:53 +0530

    sidebar menu

--- public/controllers/dashboardCtrl.js
+++ public/controllers/dashboardCtrl.js
@@ -13,12 +13,12 @@ app.controller('dashboardController', function ($scope, $window, httpFactory, se
 
     $scope.iconMenuClick = function () {
         console.log("iconMenuClick--> ");
-        var elementVisibility = document.getElementById('sidebar-menu').display;
+        var elementVisibility = document.getElementsByClassName('sidebar-menu').display;
         if(elementVisibility=='none'){
-            document.getElementById('sidebar-menu').display='block';
+            document.getElementsByClassName('sidebar-menu').display='block';
         }
         else{
-            document.getElementById('sidebar-menu').display='none';
+            document.getElementsByClassName('sidebar-menu').display='none';
         }
       
         // if ($scope.sideBar == true) {

2d1f8424c884dbde72cba21c734527e0908122dd
LogeswariGuru <logeswari.careator@gmail.com>
2018-05-06 17:49:09 +0530

    timetable added for monday in html

--- public/controllers/dashboardScheduleCtrl.js
+++ public/controllers/dashboardScheduleCtrl.js
@@ -111,9 +111,7 @@ app.controller('dashboardScheduleCtrl', function ($scope, $state, $rootScope, $c
         console.log("mon: " + JSON.stringify($scope.teacherPersonalData[0].timeTable[0]));
         console.log("mon: " + JSON.stringify($scope.teacherPersonalData[0].timeTable[0].css));
         console.log("mon: " + JSON.stringify($scope.teacherPersonalData[0].timeTable[0].css.Mon));
-        //   $scope.css = $scope.teacherData[0].css;
-        //   console.log("$scope.css: " + JSON.stringify($scope.css));
-      }
+             }
       else {
       }
     })

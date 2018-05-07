71b84a28ca9c951b3fc26c828ffcd50fd974951f
LogeswariGuru <logeswari.careator@gmail.com>
2018-05-07 11:05:56 +0530

    attendance view for student

--- public/controllers/attendanceViewCtl.js
+++ public/controllers/attendanceViewCtl.js
@@ -3,7 +3,7 @@ app.controller('attendanceViewCtl', function ($scope, $window, httpFactory, $com
 
     $scope.userData = sessionAuthFactory.getAccess();
     var schoolName = $scope.userData.schoolName;
-    console.log("$scope.userData: "+    JSON.stringify($scope.userData));
+    console.log("$scope.userData: " + JSON.stringify($scope.userData));
 
     $scope.getTeacherData = function () {
         console.log("getTeacherData-->");
@@ -25,13 +25,68 @@ app.controller('attendanceViewCtl', function ($scope, $window, httpFactory, $com
         })
         console.log("<--getTeacherData");
     }
+
+    $scope.getAttendance = function (id) {
+        console.log("getAttendance-->");
+        var api = "https://norecruits.com/vc/getStudentAttendance" + "/" + id;
+        console.log("api: " + api);
+        httpFactory.get(api).then(function (data) {
+            var checkStatus = httpFactory.dataValidation(data);
+            console.log("data--" + JSON.stringify(data.data));
+            if (checkStatus) {
+                var studData = data.data.data;
+                $scope.attendance = studData[0].attendance;
+                console.log("studData: " + JSON.stringify(studData));
+                console.log("$scope.attendance: " + JSON.stringify($scope.attendance));
+                console.log("$scope.attendance.length: " + $scope.attendance.length);
+                for (var x = 0; x < $scope.attendance.length; x++) {
+                    console.log("$scope.attendance[x]: " + JSON.stringify($scope.attendance[x]));
+                    var year = "2018";
+                    var mon = $scope.attendance[x].month;
+                    console.log("$scope.attendance[x].dateAttendance.length: " + $scope.attendance[x].dateAttendance.length);
+                    for (var y = 0; y < $scope.attendance[x].dateAttendance.length; y++) {
+                        console.log("$scope.attendance[x].dateAttendance[y]: " + JSON.stringify($scope.attendance[x].dateAttendance[y]));
+                        var day = $scope.attendance[x].dateAttendance[y].date;
+                        console.log("day: " + day + "month: " + mon + "year: " + year);
+                        var resultDate = new Date(year + " " + mon + " " + day);
+                        console.log("resultDate: " + resultDate);
+                        console.log("moment().subtract(1, 'day').toDate(): " + moment().subtract(1, 'day').toDate());
+
+                        var obj = {
+                            'title': $scope.attendance[x].dateAttendance[y].status,
+                            'startsAt': resultDate,
+                            'endsAt': resultDate,
+                            'draggable': true,
+                            'resizable': true,
+                            'incrementsBadgeTotal': false
+                        }
+                        if ($scope.attendance[x].dateAttendance[y].status == "P") {
+                            obj.color = calendarConfig.colorTypes.info;
+                        }
+                        else {
+                            obj.color = calendarConfig.colorTypes.important;
+                        }
+                        console.log("obj: " + JSON.stringify(obj));
+                        $scope.events.push(obj);
+
+                    }
+                }
+                console.log("$scope.events: " + JSON.stringify($scope.events));
+
+            }
+            else {
+                console.log("sorry");
+            }
+        })
+        console.log("<--getStudentAttendance");
+    }
     if ($scope.userData.loginType == 'teacher') {
         $scope.userLoginType = 'teacher';
         $scope.getTeacherData();
     }
     if ($scope.userData.loginType == 'studParent') {
-        $scope.userLoginType = 'teacher';
-        $scope.getStudentAttendance();
+        $scope.userLoginType = 'studParent';
+        $scope.getAttendance($scope.userData.id);
     }
     $scope.getStudListForCS = function (css) {
 
@@ -64,79 +119,27 @@ app.controller('attendanceViewCtl', function ($scope, $window, httpFactory, $com
 
     }
 
-  var vm = this;
-    
+    var vm = this;
+
     $scope.calendarView = 'month';
     $scope.viewDate = moment().startOf('day').toDate();
     var originalFormat = calendarConfig.dateFormats.hour;
     calendarConfig.dateFormats.hour = 'HH:mm';
-   
+
     var actions = [{
         // label: '<i class=\'glyphicon glyphicon-pencil\'></i>',
         label: 'Re-Schedule',
         onClick: function (args) {
-          alert("Edit Event Comming Soon");
+            alert("Edit Event Comming Soon");
         }
-        }]
-        $scope.events = [];
-        $scope.cellIsOpen = true;
+    }]
+    $scope.events = [];
+    $scope.cellIsOpen = true;
     $scope.getStudentAttendance = function (cs) {
         console.log("getStudentAttendance-->");
         $scope.events = [];
         console.log("cs: " + JSON.stringify(cs));
         var id = cs.id;
-        var api = "https://norecruits.com/vc/getStudentAttendance" + "/" + id;
-        console.log("api: " + api);
-        httpFactory.get(api).then(function (data) {
-            var checkStatus = httpFactory.dataValidation(data);
-            console.log("data--" + JSON.stringify(data.data));
-            if (checkStatus) {
-                var studData = data.data.data;
-                $scope.attendance = studData[0].attendance;
-                console.log("studData: " + JSON.stringify(studData));
-                console.log("$scope.attendance: " + JSON.stringify($scope.attendance));
-                console.log("$scope.attendance.length: " + $scope.attendance.length);
-                for (var x = 0; x < $scope.attendance.length; x++) {
-                    console.log("$scope.attendance[x]: " + JSON.stringify($scope.attendance[x]));
-                    var year = "2018";
-                    var mon = $scope.attendance[x].month;
-                    console.log("$scope.attendance[x].dateAttendance.length: " + $scope.attendance[x].dateAttendance.length);
-                    for (var y = 0; y < $scope.attendance[x].dateAttendance.length; y++) {
-                        console.log("$scope.attendance[x].dateAttendance[y]: " + JSON.stringify($scope.attendance[x].dateAttendance[y]));
-                        var day = $scope.attendance[x].dateAttendance[y].date;
-                        console.log("day: " + day + "month: " + mon + "year: " + year);
-                        var resultDate = new Date(year+" "+mon+" "+day);
-                        console.log("resultDate: " + resultDate);
-                        console.log("moment().subtract(1, 'day').toDate(): " + moment().subtract(1, 'day').toDate());
-                       
-                        var obj = {
-                            'title': $scope.attendance[x].dateAttendance[y].status,
-                            'startsAt': resultDate,
-                            'endsAt': resultDate,
-                            'draggable': true,
-                            'resizable': true,
-                            'incrementsBadgeTotal': false
-                        }
-                        if($scope.attendance[x].dateAttendance[y].status=="P")
-                        {
-                            obj.color = calendarConfig.colorTypes.info; 
-                        }
-                        else{
-                            obj.color = calendarConfig.colorTypes.important; 
-                        }
-                        console.log("obj: "+JSON.stringify(obj));
-                        $scope.events.push(obj);
-
-                    }
-                }
-                console.log("$scope.events: " + JSON.stringify($scope.events));
-
-            }
-            else {
-                console.log("sorry");
-            }
-        })
-        console.log("<--getStudentAttendance");
+        $scope.getAttendance(id);
     }
-
 })

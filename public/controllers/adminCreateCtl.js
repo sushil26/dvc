915847c079f1a2513dfcfcf129419e79bb567c83
LogeswariGuru <logeswari.careator@gmail.com>
2018-05-08 14:26:27 +0530

    admin page creating models mapped

--- public/controllers/adminCreateCtl.js
+++ public/controllers/adminCreateCtl.js
@@ -1,6 +1,6 @@
 app.controller('adminCreateCtl', function ($scope, $window, httpFactory, sessionAuthFactory) {
     console.log("adminCreateCtl==>");
-    
+
     $scope.adminCreate = function () {
         console.log("adminCreate-->");
         var objJson = {
@@ -8,17 +8,18 @@ app.controller('adminCreateCtl', function ($scope, $window, httpFactory, session
             "schoolRegNumber": $scope.schoolRegNumber,
             "firstName": $scope.firstName,
             "lastName": $scope.lastName,
-            "dob": $scope.dob,
+            "dob": $filter('date')($scope.dob , "d MMM  y"),
             "email": $scope.email,
             "mobNumber": $scope.mobNumber,
             "address": $scope.address,
-            "city": $scope.city,
             "streetName": $scope.streetName,
+            "city": $scope.city,
+            "state": $scope.state,
             "pinCode": $scope.pinCode,
             "country": $scope.country,
             "pswd": $scope.pswd
         }
-        console.log("objJson: "+JSON.stringify(objJson));
+        console.log("objJson: " + JSON.stringify(objJson));
         // var api = "https://norecruits.com/vc/adminCreate";
         // httpFactory.post(api, objJson).then(function (data) {
         //     var checkStatus = httpFactory.dataValidation(data);

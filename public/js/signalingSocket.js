ec741500eabbeeaf1b9dac1fa3eecf4e477f09a9
LogeswariGuru <logeswari.careator@gmail.com>
2018-05-08 07:05:34 +0530

    added screenRecordStop

--- public/js/signalingSocket.js
+++ public/js/signalingSocket.js
@@ -1042,7 +1042,7 @@ function setup_local_media(callback, errorback) {
      var mediaConstraints = { video: true, audio: true };
     navigator.mediaDevices.getUserMedia(mediaConstraints).then(successCallback).catch(errorCallback);
      btnStopRecording.onclick = function () {
-        recordRTC.stopRecording(function (audioVideoWebMURL) {
+        recordRTC.screenRecordStop(function (audioVideoWebMURL) {
             video.src = audioVideoWebMURL;
      
             var recordedBlob = recordRTC.getBlob();

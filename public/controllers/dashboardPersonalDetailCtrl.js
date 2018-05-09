a5cce17c0a3721fc6a50b28af85a4c31f5438a64
LogeswariGuru <logeswari.careator@gmail.com>
2018-05-09 16:54:36 +0530

    notification directive added sc on info

--- public/controllers/dashboardPersonalDetailCtrl.js
+++ public/controllers/dashboardPersonalDetailCtrl.js
@@ -6,7 +6,7 @@ app.controller('dashboardPersonalDetailController', function ($scope, $window, h
     console.log(" $scope.userData: " + JSON.stringify($scope.userData));
     $scope.loginType = $scope.userData.loginType;
     $scope.userName = $scope.userData.userName;
-    $scope.notificationType = "error";
+    $scope.notificationType = "info";
     $scope.notification = "Yeah! we done it";
 
     var id = $scope.userData.id;

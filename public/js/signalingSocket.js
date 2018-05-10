f8fe5414908eabdef14242bf7c9ae89b34cee4c2
LogeswariGuru <logeswari.careator@gmail.com>
2018-05-10 11:34:06 +0530

    alert added

--- public/js/signalingSocket.js
+++ public/js/signalingSocket.js
@@ -994,9 +994,9 @@ function setup_local_media(callback, errorback) {
     //document.querySelector('#stop-recording').disabled = false;
     // document.querySelector('#pause-recording').disabled = false;
     }, false);
-    // video.play();
-    // container.appendChild(video);
-    // container.appendChild(document.createElement('hr'));
+    video.play();
+    container.appendChild(video);
+    container.appendChild(document.createElement('hr'));
     console.log("<--attachMediaStream");
   };
   navigator.getUserMedia({ audio: USE_AUDIO, video: USE_VIDEO },

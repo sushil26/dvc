0a66aa15951ee9023d7ee4fb834aaa8a39cf472c
LogeswariGuru <logeswari.careator@gmail.com>
2018-05-11 16:57:24 +0530

    reports update console added

--- public/controllers/reportsUpdateCtl.js
+++ public/controllers/reportsUpdateCtl.js
@@ -162,7 +162,7 @@ app.controller('reportsUpdateCtl', function ($scope, $window, $state, httpFactor
     console.log("getStudListForCS-->");
     // console.log("$scope.cssSelect: "+JSON.stringify($scope.cssSelect));
     console.log("class" + clas+" section: "+section);
-    console.log("JSON.css" + JSON.stringify(css));
+    // console.log("JSON.css" + JSON.stringify(css));
     var clas = css.class;
     var section = css.section;
     $scope.studList = [];
@@ -270,44 +270,44 @@ app.controller('reportsUpdateCtl', function ($scope, $window, $state, httpFactor
   // }
 
   // $scope.getAllClass();
-  $scope.getStudListForCS = function (css) {
+  // $scope.getStudListForCS = function (css) {
 
-    console.log("getStudListForCS-->");
-    // console.log("$scope.cssSelect: "+JSON.stringify($scope.cssSelect));
-    console.log("css" + css);
-    console.log("JSON.css" + JSON.stringify(css));
-    var clas = css.class;
-    var section = css.section;
-    $scope.studList = [];
-    // var cssRef = [{"clas":css.class, "section": css.section}];
-    // console.log("cssRef: "+JSON.stringify(cssRef));
+  //   console.log("getStudListForCS-->");
+  //   // console.log("$scope.cssSelect: "+JSON.stringify($scope.cssSelect));
+  //   console.log("css" + css);
+  //   console.log("JSON.css" + JSON.stringify(css));
+  //   var clas = css.class;
+  //   var section = css.section;
+  //   $scope.studList = [];
+  //   // var cssRef = [{"clas":css.class, "section": css.section}];
+  //   // console.log("cssRef: "+JSON.stringify(cssRef));
 
-    var api = "https://norecruits.com/vc/getStudListForCS" + "/" + clas + "/" + section;
-    //var api = "http://localhost:5000/vc/getStudListForCS" + "/" + clas + "/" + section;
-    //var api = "https://norecruits.com/vc/getStudListForCS";
+  //   var api = "https://norecruits.com/vc/getStudListForCS" + "/" + clas + "/" + section;
+  //   //var api = "http://localhost:5000/vc/getStudListForCS" + "/" + clas + "/" + section;
+  //   //var api = "https://norecruits.com/vc/getStudListForCS";
 
-    console.log("api: " + api);
-    httpFactory.get(api).then(function (data) {
-      var checkStatus = httpFactory.dataValidation(data);
-      //console.log("data--" + JSON.stringify(data.data));
-      if (checkStatus) {
-        $scope.studentList = data.data.data;
-        console.log("studentList: " + JSON.stringify($scope.studentList));
-        for (var x = 0; x < $scope.studentList.length; x++) {
-          $scope.studList.push({ "id": $scope.studentList[x]._id, "name": $scope.studentList[x].studName, "studId": $scope.studentList[x].studId });
+  //   console.log("api: " + api);
+  //   httpFactory.get(api).then(function (data) {
+  //     var checkStatus = httpFactory.dataValidation(data);
+  //     //console.log("data--" + JSON.stringify(data.data));
+  //     if (checkStatus) {
+  //       $scope.studentList = data.data.data;
+  //       console.log("studentList: " + JSON.stringify($scope.studentList));
+  //       for (var x = 0; x < $scope.studentList.length; x++) {
+  //         $scope.studList.push({ "id": $scope.studentList[x]._id, "name": $scope.studentList[x].studName, "studId": $scope.studentList[x].studId });
 
-        }
-        console.log(" $scope.studList.length: " + $scope.studList.length);
-        //   $scope.css = $scope.teacherData[0].css;
-        //   console.log("$scope.css: " + JSON.stringify($scope.css));
-      }
-      else {
-        console.log("sorry");
-      }
-    })
-    console.log("<--getStudListForCS");
+  //       }
+  //       console.log(" $scope.studList.length: " + $scope.studList.length);
+  //       //   $scope.css = $scope.teacherData[0].css;
+  //       //   console.log("$scope.css: " + JSON.stringify($scope.css));
+  //     }
+  //     else {
+  //       console.log("sorry");
+  //     }
+  //   })
+  //   console.log("<--getStudListForCS");
 
-  }
+  // }
 
   $scope.festDetailSub = function (file) {
 

6390a121043f0c9ea34896a92f10874d79b93e77
LogeswariGuru <logeswari.careator@gmail.com>
2018-05-11 10:19:35 +0530

    studmark and stud attendance for conference track

---
+++ public/js/studentMarkAtt_automation.js
@@ -0,0 +1,4 @@
+var studSchoolId = localStorage.getItem("studSchoolId");
+var schoolName = localStorage.getItem("schoolName");
+console.log("studSchoolId: "+studSchoolId+" schoolName: "+schoolName);
+

15990d403024673cc423f4bde9eb94a60230673b
LogeswariGuru <logeswari.careator@gmail.com>
2018-05-13 19:43:45 +0530

    attendance view on reportsupdate

--- public/controllers/reportsUpdateCtl.js
+++ public/controllers/reportsUpdateCtl.js
@@ -1,4 +1,4 @@
-app.controller('reportsUpdateCtl', function ($scope, $window, $state, httpFactory, $uibModal, sessionAuthFactory) {
+app.controller('reportsUpdateCtl', function ($scope, $window, $state, httpFactory, $uibModal, sessionAuthFactory, moment, calendarConfig) {
   console.log("attendanceCtl==>");
   $scope.userData = sessionAuthFactory.getAccess("userData");
   var schoolName = $scope.userData.schoolName;
@@ -170,6 +170,47 @@ app.controller('reportsUpdateCtl', function ($scope, $window, $state, httpFactor
       if (checkStatus) {
         $scope.studentDetail = data.data.data;
         console.log(" $scope.studentDetail: " + JSON.stringify($scope.studentDetail));
+        var studData = data.data.data;
+        $scope.attendance = studData[0].attendance;
+        console.log("studData: " + JSON.stringify(studData));
+        console.log("$scope.attendance: " + JSON.stringify($scope.attendance));
+        console.log("$scope.attendance.length: " + $scope.attendance.length);
+        for (var x = 0; x < $scope.attendance.length; x++) {
+            console.log("$scope.attendance[x]: " + JSON.stringify($scope.attendance[x]));
+            var year = "2018";
+            var mon = $scope.attendance[x].month;
+            console.log("$scope.attendance[x].dateAttendance.length: " + $scope.attendance[x].dateAttendance.length);
+            for (var y = 0; y < $scope.attendance[x].dateAttendance.length; y++) {
+                console.log("$scope.attendance[x].dateAttendance[y]: " + JSON.stringify($scope.attendance[x].dateAttendance[y]));
+                var day = $scope.attendance[x].dateAttendance[y].date;
+                console.log("day: " + day + "month: " + mon + "year: " + year);
+                var resultDate = new Date(year + " " + mon + " " + day);
+                console.log("resultDate: " + resultDate);
+                console.log("moment().subtract(1, 'day').toDate(): " + moment().subtract(1, 'day').toDate());
+                var obj = {
+                    'title': $scope.attendance[x].dateAttendance[y].status,
+                    'startsAt': resultDate,
+                    'endsAt': resultDate,
+                    'draggable': true,
+                    'resizable': true,
+                    'incrementsBadgeTotal': false
+                }
+                if ($scope.attendance[x].dateAttendance[y].status == "P") {
+                    obj.color = calendarConfig.colorTypes.info;
+                }
+               else if ($scope.attendance[x].dateAttendance[y].status == "L") {
+                    obj.color = calendarConfig.colorTypes.warning;
+                }
+                else {
+                    obj.color = calendarConfig.colorTypes.important;
+                }
+                console.log("obj: " + JSON.stringify(obj));
+                $scope.events.push(obj);
+
+            }
+        }
+        console.log("$scope.events: " + JSON.stringify($scope.events));
+
       }
       else {
 
@@ -306,6 +347,15 @@ app.controller('reportsUpdateCtl', function ($scope, $window, $state, httpFactor
     console.log("<--uploadFile");
   }
 
+  var vm = this;
+
+  $scope.calendarView = 'month';
+  $scope.viewDate = moment().startOf('day').toDate();
+  var originalFormat = calendarConfig.dateFormats.hour;
+  calendarConfig.dateFormats.hour = 'HH:mm';
+
+  $scope.events = [];
+  $scope.cellIsOpen = true;
 
 
 })

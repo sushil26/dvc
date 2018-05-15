7cba8c97afc7ac3eca580043505042a43849b46d
LogeswariGuru <logeswari.careator@gmail.com>
2018-05-16 01:48:22 +0530

    change password validation frontend

--- public/controllers/changePasswordCtl.js
+++ public/controllers/changePasswordCtl.js
@@ -42,30 +42,30 @@ app.controller('changePasswordCtl', function ($scope, $filter, $window, httpFact
             var checkStatus = httpFactory.dataValidation(data);
             console.log("data--" + JSON.stringify(data.data));
             if (checkStatus) {
-                var loginAlert = $uibModal.open({
-                    scope: $scope,
-                    templateUrl: '/html/templates/dashboardsuccess.html',
-                    windowClass: 'show',
-                    backdropClass: 'static',
-                    keyboard: false,
-                    controller: function ($scope, $uibModalInstance) {
-                      $scope.message = data.data.message;
-                    }
-                  })
-                // alert(data.data.message);
+                // var loginAlert = $uibModal.open({
+                //     scope: $scope,
+                //     templateUrl: '/html/templates/dashboardsuccess.html',
+                //     windowClass: 'show',
+                //     backdropClass: 'static',
+                //     keyboard: false,
+                //     controller: function ($scope, $uibModalInstance) {
+                //       $scope.message = data.data.message;
+                //     }
+                //   })
+                alert(data.data.message);
             }
             else{
-                var loginAlert = $uibModal.open({
-                    scope: $scope,
-                    templateUrl: '/html/templates/dashboardwarning.html',
-                    windowClass: 'show',
-                    backdropClass: 'static',
-                    keyboard: false,
-                    controller: function ($scope, $uibModalInstance) {
-                      $scope.message = "Failed to create";
-                    }
-                  })
-             //   alert("Failed to create");
+                // var loginAlert = $uibModal.open({
+                //     scope: $scope,
+                //     templateUrl: '/html/templates/dashboardwarning.html',
+                //     windowClass: 'show',
+                //     backdropClass: 'static',
+                //     keyboard: false,
+                //     controller: function ($scope, $uibModalInstance) {
+                //       $scope.message = "Failed to create";
+                //     }
+                //   })
+              alert("Failed to create");
             }
         })
         console.log("<--passwordChange");

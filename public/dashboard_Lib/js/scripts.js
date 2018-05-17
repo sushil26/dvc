67676e3e0ab84c82c59ac0702b030a01f8606e62
shaiksajans <shaiksajans@gmail.com>
2018-05-17 14:50:07 +0530

    logu merge

--- public/dashboard_Lib/js/scripts.js
+++ public/dashboard_Lib/js/scripts.js
@@ -7,55 +7,85 @@ function initializeJS() {
     jQuery('.popovers').popover();
 
     //custom scrollbar
-        //for html
-    jQuery("html").niceScroll({styler:"fb",cursorcolor:"#007AFF", cursorwidth: '6', cursorborderradius: '10px', background: '#F7F7F7', cursorborder: '', zindex: '1000'});
-        //for sidebar
-    jQuery("#sidebar").niceScroll({styler:"fb",cursorcolor:"#007AFF", cursorwidth: '3', cursorborderradius: '10px', background: '#F7F7F7', cursorborder: ''});
-        // for scroll panel
-    jQuery(".scroll-panel").niceScroll({styler:"fb",cursorcolor:"#007AFF", cursorwidth: '3', cursorborderradius: '10px', background: '#F7F7F7', cursorborder: ''});
-    
+    //for html
+    jQuery("html").niceScroll({
+        styler: "fb",
+        cursorcolor: "#007AFF",
+        cursorwidth: '6',
+        cursorborderradius: '10px',
+        background: '#F7F7F7',
+        cursorborder: '',
+        zindex: '1000'
+    });
+    //for sidebar
+    jQuery("#sidebar").niceScroll({
+        styler: "fb",
+        cursorcolor: "#007AFF",
+        cursorwidth: '3',
+        cursorborderradius: '10px',
+        background: '#F7F7F7',
+        cursorborder: ''
+    });
+    // for scroll panel
+    jQuery(".scroll-panel").niceScroll({
+        styler: "fb",
+        cursorcolor: "#007AFF",
+        cursorwidth: '3',
+        cursorborderradius: '10px',
+        background: '#F7F7F7',
+        cursorborder: ''
+    });
+
     //sidebar dropdown menu
     jQuery('#sidebar .sub-menu > a').click(function () {
-        var last = jQuery('.sub-menu.open', jQuery('#sidebar'));        
+        var last = jQuery('.sub-menu.open', jQuery('#sidebar'));
         jQuery('.menu-arrow').removeClass('arrow_carrot-right');
         jQuery('.sub', last).slideUp(200);
         var sub = jQuery(this).next();
         if (sub.is(":visible")) {
-            jQuery('.menu-arrow').addClass('arrow_carrot-right');            
+            jQuery('.menu-arrow').addClass('arrow_carrot-right');
             sub.slideUp(200);
         } else {
-            jQuery('.menu-arrow').addClass('arrow_carrot-down');            
+            jQuery('.menu-arrow').addClass('arrow_carrot-down');
             sub.slideDown(200);
         }
         var o = (jQuery(this).offset());
         diff = 200 - o.top;
-        if(diff>0)
-            jQuery("#sidebar").scrollTo("-="+Math.abs(diff),500);
+        if (diff > 0)
+            jQuery("#sidebar").scrollTo("-=" + Math.abs(diff), 500);
         else
-            jQuery("#sidebar").scrollTo("+="+Math.abs(diff),500);
+            jQuery("#sidebar").scrollTo("+=" + Math.abs(diff), 500);
     });
 
     // sidebar menu toggle
-    jQuery(function() {
+    jQuery(function () {
         function responsiveView() {
             var wSize = jQuery(window).width();
             if (wSize <= 768) {
                 jQuery('#container').addClass('sidebar-close');
                 jQuery('#sidebar > ul').hide();
-               
+
             }
 
             if (wSize > 768) {
                 jQuery('#container').removeClass('sidebar-close');
                 jQuery('#sidebar > ul').show();
             }
-       
+
         }
         jQuery(window).on('load', responsiveView);
         jQuery(window).on('resize', responsiveView);
     });
 
     jQuery('.toggle-nav').click(function () {
+
+        if (window.matchMedia('(max-width: 767px)').matches) {
+            $("#videosAttach").css({
+                    'margin-top': '195px'
+                }
+
+            );
+        }
         if (jQuery('#sidebar > ul').is(":visible") === true) {
             jQuery('#main-content').css({
                 'margin-left': '0px'
@@ -90,6 +120,6 @@ function initializeJS() {
 
 }
 
-jQuery(document).ready(function(){
+jQuery(document).ready(function () {
     initializeJS();
 });

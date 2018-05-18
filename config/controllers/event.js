7fefb99eefdb75f96ef4bceca851ed068bc170be
LogeswariGuru <logeswari.careator@gmail.com>
2018-05-18 11:41:00 +0530

    added student model

--- config/controllers/event.js
+++ config/controllers/event.js
@@ -1,8 +1,8 @@
 var db = require('../dbConfig.js').getDb();
 var user = db.collection('user');/* ### Teacher collection  ### */
 var event = db.collection('event');
-var stud = require("./schemas/student.js");
-//var stud = db.collection('student');/* ### student collection  ### */
+//var student = require("./schemas/student.js");
+var stud = db.collection('student');/* ### student collection  ### */
 var general = require('../general.js');
 var ObjectId = require('mongodb').ObjectID;
 

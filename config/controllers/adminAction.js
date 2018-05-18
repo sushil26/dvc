a57091e0ee47dbb3f2c0e55c3d381d5f08537462
LogeswariGuru <logeswari.careator@gmail.com>
2018-05-18 12:27:34 +0530

    added teacher model

--- config/controllers/adminAction.js
+++ config/controllers/adminAction.js
@@ -1,7 +1,7 @@
 
 var db = require("../dbConfig.js").getDb();
 var student = require("./schemas/student.js");
-var teacher = require("./schemas/teacher.js");
+//var teacher = require("./schemas/teacher.js");
 var user = db.collection("user"); /* ### Teacher collection  ### */
 var stud = db.collection("student"); /* ### student collection  ### */
 var school = db.collection("school"); /* ### school collection  ### */
@@ -1815,33 +1815,12 @@ module.exports.uploadTeacherMaster = function (req, res) {
             teacher.create(objJson, function (err, data) {
                 console.log("data: " + JSON.stringify(data));
                 if (err) {
-                    if (err.code == 11000) {
-                        console.log("err: " + JSON.stringify(err.errmsg));
-                        var errmsg = err.errmsg;
-                        var splitErrMsg = errmsg.split(':');
-                        var nextSplit = splitErrMsg[4].split('}');
-                        console.log("splitErrMsg: " + splitErrMsg + " nextSplit: " + nextSplit);
-                        responseData = {
-                            status: false,
-                            message: nextSplit[0] + " Already exist"
-                        };
-                        res.status(400).send(responseData);
-                    }
-                    else {
-                        console.log("err.errors.name: " + err.name);
-                        console.log("err.errors: " + err.errors);
-                        if (err.name == 'ValidationError') {
-                            if (err.errors.mobileNum) {
-                                console.log("mobile Number has to be Number");
-                                responseData = {
-                                    status: false,
-                                    message: "Mobile Number has to be Number",
-
-                                };
-                                res.status(400).send(responseData);
-                            }
-                        }
-                    }
+                    responseData = {
+                        status: false,
+                        message: "Failed to Insert",
+                        data: data
+                    };
+                    res.status(400).send(responseData);
                 } else {
                     responseData = {
                         status: true,

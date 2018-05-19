b50fb9d3e3af554806aa2060edb90174a3f36892
LogeswariGuru <logeswari.careator@gmail.com>
2018-05-19 10:15:18 +0530

    added log file for login

--- config/controllers/user.js
+++ config/controllers/user.js
@@ -78,6 +78,30 @@ module.exports.register4VC = function (req, res) {
 
 module.exports.login4VC = function (req, res) {
   console.log("login==>");
+  const log4js = require('log4js');
+
+const log4js_extend = require("log4js-extend");
+
+log4js.configure({
+  appenders: { cheese: { type: 'file', filename: 'debug.log' } },
+  categories: { default: { appenders: ['cheese'], level: 'all' } }
+});
+
+const logger = log4js.getLogger("cheese");
+
+// log4js_extend(log4js, {
+//     path: __dirname,
+//     format: "at @name (@file:@line:@column)"
+//   });
+// Object.defineProperty(exports, "LOG", {
+//             value:logger,
+// });
+logger.trace('Entering cheese testing');
+logger.debug('Got cheese.');
+logger.info('Cheese is Gouda.');
+logger.warn('Cheese is quite smelly.');
+logger.error('Cheese is too ripe!');
+logger.fatal('Cheese was breeding ground for listeria.');
   log.info("log is working");
   log.trace('Entering cheese testing');
   log.debug('Got cheese.');

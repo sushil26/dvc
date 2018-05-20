f4e90aec8ebab5ea1d23c221904e2533e347f094
LogeswariGuru <logeswari.careator@gmail.com>
2018-05-20 17:06:39 +0530

    log

--- config/log.js
+++ config/log.js
@@ -5,8 +5,8 @@ const log4js = require('log4js');
 const log4js_extend = require("log4js-extend");
 
 log4js.configure({
-  appenders: { cheese: { type: 'file', filename: './cheese.log' } }
-  // categories: { default: { appenders: ['cheese'], level: 'error' } }
+  appenders: { cheese: { type: 'file', filename: './cheese.log' } },
+   categories: { default: { appenders: ['cheese'], level: 'error' } }
 });
 
 const logger = log4js.getLogger("cheese");

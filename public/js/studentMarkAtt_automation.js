35d52bb6e526df38a6e5a88bc6c0e4f4b87ffa8e
LogeswariGuru <logeswari.careator@gmail.com>
2018-05-20 11:19:58 +0530

    added session in upcomng eventcrt

--- public/js/studentMarkAtt_automation.js
+++ public/js/studentMarkAtt_automation.js
@@ -1,18 +1,32 @@
 var id = localStorage.getItem("id");
 var schoolName = localStorage.getItem("schoolName");
-console.log("studSchoolId: "+id+" schoolName: "+schoolName);
+console.log("studSchoolId: " + id + " schoolName: " + schoolName);
 
-function attendance(){
-    window.open(
-        "https://norecruits.com/#!dashboard/automationAttendanceView/"+id,
-        '_blank' // <- This is what makes it open in a new window.
-      );
-    
+function momVC() {
+    console.log("momVC-->");
+    var mom = document.getElementById("momtext").innerHTML;
+    console.log("mom:" + mom);
+    if (localStorage.getItem("teacherLoginId")) {
+        var teacherLoginId = localStorage.getItem("teacherLoginId");
+        console.log("teacherLoginId: " + teacherLoginId);
+    }
+    else if (localStorage.getItem("studLoginId")) {
+        var studLoginId = localStorage.getItem("studLoginId");
+        console.log("studLoginId: " + studLoginId);
+    }
+    console.log("<--momVC");
 }
-function result(){
+function attendance() {
     window.open(
-        "https://norecruits.com/#!dashboard/automationResultView/"+id,
+        "https://norecruits.com/#!dashboard/automationAttendanceView/" + id,
         '_blank' // <- This is what makes it open in a new window.
-      );
+    );
+
+}
+function result() {
+    window.open(
+        "https://norecruits.com/#!dashboard/automationResultView/" + id,
+        '_blank' // <- This is what makes it open in a new window.
+    );
     // window.location.href = "https://norecruits.com/#!dashboard/automationResultView/"+id;    
 }

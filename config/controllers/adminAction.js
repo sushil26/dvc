12e4028cb7d5ab002871cc7555954ed6df6790a7
LogeswariGuru <logeswari.careator@gmail.com>
2018-05-24 11:09:06 +0530

    monkey schema added

--- config/controllers/adminAction.js
+++ config/controllers/adminAction.js
@@ -2408,18 +2408,7 @@ module.exports.csvTest = function (req, res) {
     //var stream = fs.createReadStream(req.files.img);
     var studentDataFile = req.files.img;
     csv.fromStream(studentDataFile.data.toString(),{headers : true})
-        .validate(function (data, next) { 
-            console.log("CSV validate-->");
-            monkey.findOne({id: data.id}, function (err, model) {
-                if (err) {
-                    console.log("CSV validate: mongoose err: "+err);
-                    next(err);
-                } else {
-                    console.log("CSV validate: mongoose model: "+JSON.stringify(model));
-                    next(null, !model); //valid if the model does not exist
-                }
-            });
-        })
+       
         .on("data", function (data) {
             console.log("CSV data--> "+JSON.stringify(data));
             console.log(data);

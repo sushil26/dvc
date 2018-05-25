dd92777d71286d745f0f29d8c8ecd7387f097a70
LogeswariGuru <logeswari.careator@gmail.com>
2018-05-25 12:30:19 +0530

    file upload multipart

--- public/common/httpFactory.js
+++ public/common/httpFactory.js
@@ -118,7 +118,7 @@ app.factory('httpFactory', function ($http, $q, $rootScope) {
 
             $http.post(postUrl, fd, {
                 transformRequest: angular.identity,
-                headers: {  }
+                headers: { 'Content-Type': 'multipart/form-data' }
             }).then(function (response) {
                 console.log("lego--" + response)
                 dfd.resolve(response);

8fd5cf4ba3f2342c920c791a1924900bea70f81c
LogeswariGuru <logeswari.careator@gmail.com>
2018-05-25 15:38:30 +0530

    file upload multipart issue fixing

--- config/controllers/image.js
+++ config/controllers/image.js
@@ -3,7 +3,7 @@ var storage = multer.diskStorage({
     
     destination: function (req, file, cb) {
         console.log("storage");
-        cb(null, './public/')
+        cb(null, './public/schoolLogo/')
     },
     filename: function (req, file, cb) {
         if (!file.originalname.match(/\.(png|jpeg|jpg)$/)) 

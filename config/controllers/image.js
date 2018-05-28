16dee1b64d5a2b131a52a5eca78540ccc4a2b2b5
LogeswariGuru <logeswari.careator@gmail.com>
2018-05-28 10:34:04 +0530

    schoolLogoSt

--- config/controllers/image.js
+++ config/controllers/image.js
@@ -80,7 +80,7 @@ if (!req.files)
   // console.log(" req.files.logo.name: "+ req.files.logo.name);
   let myFile = req.files.logo;
   //console.log(__dirname+myFile.name, require.main.filename, process.cwd());
-  console.log("myFile--"+myFile)
+ // console.log("myFile--"+myFile)
   var fileArr = myFile.name.split(".");
   var fileName="";
   for(var i=0;i<fileArr.length-1;i++)

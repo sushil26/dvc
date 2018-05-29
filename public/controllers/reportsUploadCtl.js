8ebe3d74e2cd661b80f691adad77251f3337f66e
LogeswariGuru <logeswari.careator@gmail.com>
2018-05-29 06:54:39 +0530

    fee type added

--- public/controllers/reportsUploadCtl.js
+++ public/controllers/reportsUploadCtl.js
@@ -483,7 +483,7 @@ app.controller('reportsUploadCtl', function ($scope, $rootScope, $window, httpFa
 
   $scope.uploadFeeFile = function (file, clas, section, reportType) {
     console.log("uploadFeeFile-->");
-    console.log("testTYpe: " + reportType + "date: " + date);
+    console.log("testTYpe: " + reportType + "clas: " + clas + "section: " + section);
     var obj = {
       "file": file
     }

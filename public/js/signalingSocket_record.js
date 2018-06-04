5f5303b6465feba6226623bf7d1e9402e7438466
shaiksajans <shaiksajans@gmail.com>
2018-06-04 17:42:42 +0530

    jhjhjhc

--- public/js/signalingSocket_record.js
+++ public/js/signalingSocket_record.js
@@ -1437,7 +1437,7 @@ function onMediaSuccess(stream) {
       if (multiStreamRecorder && multiStreamRecorder.stream) return;
     
      
-      multiStreamRecorder = new MultiStreamRecorder([stream, remote_media]);
+      multiStreamRecorder = new MultiStreamRecorder([stream, peerStream]);
       multiStreamRecorder.stream = stream;
 
       multiStreamRecorder.previewStream = function (stream) {

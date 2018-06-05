cb57bd6c59ec3e3d008b9108b1ff222c29ed459a
LogeswariGuru <logeswari.careator@gmail.com>
2018-06-05 15:30:35 +0530

    captureImg

--- public/controllers/captureImgCtl.js
+++ public/controllers/captureImgCtl.js
@@ -90,24 +90,53 @@ app.controller('captureImgCtl', function ($scope, $rootScope, $window, httpFacto
 
         // Pause video playback of stream.
         video.pause();
-        var api = $scope.propertyJson.VC_captureImgSend;
-        console.log("api: " + api);
-        var obj = {
-            "data": snap
-        }
-        httpFactory.post(api, obj).then(function (data) {
-            var checkStatus = httpFactory.dataValidation(data);
-            //console.log("data--" + JSON.stringify(data.data));
-            if (checkStatus) {
-                console.log("data" + JSON.stringify(data.data));
-                alert("success");;
-            }
-            else {
-                alert("fail");;
-            }
+        var resultBlob = dataURItoBlob(snap);
+        console.log("resultBlob: "+resultBlob);
+        console.log("resultBlob: "+JSON.stringify(resultBlob));
+        // $scope.mySelfi = snap;
+        // var api = $scope.propertyJson.VC_captureImgSend;
+        // console.log("api: " + api);
+        // var obj = {
+        //     "data": snap
+        // }
+        // httpFactory.post(api, obj).then(function (data) {
+        //     var checkStatus = httpFactory.dataValidation(data);
+        //     //console.log("data--" + JSON.stringify(data.data));
+        //     if (checkStatus) {
+        //         console.log("data" + JSON.stringify(data.data));
+        //         alert("success");;
+        //     }
+        //     else {
+        //         alert("fail");;
+        //     }
 
-        });
+        // });
     })
+
+    function dataURItoBlob(dataURI) {
+        // convert base64 to raw binary data held in a string
+        // doesn't handle URLEncoded DataURIs - see SO answer #6850276 for code that does this
+        var byteString = atob(dataURI.split(',')[1]);
+      
+        // separate out the mime component
+        var mimeString = dataURI.split(',')[0].split(':')[1].split(';')[0]
+      
+        // write the bytes of the string to an ArrayBuffer
+        var ab = new ArrayBuffer(byteString.length);
+      
+        // create a view into the buffer
+        var ia = new Uint8Array(ab);
+      
+        // set the bytes of the buffer to the correct values
+        for (var i = 0; i < byteString.length; i++) {
+            ia[i] = byteString.charCodeAt(i);
+        }
+      
+        // write the ArrayBuffer to a blob, and you're done
+        var blob = new Blob([ab], {type: mimeString});
+        return blob;
+      
+      }
 
 
     delete_photo_btn.addEventListener("click", function (e) {

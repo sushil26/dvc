e7ae85c21a5766613a52f209e56044f33e7dd34e
LogeswariGuru <logeswari.careator@gmail.com>
2018-06-05 18:02:42 +0530

    captureImg

--- config/controllers/capture.js
+++ config/controllers/capture.js
@@ -50,12 +50,12 @@ module.exports.captureImgSend = function (req, res) {
 
         }
         else {
-            var responseData = {
-                "status": true,
-                "message": "date stored successfully",
-                "data": { "filePath": "/dailyPic/" + fileName }
-            }
-            res.status(200).send(responseData);
+            // var responseData = {
+            //     "status": true,
+            //     "message": "date stored successfully",
+            //     "data": { "filePath": "/dailyPic/" + fileName }
+            // }
+            // res.status(200).send(responseData);
             var mailOptions = {
                 from: "info@vc4all.in",
                 to: "logeswari.g@careator.com",
@@ -66,34 +66,55 @@ module.exports.captureImgSend = function (req, res) {
                 //     path: ABSPATH + '/public/home/img/bc.jpg',
                 //     cid: 'unique@kreata.ee' //same cid value as in the html img src
                 // }]
-                html: 'Embedded image: <img src=cid:/dailyPic/'+fileName + '/>',
+                html: 'Embedded image: <img src=cid:/dailyPic/' + fileName + '/>',
                 attachments: [{
                     filename: 'selfi.jpg',
                     path: ABSPATH + '/public/dailyPic/' + fileName,
-                    cid: '/dailyPic/'+fileName //same cid value as in the html img src
+                    cid: '/dailyPic/' + fileName //same cid value as in the html img src
                 }]
             };
+            transporter.sendMail(mailOptions, function (error, info) {
+                if (error) {
+                    console.log(error);
+                    responseData = {
+                        status: true,
+                        errorCode: 200,
+                        message: "insert Successfull and Failed to send mail",
+                        data: data
+                    };
+                    res.status(200).send(responseData);
+                } else {
+                    console.log("Email sent: " + info.response);
+                    responseData = {
+                        status: true,
+                        errorCode: 200,
+                        message: "Successfully mail sent",
+                        data: data
+                    };
+                    res.status(200).send(responseData);
+                }
+            });
         }
     });
 
 
-    var mailOptions = {
-        from: "info@vc4all.in",
-        to: "logeswari.g@careator.com",
-        subject: 'VC4ALL Credential',
-        // html: 'Embedded image: <img src="cid:unique@kreata.ee"/>',
-        // attachments: [{
-        //     filename: 'image.png',
-        //     path: ABSPATH + '/public/home/img/bc.jpg',
-        //     cid: 'unique@kreata.ee' //same cid value as in the html img src
-        // }]
-        html: 'Embedded image: <img src=cid:' + req.body.data + '/>',
-        attachments: [{
-            filename: 'selfi.jpg',
-            path: req.body.data,
-            cid: req.body.data //same cid value as in the html img src
-        }]
-    };
+    // var mailOptions = {
+    //     from: "info@vc4all.in",
+    //     to: "logeswari.g@careator.com",
+    //     subject: 'VC4ALL Credential',
+    //     // html: 'Embedded image: <img src="cid:unique@kreata.ee"/>',
+    //     // attachments: [{
+    //     //     filename: 'image.png',
+    //     //     path: ABSPATH + '/public/home/img/bc.jpg',
+    //     //     cid: 'unique@kreata.ee' //same cid value as in the html img src
+    //     // }]
+    //     html: 'Embedded image: <img src=cid:' + req.body.data + '/>',
+    //     attachments: [{
+    //         filename: 'selfi.jpg',
+    //         path: req.body.data,
+    //         cid: req.body.data //same cid value as in the html img src
+    //     }]
+    // };
     // transporter.sendMail(mailOptions, function (error, info) {
     //     if (error) {
     //         console.log(error);

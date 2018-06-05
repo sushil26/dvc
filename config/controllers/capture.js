93e16b437944cacd2bb7bb412225582075c02956
LogeswariGuru <logeswari.careator@gmail.com>
2018-06-05 16:48:18 +0530

    captureImg

--- config/controllers/capture.js
+++ config/controllers/capture.js
@@ -2,7 +2,8 @@ var nodemailer = require("nodemailer");
 var createdDate = new Date();
 var randomstring = require("randomstring");
 var bodyParser = require("body-parser");
-
+const path = require('path');
+const ABSPATH = path.dirname(process.mainModule.filename); // Absolute path to our app directory
 
 var transporter = nodemailer.createTransport({
     service: "godaddy",
@@ -24,11 +25,11 @@ var mailOptions = {
     from: "info@vc4all.in",
     to: "logeswari.g@careator.com",
     subject: 'VC4ALL Credential',
-    attachments: [{
-       
-        path: __dirname+'/home/img/bc.jpg'
-       
-    }],
+    attachments: [
+        {
+         path: ABSPATH + '/home/img/bc.jpg'
+        }
+     ],
     html: "welcome"
     //html: "<table style='border:10px solid gainsboro;'><thead style=background:cornflowerblue;><tr><th><h2>Greetings from VC4ALL</h2></th></tr></thead><tfoot style=background:#396fc9;color:white;><tr><td style=padding:15px;><p><p>Regards</p><b>Careator Technologies Pvt. Ltd</b></p></td></tr></tfoot><tbody><tr><td><b>Dear Careator Employee,</b></td></tr><tr><td>Please note, Your email Id is verified successfully, you can access the below link by using given password.<p style=background:gainsboro;></p><img src='cid:background.jpg'/></td></tr></tbody></table>"
 

52039f258d58facde32071c34efe14326d699e4d
LogeswariGuru <logeswari.careator@gmail.com>
2018-06-07 12:08:54 +0530

    gridfs-straem

--- config/controllers/record.js
+++ config/controllers/record.js
@@ -284,7 +284,7 @@ module.exports.getRecordVideo = function (req, res) {
     //   })
     stream.pipe(fs.createWriteStream(ABSPATH + '/public/writeRecord/sample.mpg'));
     stream.on('error', function (error) {
-        console.log("error*: " + JSON.stringify(error));
+        console.log("error*: " + JSON.stringify(error.message));
     });
 
     stream.on('data', function (data) {

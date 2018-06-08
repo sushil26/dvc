5e1e5734033621d770cac73c0d24a4236effc87c
LogeswariGuru <logeswari.careator@gmail.com>
2018-06-08 11:17:44 +0530

    added atob

--- public/js/signalingSocket_record.js
+++ public/js/signalingSocket_record.js
@@ -1466,43 +1466,26 @@ function storeRecordVideo() {
   var obj = {
     "url": recordedURL
   }
-  //var resultBlob = dataURItoBlob(recordedURL);
-  //console.log("recordedURL: " + recordedURL);
   console.log("obj: " + JSON.stringify(obj));
-  // var fd = new FormData();
-  // console.log("recordedURL: " + recordedURL);
-  // fd.append('logo', recordedURL);
-  // console.log("fd: " + fd);
- 
-  
+
+  $.ajax({
+    url: "https://norecruits.com/record/recordVideo",
+    type: "POST",
+    data: JSON.stringify(obj),
+    contentType: "application/json",
+    dataType: "json",
+    success: function (data) {
+      console.log("data: " + JSON.stringify(data));
+    },
+    error: function (err) {
+      console.log("err: " + JSON.stringify(err));
+      console.log("err.responseText: " + JSON.stringify(err.responseText));
+      console.log("err.responseJSON: " + JSON.stringify(err.responseJSON.message));
+      alert(err.responseJSON.message);
+    }
+  });
 }
 
-// function dataURItoBlob(dataURI) {
-//   console.log("dataURItoBlob-->")
-//   // convert base64 to raw binary data held in a string
-//   // doesn't handle URLEncoded DataURIs - see SO answer #6850276 for code that does this
-
-//   var byteString = atob(dataURI.split(',')[1]);
-
-//   // separate out the mime component
-//   var mimeString = dataURI.split(',')[0].split(':')[1].split(';')[0]
-
-//   // write the bytes of the string to an ArrayBuffer
-//   var ab = new ArrayBuffer(byteString.length);
-
-//   // create a view into the buffer
-//   var ia = new Uint8Array(ab);
-
-//   // set the bytes of the buffer to the correct values
-//   for (var i = 0; i < byteString.length; i++) {
-//       ia[i] = byteString.charCodeAt(i);
-//   }
-
-//   // write the ArrayBuffer to a blob, and you're done
-//   var blob = new Blob([ab], { type: mimeString });
-//   return blob;
-
-// }
 
 var multiStreamRecorder;
 var audioVideoBlobs = {};

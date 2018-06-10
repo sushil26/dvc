326aa5b5b040d3cf375fd4d91cff58829d630d4c
LogeswariGuru <logeswari.careator@gmail.com>
2018-06-10 22:52:26 +0530

    data added

--- public/js/signalingSocket_record.js
+++ public/js/signalingSocket_record.js
@@ -1476,7 +1476,7 @@ function storeRecordVideo() {
 }
 
 function dataURItoBlob(dataURI) {
-  var stringDataURL = dataURL.toString();
+  var stringDataURL = dataURI.toString();
   // convert base64 to raw binary data held in a string
   // doesn't handle URLEncoded DataURIs - see SO answer #6850276 for code that does this
   var byteString = atob(stringDataURL.split(',')[1]);

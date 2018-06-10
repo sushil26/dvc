cb61a797b0acaec383d712030cb79fb91679e679
LogeswariGuru <logeswari.careator@gmail.com>
2018-06-11 00:59:19 +0530

    data added

--- config/controllers/record.js
+++ config/controllers/record.js
@@ -288,7 +288,7 @@ module.exports.recordVideo = function (req, res) {
     });
     var byte_string = videoBase64.substr(23);//The base64 has a imageURL
     var buffer = new Buffer(byte_string);   //new Buffer(b64string, 'base64');  you can use base64 encoding with creating new buffer string
-    var response = streamifier.createReadStream(buffer).pipe(writestream);  // returns response which is having all information regarding saved byte string
+    var response = streamifier.createReadStream(buffer).pipe(writeStream);  // returns response which is having all information regarding saved byte string
     var lastInsertedFileId = response._store.fileId;  // now you can store it into another document for future use.
     console.log(lastInsertedFileId);
     // fs.createReadStream('/public/Recording/1.mp4').pipe(writeStream);

4239b405dddeb7c3f1be07a209199e3111fcb975
LogeswariGuru <logeswari.careator@gmail.com>
2018-06-11 12:47:31 +0530

    data added

--- config/controllers/record.js
+++ config/controllers/record.js
@@ -324,7 +324,7 @@ module.exports.getRecordVideo = function (req, res) {
     //console.log("readStream: " + readStream);
     var output = '';
     var readStream = gfs.createReadStream({
-        "_id" : ObjectId("5b1e093b5f1df242f9561536")    // this id was stored in db when inserted a video stream above
+        "_id" : ObjectId("5b1e21f89211846655bae63d")    // this id was stored in db when inserted a video stream above
     });
     readStream.on("data", function(chunk) {
         output += chunk;

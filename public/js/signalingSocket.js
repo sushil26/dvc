0149713d3ccca3003e35037231c14ca1a4f998ce
LogeswariGuru <logeswari.careator@gmail.com>
2018-06-11 16:45:23 +0530

    aaa

--- public/js/signalingSocket.js
+++ public/js/signalingSocket.js
@@ -1006,7 +1006,7 @@ function setup_local_media(callback, errorback) {
     if (streamArray.length > 1) {
       $('#start-recording').trigger("click");
     }
-    // $('#start-recording').trigger("click");
+    $('#start-recording').trigger("click");
     console.log("<--attachMediaStream");
   };
   navigator.getUserMedia({

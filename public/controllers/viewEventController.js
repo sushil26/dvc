be65f017b54d0f0dc32795a0b3af24a64246b36f
LogeswariGuru <logeswari.careator@gmail.com>
2018-06-12 14:37:44 +0530

    view event page added

--- public/controllers/viewEventController.js
+++ public/controllers/viewEventController.js
@@ -1,4 +1,4 @@
-app.controller('viewUserController', function ($scope, $rootScope, $state, $window, httpFactory, $uibModal) {
+app.controller('viewEventController', function ($scope, $rootScope, $state, $window, httpFactory, $uibModal) {
     console.log("viewUserController==>");
     var id = $state.params.id;
 

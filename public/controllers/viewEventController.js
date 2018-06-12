605aa7f90b0c64d3507b8c79e88016a44f4c1f62
LogeswariGuru <logeswari.careator@gmail.com>
2018-06-12 14:36:42 +0530

    view event page added

--- public/controllers/viewEventController.js
+++ public/controllers/viewEventController.js
@@ -2,22 +2,22 @@ app.controller('viewUserController', function ($scope, $rootScope, $state, $wind
     console.log("viewUserController==>");
     var id = $state.params.id;
 
-    $scope.getEventDetails = function (id) {
-        console.log("getTeacherData-->");
-        var api = $scope.propertyJson.VC_getEventById + "/" + id;
-        //var api = "http://localhost:5000/vc/eventGet"+ "/" + id;;
-        httpFactory.get(api).then(function (data) {
-            var checkStatus = httpFactory.dataValidation(data);
-            console.log("data--" + JSON.stringify(data.data));
-            if (checkStatus) {
-                $scope.eventData = data.data.data;
-                console.log("$scope.eventData");
-            }
-            else {
-                //alert("Event get Failed");
-            }
-        })
-        console.log("<--getTeacherData");
-    }
-    $scope.getEventDetails(id);
+    // $scope.getEventDetails = function (id) {
+    //     console.log("getTeacherData-->");
+    //     var api = $scope.propertyJson.VC_getEventById + "/" + id;
+    //     //var api = "http://localhost:5000/vc/eventGet"+ "/" + id;;
+    //     httpFactory.get(api).then(function (data) {
+    //         var checkStatus = httpFactory.dataValidation(data);
+    //         console.log("data--" + JSON.stringify(data.data));
+    //         if (checkStatus) {
+    //             $scope.eventData = data.data.data;
+    //             console.log("$scope.eventData");
+    //         }
+    //         else {
+    //             //alert("Event get Failed");
+    //         }
+    //     })
+    //     console.log("<--getTeacherData");
+    // }
+    // $scope.getEventDetails(id);
 })

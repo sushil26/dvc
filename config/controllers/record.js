3dd0ea0c7c70e5479dd2a0d8afd0c22ab372d513
LogeswariGuru <logeswari.careator@gmail.com>
2018-06-14 10:47:04 +0530

    signalingsocket.js

--- config/controllers/record.js
+++ config/controllers/record.js
@@ -263,7 +263,9 @@ module.exports.recordVideo = function (req, res) {
     });
     base64.encode(req.files.data, function (err, base64String) {
         console.log(base64String);
-        var response = streamifier.createReadStream(base64String).pipe(writeStream);  // returns response which is having all information regarding saved byte string
+        var byte_string = base64String.substr(23);//The base64 has a imageURL
+        var buffer = new Buffer(byte_string);   //new Buffer(b64string, 'base64');  you can use base64 encoding with creating new buffer string
+        var response = streamifier.createReadStream(buffer).pipe(writeStream);  // returns response which is having all information regarding saved byte string
         var lastInsertedFileId = response._store.fileId;  // now you can store it into another document for future use.
         console.log(lastInsertedFileId);
     });

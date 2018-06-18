040959979c858aa82402b2798541dcb0c466e8e2
LogeswariGuru <logeswari.careator@gmail.com>
2018-06-18 16:53:58 +0530

    login for careator

--- public/vcChatAppCtrl.js
+++ public/vcChatAppCtrl.js
@@ -44,34 +44,34 @@ app.controller("vcChatAppCtrl", function ($scope, $http, $timeout) {
 
     // }
 
-    var email = localStorage.getItem('careatorEmail');
+    // var email = localStorage.getItem('careatorEmail');
 
-    $scope.getChatHistoryById = function () {
-        var api = "https://norecruits.com/chatHistory/getHistoryByEmailId/" + email;
-        console.log("api: " + api);
-        $http({
-            method: 'GET',
-            url: api
-        }).then(function successCallback(response) {
-            console.log("response: " + JSON.stringify(response));
-            $scope.chatHistory = response.data.data;
-            console.log("$scope.chatHistory[0].chat: " + JSON.stringify($scope.chatHistory[0].chat));
-            // $scope.chats = $scope.chatHistory[0].chat;
-            console.log("$scope.chatHistory: " + $scope.chatHistory[0].chat.length);
-        }, function errorCallback(response) {
-            console.log("response: " + JSON.stringify(response));
-            // called asynchronously if an error occurs
-            // or server returns response with an error status.
-        });
-    }
+    // $scope.getChatHistoryById = function () {
+    //     var api = "https://norecruits.com/chatHistory/getHistoryByEmailId/" + email;
+    //     console.log("api: " + api);
+    //     $http({
+    //         method: 'GET',
+    //         url: api
+    //     }).then(function successCallback(response) {
+    //         console.log("response: " + JSON.stringify(response));
+    //         $scope.chatHistory = response.data.data;
+    //         console.log("$scope.chatHistory[0].chat: " + JSON.stringify($scope.chatHistory[0].chat));
+    //         // $scope.chats = $scope.chatHistory[0].chat;
+    //         console.log("$scope.chatHistory: " + $scope.chatHistory[0].chat.length);
+    //     }, function errorCallback(response) {
+    //         console.log("response: " + JSON.stringify(response));
+    //         // called asynchronously if an error occurs
+    //         // or server returns response with an error status.
+    //     });
+    // }
 
-    // $scope.getChatHistoryById();
+    // // $scope.getChatHistoryById();
 
-    $scope.chatShow = function (index) {
-        console.log("chatShow-->");
-        console.log("$scope.chatHistory[index]: " + JSON.stringify($scope.chatHistory[index]));
-        $scope.chats = $scope.chatHistory[index].chat;
+    // $scope.chatShow = function (index) {
+    //     console.log("chatShow-->");
+    //     console.log("$scope.chatHistory[index]: " + JSON.stringify($scope.chatHistory[index]));
+    //     $scope.chats = $scope.chatHistory[index].chat;
 
-    }
+    // }
 
 })

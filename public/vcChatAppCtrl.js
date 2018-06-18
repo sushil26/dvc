d5fb143a0404cb1f6f5dba219c46dd63f7d6138f
LogeswariGuru <logeswari.careator@gmail.com>
2018-06-18 12:40:44 +0530

    chat controller ng-repeat add

--- public/vcChatAppCtrl.js
+++ public/vcChatAppCtrl.js
@@ -1,3 +1,25 @@
 app.controller("vcChatAppCtrl", function ($scope, $rootScope, httpFactory) {
     console.log("Chat controller==>");
+    var email = localStorage.getItem('careatorEmail');
+
+    $scope.getChatHistoryById = function () {
+        var api = "chatHistory/getHistoryByEmailId/" + email;
+        console.log("api: " + api);
+        httpFactory.get(api).then(function (data) {
+            console.log("data--" + JSON.stringify(data.data));
+            var checkStatus = httpFactory.dataValidation(data);
+            if (checkStatus) {
+                $scope.chatHistory = data.data;
+                // $scope.adminList = data.data.data;
+                // console.log("adminList: " + JSON.stringify($scope.adminList));
+                // console.log(data.data.message);
+            }
+            else {
+                console.log("Sorry");
+            }
+        })
+    }
+
+    $scope.getChatHistoryById();
+
 })

a95aa66369684b3e03062a93d5db1447eabfbeb5
LogeswariGuru <logeswari.careator@gmail.com>
2018-06-18 12:51:20 +0530

    chat controller ng-repeat add

--- public/common/httpFactory.js
+++ public/common/httpFactory.js
@@ -135,6 +135,7 @@ app.factory('httpFactory', function ($http, $q, $rootScope) {
                 method: 'GET',
                 url: fileAddress
             }).then(function (data) {
+                console.log.log("data: "+JSON.stringify(data));
                 $rootScope.propertyJson = data.data;
                 return $rootScope.propertyJson;
             });

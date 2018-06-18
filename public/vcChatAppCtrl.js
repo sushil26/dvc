6716c7310e067ac288f2d9e52b53b2e6cbd13c43
LogeswariGuru <logeswari.careator@gmail.com>
2018-06-18 17:59:48 +0530

    login for careator

--- public/vcChatAppCtrl.js
+++ public/vcChatAppCtrl.js
@@ -4,6 +4,34 @@ app.controller("vcChatAppCtrl", function ($scope, $http, $timeout) {
     //    httpFactory.getFile('property.json');
     //    console.log("$rootScope.propertyJson: "+JSON.stringify($rootScope.propertyJson));
 
+
+    $scope.getChatHistoryById = function () {
+        // var api = "https://norecruits.com/chatHistory/getHistoryByEmailId/" + email;
+        var api = "https://norecruits.com/chatHistory/getHistory";
+        console.log("api: " + api);
+        $http({
+            method: 'GET',
+            url: api
+        }).then(function successCallback(response) {
+            console.log("response: " + JSON.stringify(response));
+            $scope.chatHistory = response.data.data;
+            console.log("$scope.chatHistory[0].chat: " + JSON.stringify($scope.chatHistory[0].chat));
+            // $scope.chats = $scope.chatHistory[0].chat;
+            console.log("$scope.chatHistory: " + $scope.chatHistory[0].chat.length);
+        }, function errorCallback(response) {
+            console.log("response: " + JSON.stringify(response));
+            // called asynchronously if an error occurs
+            // or server returns response with an error status.
+        });
+    }
+
+    $scope.chatShow = function (index) {
+        console.log("chatShow-->");
+        console.log("$scope.chatHistory[index]: " + JSON.stringify($scope.chatHistory[index]));
+        $scope.chats = $scope.chatHistory[index].chat;
+
+    }
+
     if (localStorage.getItem("admin")) {
         $scope.getChatHistoryById();
     }
@@ -49,33 +77,5 @@ app.controller("vcChatAppCtrl", function ($scope, $http, $timeout) {
 
     //var email = localStorage.getItem('careatorEmail');
 
-    $scope.getChatHistoryById = function () {
-        // var api = "https://norecruits.com/chatHistory/getHistoryByEmailId/" + email;
-        var api = "https://norecruits.com/chatHistory/getHistory";
-        console.log("api: " + api);
-        $http({
-            method: 'GET',
-            url: api
-        }).then(function successCallback(response) {
-            console.log("response: " + JSON.stringify(response));
-            $scope.chatHistory = response.data.data;
-            console.log("$scope.chatHistory[0].chat: " + JSON.stringify($scope.chatHistory[0].chat));
-            // $scope.chats = $scope.chatHistory[0].chat;
-            console.log("$scope.chatHistory: " + $scope.chatHistory[0].chat.length);
-        }, function errorCallback(response) {
-            console.log("response: " + JSON.stringify(response));
-            // called asynchronously if an error occurs
-            // or server returns response with an error status.
-        });
-    }
-
-
-
-    $scope.chatShow = function (index) {
-        console.log("chatShow-->");
-        console.log("$scope.chatHistory[index]: " + JSON.stringify($scope.chatHistory[index]));
-        $scope.chats = $scope.chatHistory[index].chat;
-
-    }
 
 })

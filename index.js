3a1a61ac501d3945dfa6f923de5fc8e3aca9bd69
LogeswariGuru <logeswari.careator@gmail.com>
2018-06-20 14:46:59 +0530

    body limit extended in index.js

--- index.js
+++ index.js
@@ -11,10 +11,12 @@ var fs = require('fs'),
     url = require('url'),
     path = require('path');
 var app = express();
-app.use(bodyParser.urlencoded({ extended: true }));
-app.use(bodyParser.json({
-    limit: '100mb'
-}));
+// app.use(bodyParser.urlencoded({ extended: true }));
+// app.use(bodyParser.json({
+//     limit: '100mb'
+// }));
+app.use(bodyParser.json({limit: "1024mb"}));
+app.use(bodyParser.urlencoded({limit: "1024mb", extended: true, parameterLimit:50000}));
 
 //app.use(multer());
 app.use(fileUpload());

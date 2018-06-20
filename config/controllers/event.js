3e4be5c4ea9309c4ee1cd0b579c98d9fb5e26025
LogeswariGuru <logeswari.careator@gmail.com>
2018-06-20 09:52:04 +0530

    getevent console commented

--- config/controllers/event.js
+++ config/controllers/event.js
@@ -185,7 +185,7 @@ module.exports.eventGet = function (req, res) {
 
     if (general.emptyCheck(req.params.id)) {
         event.find({ $or: [{ "userId": req.params.id }, { "remoteCalendarId": req.params.id }] }).sort({ "$natural": -1 }).toArray(function (err, listOfevents) {
-            console.log("listOfevents: " + JSON.stringify(listOfevents))
+            // console.log("listOfevents: " + JSON.stringify(listOfevents))
             if (err) {
                 responseData = {
                     "status": false,

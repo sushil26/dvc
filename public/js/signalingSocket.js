ee80cf1c916de94a0cd21f2c0a9fc84bf08ad4ef
LogeswariGuru <logeswari.careator@gmail.com>
2018-06-20 15:40:14 +0530

    time added

--- public/js/signalingSocket.js
+++ public/js/signalingSocket.js
@@ -1595,7 +1595,7 @@ function onMediaSuccess(stream) {
 
     var timeInterval = document.querySelector('#time-interval').value;
     if (timeInterval) timeInterval = parseInt(timeInterval);
-    else timeInterval = 5 * 1000;
+    else timeInterval = 1 * 1000;
 
     // get blob after specific time interval
     multiStreamRecorder.start(timeInterval);

64e5c39a169bf8f9ba34378b13b778913190f415
LogeswariGuru <logeswari.careator@gmail.com>
2018-06-22 15:23:31 +0530

    changes revert

--- public/js/signalingSocket.js
+++ public/js/signalingSocket.js
@@ -1611,7 +1611,7 @@ function onMediaSuccess(stream) {
     // var timeInterval = document.querySelector('#time-interval').value;
     // if (timeInterval) timeInterval = parseInt(timeInterval);
     // else timeInterval = 5 * 1000;
-    timeInterval = 10000;
+    timeInterval = 60000;
 
     // get blob after specific time interval
     multiStreamRecorder.start(timeInterval);

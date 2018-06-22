44ff2934fbb5329ef57356326871feb9b1a0c9d8
shaiksajans <shaiksajans@gmail.com>
2018-06-22 11:49:14 +0530

    jhjhsz

--- public/home/js/main.js
+++ public/home/js/main.js
@@ -6,6 +6,7 @@ jQuery(document).ready(function ($) {
     if ($(this).scrollTop() > 100) {
       $('.back-to-top').fadeIn('slow');
       $('#header').addClass('header-fixed');
+      $('#greenline').addClass('header-fixed');
     } else {
       $('.back-to-top').fadeOut('slow');
       $('#header').removeClass('header-fixed');

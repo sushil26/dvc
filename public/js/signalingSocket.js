9351cfe4c7c29fc43e167fff238883e63a3480b6
shaiksajans <shaiksajans@gmail.com>
2018-06-26 19:35:49 +0530

    path changes

--- public/js/signalingSocket.js
+++ public/js/signalingSocket.js
@@ -1079,7 +1079,7 @@ function setup_local_media(callback, errorback) {
       console.log("stream: " + JSON.stringify(stream));
       local_media_stream = stream;
       var local_media = USE_VIDEO ? $("<video>") : $();
-      local_media.attr("muted", "true"); /* always mute ourselves by default */
+      local_media.attr("muted", true); /* always mute ourselves by default */
       local_media.attr("id", "videoElem");
       local_media.attr("autoplay", "true");
       $("#portfolio-wrapper").append(
@@ -1188,10 +1188,10 @@ function setup_local_media(callback, errorback) {
             $("<video>") :
             $("<audio>");
           //local_mediaScreenShare.attr("autoplay", "autoplay");
-          local_mediaScreenShare.attr("muted",  "true" ); /* always mute ourselves by default */
+          local_mediaScreenShare.attr("muted", true ); /* always mute ourselves by default */
           // local_mediaScreenShare.attr("controls", "");
           local_mediaScreenShare.attr("id", "screenShareElem");
-          local_mediaScreenShare.attr("autoplay", "true");
+          local_mediaScreenShare.attr("autoplay", true);
           // local_mediaScreenShare.attr(
           //   "style",
           //   "border:1px solid skyblue"
@@ -1235,7 +1235,7 @@ function setup_local_media(callback, errorback) {
                 // local_media_shareStream = stream;
                 var local_media = USE_VIDEO ? $("<video>") : $();
                 //local_media.attr("autoplay", "autoplay");
-                local_media.attr( "muted", "true"); /* always mute ourselves by default */
+                local_media.attr( "muted", true); /* always mute ourselves by default */
                 // local_media.attr("controls", "");
                 local_media.attr("id", "videoElem");
                 // local_media.attr("style", "border:1px solid skyblue");

605a6e1795d8f9bcd6da98360f05e93179dff5d5
LogeswariGuru <logeswari.careator@gmail.com>
2018-06-27 12:16:57 +0530

    loader added for localvideo

--- public/js/csigsocket.js
+++ public/js/csigsocket.js
@@ -539,49 +539,35 @@ signaling_socket.on("addPeer", function (config) {
     if (existing) {
       existing.parentNode.removeChild(existing);
     }
-
     var remote_media = USE_VIDEO ? $("<video>") : $();
     console.log("remote_media: " + remote_media);
     remote_media.attr("autoplay", "autoplay");
-    // remote_media.attr("style", "border:5px solid gray");
     remote_media.attr("id", peer_id + "Remote");
     if (MUTE_AUDIO_BY_DEFAULT) {
       remote_media.prop("muted", true);
     }
-    // remote_media.attr("controls", "");
-
     remote_media.attr("name", config.userName);
     console.log("onaddstream: peer_id: " + peer_id);
     peer_media_elements[peer_id] = remote_media;
-
     remote_media.attr("id", peer_id + "Remote");
-    // $('#portfolio-wrapper').append('<div id="' + peer_id + 'remoteContainer" class="col-xs-12 col-sm-6 col-md-4 col-lg-3 portfolio-items" ><div id="' + peer_id + 'remoteVideoElement"></div><div class="details"><button id="fullscreenbtn2" class="btn fa fa-expand" style="float:left;  margin-top: 10px; margin-left: 10px;"></button><h4>' + config.userName + '</h4><i style="display:none; float:right;color: #555555e3; margin-top: -15px; margin-right: 10px;" id="closeThisConn' + peer_id + '" class="fa fa-window-close cancelColrChange" aria-hidden="true" id="closeThisConn' + peer_id + '" owner=' + peer_id + ' name=' + config.userName + '></i><span>All is well</span></div></div>');
-    // $('#' + peer_id + 'remoteVideoElement').append(remote_media);
     $("#portfolio-wrapper").append(
-      '<div id="' +
-      peer_id +
-      'remoteContainer" class="portfolio-items col-xs-6 col-sm-6 col-md-4 col-lg-3" ><div id="' +
-      peer_id +
-      'remoteVideoElement"></div><div class="details"><button id="' +
-      peer_id +
-      'fullscreenbtn2" class="btn fa fa-expand" style="float:left;  margin-top: 10px; margin-left: 10px;"></button><h4>' +
-      config.userName +
-      '</h4><i style="display:none; float:right;color: #555555e3; margin-top: -15px; margin-right: 10px;" id="closeThisConn' +
-      peer_id +
-      '" class="fa fa-window-close cancelColrChange" aria-hidden="true" id="closeThisConn' +
-      peer_id +
-      '" owner=' +
-      peer_id +
-      " name=" +
-      config.userName +
-      "></i><span>All is well</span></div></div>"
-    );
+      '<div id="' +peer_id +'remoteContainer" class="portfolio-items col-xs-6 col-sm-6 col-md-4 col-lg-3" ><div id="' +
+      peer_id +'remoteVideoElement"></div><div class="details"><button id="' +
+      peer_id +'fullscreenbtn2" class="btn fa fa-expand" style="float:left;  margin-top: 10px; margin-left: 10px;"></button><h4>' +
+      config.userName + '</h4><i style="display:none; float:right;color: #555555e3; margin-top: -15px; margin-right: 10px;" id="closeThisConn' +
+      peer_id +'" class="fa fa-window-close cancelColrChange" aria-hidden="true" id="closeThisConn' +
+      peer_id +'" owner=' +peer_id +" name=" +config.userName +"></i><span>All is well</span></div></div>");
     $("#" + peer_id + "remoteVideoElement").append(remote_media);
-
-    peer_userName_elements[peer_id] = document.getElementById(
-      "" + peer_id + "remoteContainer"
-    );
-
+    peer_userName_elements[peer_id] = document.getElementById("" + peer_id + "remoteContainer");
+    $("#"+peer_id+ "Remote").on('loadstart', function (event) {
+      $(this).addClass('background');
+      $(this).attr("poster", "/Preloader_2.gif");
+    });
+    
+    $("#"+peer_id+ "Remote").on('canplay', function (event) {
+      $(this).removeClass('background');
+      $(this).removeAttr("poster");
+    });
     if (peerNew_id == sessionHeader) {
       document.getElementById("closeThisConn" + peer_id).style.display =
         "inline";
@@ -998,12 +984,12 @@ function setup_local_media(callback, errorback) {
         $(this).addClass('background');
         $(this).attr("poster", "/Preloader_2.gif");
       });
-      
+
       $('#videoElem').on('canplay', function (event) {
         $(this).removeClass('background');
         $(this).removeAttr("poster");
       });
-      
+
 
       document.getElementById("videoElem").addEventListener("click", function () {
         console.log("screem size change request-->");
@@ -1097,8 +1083,8 @@ function setup_local_media(callback, errorback) {
 
           //local_media_stream = stream;
           local_media_shareStream = stream;
-          var local_mediaScreenShare = USE_VIDEO ?$("<video>") : $("<audio>");
-          local_mediaScreenShare.prop("muted",true); /* always mute ourselves by default */
+          var local_mediaScreenShare = USE_VIDEO ? $("<video>") : $("<audio>");
+          local_mediaScreenShare.prop("muted", true); /* always mute ourselves by default */
           local_mediaScreenShare.attr("id", "screenShareElem");
           local_mediaScreenShare.attr("autoplay", "true");
           local_mediaScreenShare.attr("style", "border:1px solid skyblue");

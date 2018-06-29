3028d6426787d364ff7cd81be0014dc7b8587a95
LogeswariGuru <logeswari.careator@gmail.com>
2018-06-29 14:26:01 +0530

    small change on html

--- public/js/csigsocket.js
+++ public/js/csigsocket.js
@@ -273,6 +273,7 @@ function disconnecSession() {
       deleteSessionId: queryLink,
       owner: peerNew_id
     });
+    window.location.href = "https://norecruits.com";
   } else {
     localStorage.removeItem("careatorEmail");
     localStorage.removeItem("sessionUrlId");

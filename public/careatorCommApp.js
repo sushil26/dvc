b2d54c619f9404f0eada47dea577b4c312ea3d5b
LogeswariGuru <logeswari.careator@gmail.com>
2018-06-30 15:21:50 +0530

    careatorApp Folder created

--- public/careatorCommApp.js
+++ public/careatorCommApp.js
@@ -1,4 +1,4 @@
-var careatorApp = angular.module('careatorApp', ['ui.router', 'ui.bootstrap', 'ngAnimate']);
+var careatorApp = angular.module('careatorApp', ['ui.router']);
 
 careatorApp.config(function ($stateProvider, $urlRouterProvider) {
 

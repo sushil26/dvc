2e6df98ef12d7ddb1c3d8e0e76a6b0fec0847efd
LogeswariGuru <logeswari.careator@gmail.com>
2018-06-30 16:04:09 +0530

    issue fixing

--- public/careatorCommApp.js
+++ public/careatorCommApp.js
@@ -7,49 +7,49 @@ careatorApp.config(function ($stateProvider, $urlRouterProvider) {
         url: careator_dashboard(),
         templateUrl: '/careatorApp/html/careator_dashboard.html',
         controller: 'careator_dashboardCtrl',
-        resolve: {
-            result: function (sessionAuthFactory, $window) {
-                var userData = sessionAuthFactory.getAccess("userData");
-                if (userData.loginType == 'admin') {
+        // resolve: {
+        //     result: function (sessionAuthFactory, $window) {
+        //         var userData = sessionAuthFactory.getAccess("userData");
+        //         if (userData.loginType == 'admin') {
 
-                }
-                else {
-                    $window.location.href = 'https://norecruits.com';
-                }
-            }
-        }
+        //         }
+        //         else {
+        //             $window.location.href = 'https://norecruits.com';
+        //         }
+        //     }
+        // }
     })
         .state('careatorApp.userCreate', {
             url: careator_userCreate(),
             templateUrl: '/html/careatorApp/careatorHTML/createUsers.html',
             controller: 'createUsersCtrl',
-            resolve: {
-                result: function (sessionAuthFactory, $window) {
-                    var userData = sessionAuthFactory.getAccess("userData");
-                    if (userData.loginType == 'admin') {
+            // resolve: {
+            //     result: function (sessionAuthFactory, $window) {
+            //         var userData = sessionAuthFactory.getAccess("userData");
+            //         if (userData.loginType == 'admin') {
 
-                    }
-                    else {
-                        $window.location.href = 'https://norecruits.com';
-                    }
-                }
-            }
+            //         }
+            //         else {
+            //             $window.location.href = 'https://norecruits.com';
+            //         }
+            //     }
+            // }
         })
         .state('careatorApp.chatHistory', {
             url: careator_chatHistory(),
             templateUrl: '/careatorApp/html/chatHistory.html',
             controller: 'chatHistoryCtrl',
-            resolve: {
-                result: function (sessionAuthFactory, $window) {
-                    var userData = sessionAuthFactory.getAccess("userData");
-                    if (userData.loginType == 'admin') {
+            // resolve: {
+            //     result: function (sessionAuthFactory, $window) {
+            //         var userData = sessionAuthFactory.getAccess("userData");
+            //         if (userData.loginType == 'admin') {
 
-                    }
-                    else {
-                        $window.location.href = 'https://norecruits.com';
-                    }
-                }
-            }
+            //         }
+            //         else {
+            //             $window.location.href = 'https://norecruits.com';
+            //         }
+            //     }
+            // }
         })
 })
 

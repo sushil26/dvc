7e1416113adc3d4257708c66aa8a9aa4d3f2643e
LogeswariGuru <logeswari.careator@gmail.com>
2018-06-30 15:54:16 +0530

    folder done for dashboard

--- public/careatorCommAppCtrl.js
+++ public/careatorCommAppCtrl.js
@@ -4,7 +4,7 @@ careatorApp.controller("careatorCommAppCtrl", function ($scope, $http, $timeout,
     //    httpFactory.getFile('property.json');
     //    console.log("$rootScope.propertyJson: "+JSON.stringify($rootScope.propertyJson));
 
-$scope.gotToDashboard() = function(){
+$scope.gotToDashboard = function(){
     console.log("gotToDashboard-->");
     $state.go('dashboard', {});
 

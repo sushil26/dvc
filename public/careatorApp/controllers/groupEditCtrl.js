f304147c10e4218cdf3fcdf1334d65e513ef5d65
shaiksajans <shaiksajans@gmail.com>
2018-07-02 17:48:38 +0530

    multiseletc

---
+++ public/careatorApp/controllers/groupEditCtrl.js
@@ -0,0 +1,2 @@
+careatorApp.controller('editGroupCtrl', function ($scope, $rootScope, $filter, $window, careatorHttpFactory) {
+    console.log("editGroupCtrl==>");})
---
+++ public/careatorApp/controllers/userEditCtrl.js
@@ -0,0 +1,3 @@
+careatorApp.controller('editUserCtrl', function ($scope, $rootScope, $filter, $window, careatorHttpFactory) {
+    console.log("editUserCtrl==>");
+})

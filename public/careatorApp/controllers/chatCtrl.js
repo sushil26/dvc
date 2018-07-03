32dd6295b10b50271ca9c3109db812cc7e1b423d
LogeswariGuru <logeswari.careator@gmail.com>
2018-07-03 17:37:22 +0530

    navigation to chat

--- public/careatorApp/controllers/chatCtrl.js
+++ public/careatorApp/controllers/chatCtrl.js
@@ -1,6 +1,40 @@
 careatorApp.controller('chatCtrl', function ($scope, $rootScope, $filter, $window) {
     console.log("chatCtrl==>");
+    var userData = careatorSessionAuth.getAccess("userData");
+    console.log("userData: " + JSON.stringify(userData));
+ 
+    $scope.getChatGroupListById = function (id) {
+        console.log("getAllEmployee-->: "+id);
+        var api = "https://norecruits.com/careator_chatGroupList/careator_getChatGroupListById/"+id;
+        console.log("api: " + api);
+        careatorHttpFactory.get(api).then(function (data) {
+            console.log("data--" + JSON.stringify(data.data));
+            var checkStatus = careatorHttpFactory.dataValidation(data);
+            console.log("data--" + JSON.stringify(data.data));
+            if (checkStatus) {
+                $scope.allGroup = data.data.data;
+                console.log("allemployee: " + JSON.stringify($scope.allGroup));
+                console.log(data.data.message);
+            }
+            else {
+                console.log("Sorry");
+                console.log(data.data.message);
+            }
+        })
+        console.log("<--getAllEmployee");
+    }
 
+    if(userData.chatRights=='yes'){
+        $scope.getChatGroupListById(localStorage.getItem("userId"));
+    }
+
+
+
+
+
+
+
+    /* ### Start: Front end  CSS### */
     $(".heading-compose").click(function () {
         $(".side-two").css({
             "left": "0"
@@ -12,4 +46,5 @@ careatorApp.controller('chatCtrl', function ($scope, $rootScope, $filter, $windo
             "left": "-100%"
         });
     });
+    /* ### End: Front end CSS ### */
 })

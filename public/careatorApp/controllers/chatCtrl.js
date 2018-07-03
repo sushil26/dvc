192d2ad7fda236e7fa003b4557398f9e0b323939
shaiksajans <shaiksajans@gmail.com>
2018-07-03 11:25:48 +0530

    multiseletc

--- public/careatorApp/controllers/chatCtrl.js
+++ public/careatorApp/controllers/chatCtrl.js
@@ -1,3 +1,15 @@
 careatorApp.controller('chatCtrl', function ($scope, $rootScope, $filter, $window) {
     console.log("chatCtrl==>");
+
+    $(".heading-compose").click(function () {
+        $(".side-two").css({
+            "left": "0"
+        });
+    });
+
+    $(".newMessage-back").click(function () {
+        $(".side-two").css({
+            "left": "-100%"
+        });
+    });
 })

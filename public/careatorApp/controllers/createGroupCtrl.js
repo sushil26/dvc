c8202684799612490b460ca15c3d2d752277b0c4
LogeswariGuru <logeswari.careator@gmail.com>
2018-07-03 10:39:21 +0530

    groupmember setting

--- public/careatorApp/controllers/createGroupCtrl.js
+++ public/careatorApp/controllers/createGroupCtrl.js
@@ -22,15 +22,7 @@ careatorApp.controller('createGroupCtrl', function ($scope, $rootScope, $filter,
     };
     console.log("$scope.groupMemberModel: " + JSON.stringify($scope.groupMemberModel));
     $scope.groupAdminData = $scope.groupMemberModel;
-    $scope.selectedGroupMembers = [];
-    angular.forEach($scope.groupMemberModel, function(value, key) {
-      if(key == 'label'){
-        this.push(value);
-      }
-        
-      }, $scope.selectedGroupMembers);
-    //$scope.groupAdminData = $scope.groupMemberModel;
-
+     
     $scope.rightEmployeeList = function (value) {
         console.log("rightEmployeeList-->");
         console.log("value: " + value);

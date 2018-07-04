612b9936254765ade062c19d90cb3fbb1980684b
shaiksajans <shaiksajans@gmail.com>
2018-07-04 11:22:04 +0530

    multiseletc

--- public/careatorApp/controllers/careator_dashboardCtrl.js
+++ public/careatorApp/controllers/careator_dashboardCtrl.js
@@ -1,8 +1,8 @@
 careatorApp.controller('careator_dashboardCtrl', function ($scope, $rootScope, $filter, careatorSessionAuth, careatorHttpFactory) {
     console.log("careator_dashboardCtrl==>");
     $scope.getChatGroupListById = function (id) {
-        console.log("getAllEmployee-->: "+id);
-        var api = "https://norecruits.com/careator_chatGroupList/careator_getChatGroupListById/"+id;
+        console.log("getAllEmployee-->: " + id);
+        var api = "https://norecruits.com/careator_chatGroupList/careator_getChatGroupListById/" + id;
         console.log("api: " + api);
         careatorHttpFactory.get(api).then(function (data) {
             console.log("data--" + JSON.stringify(data.data));
@@ -12,8 +12,7 @@ careatorApp.controller('careator_dashboardCtrl', function ($scope, $rootScope, $
                 $scope.allemployee = data.data.data;
                 console.log("allemployee: " + JSON.stringify($scope.allemployee));
                 console.log(data.data.message);
-            }
-            else {
+            } else {
                 console.log("Sorry");
                 console.log(data.data.message);
             }
@@ -33,24 +32,31 @@ careatorApp.controller('careator_dashboardCtrl', function ($scope, $rootScope, $
         userData.chatRights = "yes";
         $scope.getChatGroupListById(localStorage.getItem("userId"));
     }
- 
+
     careatorSessionAuth.setAccess(userData);
     var userData = careatorSessionAuth.getAccess("userData");
-    console.log("userData: "+JSON.stringify(userData));
+    console.log("userData: " + JSON.stringify(userData));
 
 
-///////////////Hamburger/////////////////////////
-$('#nav-icon1,#nav-icon2,#nav-icon3,#nav-icon4').click(function(){
-    $(this).toggleClass('open');
-});
+    ///////////////Hamburger/////////////////////////
+    $('#nav-icon1,#nav-icon2,#nav-icon3,#nav-icon4').click(function () {
+        $(this).toggleClass('open');
+    });
 
-//////////////toggle//////////////////////////////
-$('.icon_menu').click(function(){
+    //////////////toggle//////////////////////////////
+    $('.icon_menu').click(function () {
 
-    $('#tog').removeClass("icon_menu");
-    $('#tog').addClass("fa fa-plus");
-  
-});
+        $('#tog').removeClass("icon_menu");
+        $('#tog').addClass("fa fa-plus kkkllkl");
+
+    });
+    $('.kkkllkl').click(function () {
+        $('#tog').removeClass("fa fa-plus");
+        $('#tog').removeClass("kkkllkl");
+        $('#tog').addClass("icon_menu");
+
+
+    });
 
 
 

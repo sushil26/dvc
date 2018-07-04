3b775fe920a5246f2b739d436c902bed3a67cb1b
LogeswariGuru <logeswari.careator@gmail.com>
2018-07-04 16:51:59 +0530

    csigsocket.js sm

--- public/careatorApp/controllers/userEditCtrl.js
+++ public/careatorApp/controllers/userEditCtrl.js
@@ -2,9 +2,11 @@ careatorApp.controller('editUserCtrl', function ($scope, $state, $rootScope, $fi
     console.log("editUserCtrl==>");
     console.log("id: " + $state.params.id);
     var id = $state.params.id;
-    $scope.getAllEmployee = function () {
-        console.log("getAllEmployee-->");
-        var api = "https://norecruits.com/careator/careator_getAllEmp";
+   
+    $scope.getUser = function () {
+        console.log("getUser-->");
+
+        var api = "https://norecruits.com/careator_getUser/careator_getUserById/" + id;
         console.log("api: " + api);
         careatorHttpFactory.get(api).then(function (data) {
             console.log("data--" + JSON.stringify(data.data));
@@ -20,27 +22,8 @@ careatorApp.controller('editUserCtrl', function ($scope, $state, $rootScope, $fi
                 console.log(data.data.message);
             }
         })
-        console.log("<--getAllEmployee");
-    }
 
-    $scope.getAllEmployee();
-    $scope.getUser = function () {
-        console.log("getUser-->");
-
-        var api = "https://norecruits.com/careator_getUser/careator_getUserById/" + id;
-        console.log("api: " + api);
-        careatorHttpFactory.get(api).then(function (data) {
-            console.log("data--" + JSON.stringify(data.data));
-            var checkStatus = careatorHttpFactory.dataValidation(data);
-            if (checkStatus) {
-                $scope.userData = data.data.data;
-                console.log("userData: " + JSON.stringify($scope.userData));
-                console.log(data.data.message);
-            } else {
-                console.log("Sorry");
-                console.log(data.data.message);
-            }
-        })
+       
         console.log("<--getAllEmployee");
     }
     $scope.getUser();

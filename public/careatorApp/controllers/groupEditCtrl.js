d00c40db6765a90f65a3b3d87de712a1d148fb2e
shaiksajans <shaiksajans@gmail.com>
2018-07-05 11:53:24 +0530

    user edit mapping

--- public/careatorApp/controllers/groupEditCtrl.js
+++ public/careatorApp/controllers/groupEditCtrl.js
@@ -2,6 +2,7 @@ careatorApp.controller('editGroupCtrl', function ($scope,$state, $rootScope, $fi
     console.log("editGroupCtrl==>");
     console.log("id: " + $state.params.id);
     var id = $state.params.id;
+
     $scope.getGroup = function () {
         console.log("getGroup-->");
         var api = "https://norecruits.com/careator_getGroup/careator_getGroupById/" + id;
@@ -11,10 +12,6 @@ careatorApp.controller('editGroupCtrl', function ($scope,$state, $rootScope, $fi
             var checkStatus = careatorHttpFactory.dataValidation(data);
             if (checkStatus) {
                 $scope.userData = data.data.data;
-                $scope.userDataRights = {
-                    "videoRights": $scope.userData.videoRights,
-                    "chatRights": $scope.userData.chatRights
-                }
                 console.log("userData: " + JSON.stringify($scope.userData));
                 console.log(data.data.message);
             }
@@ -26,4 +23,13 @@ careatorApp.controller('editGroupCtrl', function ($scope,$state, $rootScope, $fi
         console.log("<--getAllEmployee");
     }
     $scope.getGroup();
+
+    $scope.groupMemberSettings = {
+        scrollableHeight: '200px',
+        scrollable: true,
+        enableSearch: true,
+        externalIdProp: ''
+    };
+    $scope.groupMemberData = [];
+    $scope.groupMemberModel = [];
 })

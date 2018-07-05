e79b641ba4ec408b022af8cdd23b61ec2a00fc62
LogeswariGuru <logeswari.careator@gmail.com>
2018-07-05 09:02:02 +0530

    allChatRecords

--- config/router.js
+++ config/router.js
@@ -30,16 +30,16 @@ module.exports = function (app) {
     // app.post('/careator/groupEditById', careator.groupEditById);
 
 
-    app.get('/careator_getUser/careator_getUserById/:id', careator.careator_getUserById);
-    app.get('/careator_getGroup/careator_getGroupById/:id', careator.careator_getGroupById);
+    // app.get('/careator_getUser/careator_getUserById/:id', careator.careator_getUserById);
+    // app.get('/careator_getGroup/careator_getGroupById/:id', careator.careator_getGroupById);
 
 
-    // app.get('/careator/getChatRights_emp', careator.getChatRights_emp);
-    // app.get('/careator/getVideoRights_emp', careator.getVideoRights_emp);
-    // app.get('/careator/careator_getChatVideo_emp', careator.careator_getChatVideo_emp);
-    // app.post('/careator/careator_chat_creteGroup', careator.careator_chat_creteGroup);
-    // app.post('/careator/careator_video_creteGroup', careator.careator_video_creteGroup);
-    // app.post('/careator/careator_chatVideo_creteGroup', careator.careator_chatVideo_creteGroup);
+    app.get('/careator/getChatRights_emp', careator.getChatRights_emp);
+    app.get('/careator/getVideoRights_emp', careator.getVideoRights_emp);
+    app.get('/careator/careator_getChatVideo_emp', careator.careator_getChatVideo_emp);
+    app.post('/careator/careator_chat_creteGroup', careator.careator_chat_creteGroup);
+    app.post('/careator/careator_video_creteGroup', careator.careator_video_creteGroup);
+    app.post('/careator/careator_chatVideo_creteGroup', careator.careator_chatVideo_creteGroup);
     app.get('/careator_chatGroupList/careator_getChatGroupListById/:id', careator.careator_getChatGroupListById);
     app.get('/careator_chatGroupList/careator_getChatGroupList', careator.careator_getChatGroupList);
     app.get('/careator_getEmp/careator_getChatRightsAllemp/:id', careator.careator_getChatRightsAllemp);

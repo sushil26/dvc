1efa2c2d276a4397f8d26313f152d2568fb84b67
LogeswariGuru <logeswari.careator@gmail.com>
2018-07-06 15:30:09 +0530

    updated group

--- public/careatorApp/controllers/chatCtrl.js
+++ public/careatorApp/controllers/chatCtrl.js
@@ -228,8 +228,20 @@ careatorApp.controller('chatCtrl', function ($scope, $rootScope, $filter, $windo
     /* ### Start: receive message from careator.js  ### */ //update to client with new message;
     socket.on('comm_textReceived', function (data) {
         console.log("****comm_textReceived-->: " + JSON.stringify(data));;
-        console.log("$scope.allChat._id: " + $scope.allChat._id + " data.id: " + data.id);
+        console.log("$scope.allChat._id: " + $scope.allChat._id);
+        console.log(" data.id: " + data.id);
         if ($scope.allChat._id == data.id) {
+            console.log("start pushing message");
+            $scope.allChat.chats.push({
+                "senderId": data.senderId,
+                "senderName": data.senderName,
+                "message": data.message,
+                "sendTime": data.sendTime
+            });
+            $scope.scrollDown();
+        }
+
+        if ($scope.individualData._id == data.id) {
             console.log("start pushing message");
             $scope.allChat.chats.push({
                 "senderId": data.senderId,

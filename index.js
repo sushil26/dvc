1e6b9b69cb5569fb90de59bed54d6265157fb2d7
LogeswariGuru <logeswari.careator@gmail.com>
2018-07-09 12:27:14 +0530

    db called in index.js

--- index.js
+++ index.js
@@ -351,26 +351,26 @@ io.sockets.on('connection', function (socket) {
             //console.log("timeLink: "+timeLink);
             console.log("peerWithQueryId: " + peerWithQueryId[data.userId]);
             console.log("peerWithQueryId: " + peerWithQueryId[data.userId]);
-            var queryObj = {
-                "url": "https://norecruits.com/careator/" + peerWithQueryId[data.userId] + "/" + data.urlDate,
-            }
-            console.log("queryObj: " + JSON.stringify(queryObj));
-            var obj = {
-                "email": data.email,
-                'message': data.message,
-                'userName': data.userName,
-                'textTime': date
-            }
-            console.log("obj: " + JSON.stringify(obj));
-            console.log("chatHistory: "+JSON.stringify(chatHistory));
-            chatHistory.update(queryObj, { $push: { "chat": obj } }, function (err, data) {
-                if (err) {
-                    console.log("errr: " + JSON.stringify(err));
-                }
-                else {
-                    console.log("data: " + JSON.stringify(data));
-                }
-            })
+            // var queryObj = {
+            //     "url": "https://norecruits.com/careator/" + peerWithQueryId[data.userId] + "/" + data.urlDate,
+            // }
+            // console.log("queryObj: " + JSON.stringify(queryObj));
+            // var obj = {
+            //     "email": data.email,
+            //     'message': data.message,
+            //     'userName': data.userName,
+            //     'textTime': date
+            // }
+            // console.log("obj: " + JSON.stringify(obj));
+            // console.log("chatHistory: "+JSON.stringify(chatHistory));
+            // chatHistory.update(queryObj, { $push: { "chat": obj } }, function (err, data) {
+            //     if (err) {
+            //         console.log("errr: " + JSON.stringify(err));
+            //     }
+            //     else {
+            //         console.log("data: " + JSON.stringify(data));
+            //     }
+            // })
             io.sockets.emit('newTextMsg', { 'message': data.message, 'userId': data.userId, 'queryId': peerWithQueryId[data.userId], 'time': peerWithTimeId[data.userId], 'userName': data.userName });
 
             // io.sockets.emit('userDetail', {'userId': data.userId,'userName': data.userName });

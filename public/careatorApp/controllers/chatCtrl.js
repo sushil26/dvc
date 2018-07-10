cc07f70939ccf52b3df1b08cf4fc9edbefcc1045
LogeswariGuru <logeswari.careator@gmail.com>
2018-07-10 11:10:49 +0530

    opacity changed into none

--- public/careatorApp/controllers/chatCtrl.js
+++ public/careatorApp/controllers/chatCtrl.js
@@ -35,7 +35,7 @@ careatorApp.controller('chatCtrl', function ($scope, $rootScope, $filter, $windo
           //  $("#bcktohome").click();
             console.log("chatMenu-->");
             $('.side-one').css({
-                "display": " "
+                "display": "block"
             });
             $('.conversation').css({
                 "position": " ",

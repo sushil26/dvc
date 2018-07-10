4af6cea2dd80d7b81e08a02d059596f3ce738910
LogeswariGuru <logeswari.careator@gmail.com>
2018-07-10 15:30:02 +0530

    groupapi sc

--- config/controllers/careator.js
+++ config/controllers/careator.js
@@ -1142,10 +1142,10 @@ module.exports.groupText = function (req, res) {
                             var io = req.app.get('socketio');
                             io.emit('comm_textReceived', {
                                 "id": insertedData.ops[0]._id,
-                                "senderId": obj.senderId,
-                                "senderName": obj.senderName,
-                                "message": obj.message,
-                                "sendTime": obj.sendTime
+                                "senderId": obj.chats[0].senderId,
+                                "senderName": obj.chats[0].senderName,
+                                "message": obj.chats[0].message,
+                                "sendTime": obj.chats[0].sendTime
                             }); /* ### Note: Emit message to client ### */
                             response = {
                                 status: true,

33a2e96e9c727f4fa9d02b4ad8641ef1174b5a8e
LogeswariGuru <logeswari.careator@gmail.com>
2018-07-11 17:17:26 +0530

    select and unselect list

--- config/controllers/careator.js
+++ config/controllers/careator.js
@@ -380,7 +380,8 @@ module.exports.careatorMasterInsertValidate = function (data, callback) {
         "email": data.Email,
         "videoRights": data.VideoRights,
         "chatRights": data.ChatRights,
-        "status": "active"
+        "status": "active",
+        "restrictedTo" : []
     }
     careatorMaster.find(findEmpId).toArray(function (err, findData) {
         if (err) {
@@ -1683,6 +1684,56 @@ module.exports.removeRestrictedUserById = function (req, res) {
                 console.log("restrict: " + JSON.stringify(restrict));
                 var io = req.app.get('socketio');
                 io.emit('comm_aboutRestrictedRemoveUpdate', {
+                    "id": id,
+                    "restrictedTo": req.body.restrictedTo
+                }); /* ### Note: Emit message to user about their new restricted user ### */
+                response = {
+                    status: true,
+                    message: "Successfull",
+                    data: restrict
+                };
+                res.status(200).send(response);
+            }
+        })
+    } else {
+        console.log("Epty value found");
+        response = {
+            status: false,
+            message: "empty value found"
+        };
+        res.status(400).send(response);
+    }
+
+}
+
+module.exports.restrictedToSave = function (req, res) {
+    console.log("restrictedToSave-->");
+    var response;
+    var id = req.params.id;
+    console.log("req.body.restrictedTo: " + JSON.stringify(req.body.restrictedTo));
+    if (general.emptyCheck(id)) {
+        var objFind = {
+            "_id": ObjectId(id)
+        }
+        var objUpdate = {
+            "restrictedTo": req.body.restrictedTo
+        };
+
+        console.log("objFind: " + JSON.stringify(objFind));
+        console.log("objUpdate: " + JSON.stringify(objUpdate));
+        careatorMaster.update(objFind, { $set: { "restrictedTo": req.body.restrictedTo } }, function (err, restrict) {
+            if (err) {
+                console.log("err: " + JSON.stringify(err));
+                response = {
+                    status: false,
+                    message: "Unsuccessfull",
+                    data: err
+                };
+                res.status(400).send(response);
+            } else {
+                console.log("restrict: " + JSON.stringify(restrict));
+                var io = req.app.get('socketio');
+                io.emit('comm_aboutRestrictedUpdate', {
                     "id": id,
                     "restrictedTo": req.body.restrictedTo
                 }); /* ### Note: Emit message to user about their new restricted user ### */

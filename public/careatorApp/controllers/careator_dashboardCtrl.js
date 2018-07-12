49e215ec40ea99ec1987ebe3e354184189140e53
LogeswariGuru <logeswari.careator@gmail.com>
2018-07-12 12:45:25 +0530

    careator_getChatRightsAllemp

--- public/careatorApp/controllers/careator_dashboardCtrl.js
+++ public/careatorApp/controllers/careator_dashboardCtrl.js
@@ -15,6 +15,7 @@ careatorApp.controller('careator_dashboardCtrl', function ($scope, $rootScope, $
     $timeout(tick, $scope.tickInterval);
 
     var userData = careatorSessionAuth.getAccess("userData");
+    console.log("userData==>: "+userData);
     if (userData == undefined) {
 
         var userData = {

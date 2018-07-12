f13ca836e31492772e3baba224a1532548291454
LogeswariGuru <logeswari.careator@gmail.com>
2018-07-12 11:50:39 +0530

    restricted user list session update

--- public/careatorApp/controllers/chatCtrl.js
+++ public/careatorApp/controllers/chatCtrl.js
@@ -468,7 +468,7 @@ careatorApp.controller('chatCtrl', function ($scope, $rootScope, $filter, $windo
                 if (checkStatus) {
                     $scope.getUserById = data.data.data[0];
                     console.log("getUserById: " + JSON.stringify($scope.getUserById));
-                    console.log("$scope.getUserById.restrictedTo: "+JSON.restrictedTo($scope.getUserById.restrictedTo));
+                    console.log("$scope.getUserById.restrictedTo: "+JSON.stringify($scope.getUserById.restrictedTo));
                     userData.restrictedTo = $scope.getUserById.restrictedTo;
                     careatorSessionAuth.setAccess(userData.restrictedTo);
                     var userData = careatorSessionAuth.getAccess("userData");

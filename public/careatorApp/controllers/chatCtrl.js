b3e2b90957aa6fbd6a9470cb066ffd0e30036300
LogeswariGuru <logeswari.careator@gmail.com>
2018-07-12 12:48:51 +0530

    careator_getChatRightsAllemp

--- public/careatorApp/controllers/chatCtrl.js
+++ public/careatorApp/controllers/chatCtrl.js
@@ -494,9 +494,9 @@ careatorApp.controller('chatCtrl', function ($scope, $rootScope, $filter, $windo
                     }
 
                     console.log("userData.restrictedTo: " + JSON.stringify(userData.restrictedTo));
-                    careatorSessionAuth.setAccess(userData.restrictedTo);
-                    careatorSessionAuth.setAccess(userData);
+                                       
                     var userData = careatorSessionAuth.clearAccess("userData");
+                    careatorSessionAuth.setAccess(userData);
                     var userData = careatorSessionAuth.getAccess("userData");
                     console.log("***userData: " + JSON.stringify(userData));
                     $scope.getAllChatRightEmp();

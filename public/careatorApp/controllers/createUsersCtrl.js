f2e8d4c0c4dea423a9514a4ad17eee308d1a82fa
LogeswariGuru <logeswari.careator@gmail.com>
2018-07-13 16:36:22 +0530

    userData sc

--- public/careatorApp/controllers/createUsersCtrl.js
+++ public/careatorApp/controllers/createUsersCtrl.js
@@ -1,15 +1,15 @@
-careatorApp.controller('createUsersCtrl', function ($scope, $rootScope,$state,careatorHttpFactory) {
+careatorApp.controller('createUsersCtrl', function ($scope, $rootScope, $state, careatorHttpFactory) {
     console.log("createUsersCtrl==>");
     $scope.propertyJson = $rootScope.propertyJson;
-    $scope.uploadCareatorEmp = function(careatorEmp){
+    $scope.uploadCareatorEmp = function (careatorEmp) {
         console.log("uploadCareatorEmp-->");
-        
+
         var obj = {
             "file": careatorEmp
-          }
-          var api = "https://norecruits.com/careator/careatorMasterInsert";
-          console.log("api: "+api);
-          careatorHttpFactory.csvUpload(obj, api).then(function (data) {
+        }
+        var api = "https://norecruits.com/careator/careatorMasterInsert";
+        console.log("api: " + api);
+        careatorHttpFactory.csvUpload(obj, api).then(function (data) {
             var checkStatus = careatorHttpFactory.dataValidation(data);
             console.log("data--" + JSON.stringify(data.data));
             if (checkStatus) {
@@ -24,7 +24,14 @@ careatorApp.controller('createUsersCtrl', function ($scope, $rootScope,$state,ca
         })
         console.log("<--uploadCareatorEmp");
     }
-    $scope.careatorEmp = function(){
+    $scope.careatorEmp = function () {
         console.log("careatorEmp-->");
+        var obj = {
+            "userName": $scope.userName,
+            "empId": $scope.empId,
+            "empEmail": $scope.empEmail,
+            "rightSelect": $scope.rightSelect
+        }
+        console.log("obj: "+JSON.stringify(obj));
     }
 })

beb7415e762ad59b871eb3d058acf842a36543f3
LogeswariGuru <logeswari.careator@gmail.com>
2018-07-16 14:33:45 +0530

    maximize and minimize, mute and unmute added while share screen

--- config/router.js
+++ config/router.js
@@ -43,6 +43,7 @@ module.exports = function (app) {
     app.get('/careator_getChatsById/getChatsById/:id', careator.getChatsById);
     //app.post('/careator/careator_video_creteGroup', careator.careator_video_creteGroup);
     //app.post('/careator/careator_chatVideo_creteGroup', careator.careator_chatVideo_creteGroup);
+    app.post('/vc/login4VC', user.login4VC);
     app.get('/careator_chatGroupList/careator_getChatGroupListById/:id', careator.careator_getChatGroupListById);
     app.get('/careator_chatGroupList/careator_getChatGroupList', careator.careator_getChatGroupList);
     app.post('/careator_getEmp/careator_getChatRightsAllemp/:id', careator.careator_getChatRightsAllemp);
@@ -52,7 +53,7 @@ module.exports = function (app) {
     app.get('/careator_groupTextRead/groupTextReadByGroupId/:group_id', careator.groupTextReadByGroupId);
     app.post('/careator_groupText/groupText', careator.groupText);
     app.get('/careator_groupTextRead/groupTextReadById/:sId/:rId', careator.groupTextReadById);
-    app.post('/vc/login4VC', user.login4VC);
+   
 
     app.post('/record/pswdGenerate', record.pswdGenerate);
     app.post('/record/pswdCheck', record.pswdCheck);

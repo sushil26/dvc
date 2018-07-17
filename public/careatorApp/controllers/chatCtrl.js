881834d4bac7bdd127bee59ce2cc75a36c1ee4c8
shaiksajans <shaiksajans@gmail.com>
2018-07-17 21:46:01 +0530

    img replaced change

--- public/careatorApp/controllers/chatCtrl.js
+++ public/careatorApp/controllers/chatCtrl.js
@@ -766,38 +766,38 @@ careatorApp.controller('chatCtrl', function ($scope, $rootScope, $filter, $windo
 
 
     //////////emoji/////////////////////////////
-    $(document).ready(function () {
-        $("#comment").emojioneArea({
-            pickerPosition: "top",
-            tonesStyle: "bullet"
-        });
-        $("#emojionearea2").emojioneArea({
-            pickerPosition: "bottom",
-            tonesStyle: "radio"
-        });
-        $("#emojionearea3").emojioneArea({
-            pickerPosition: "right",
-            filtersPosition: "bottom",
-            tonesStyle: "square"
-        });
-        $("#emojionearea4").emojioneArea({
-            pickerPosition: "bottom",
-            filtersPosition: "bottom",
-            tonesStyle: "checkbox"
-        });
-        $("#emojionearea5").emojioneArea({
-            pickerPosition: "top",
-            filtersPosition: "bottom",
-            tones: false,
-            autocomplete: false,
-            inline: true,
-            hidePickerOnBlur: false
-        });
-        $("#standalone").emojioneArea({
-            standalone: true,
-            autocomplete: false
-        });
-    });
+    // $(document).ready(function () {
+    //     $("#comment").emojioneArea({
+    //         pickerPosition: "top",
+    //         tonesStyle: "bullet"
+    //     });
+    //     $("#emojionearea2").emojioneArea({
+    //         pickerPosition: "bottom",
+    //         tonesStyle: "radio"
+    //     });
+    //     $("#emojionearea3").emojioneArea({
+    //         pickerPosition: "right",
+    //         filtersPosition: "bottom",
+    //         tonesStyle: "square"
+    //     });
+    //     $("#emojionearea4").emojioneArea({
+    //         pickerPosition: "bottom",
+    //         filtersPosition: "bottom",
+    //         tonesStyle: "checkbox"
+    //     });
+    //     $("#emojionearea5").emojioneArea({
+    //         pickerPosition: "top",
+    //         filtersPosition: "bottom",
+    //         tones: false,
+    //         autocomplete: false,
+    //         inline: true,
+    //         hidePickerOnBlur: false
+    //     });
+    //     $("#standalone").emojioneArea({
+    //         standalone: true,
+    //         autocomplete: false
+    //     });
+    // });
 
 
 

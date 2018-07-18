6265d42651d7c636d4d02673ceb093eb2c92b8dd
shaiksajans <shaiksajans@gmail.com>
2018-07-18 08:42:30 +0530

    img replaced change

--- public/careatorApp/controllers/chatCtrl.js
+++ public/careatorApp/controllers/chatCtrl.js
@@ -394,7 +394,7 @@ careatorApp.controller('chatCtrl', function ($scope, $rootScope, $filter, $windo
     }
 
     $scope.sendText = function () {
-        $('.reply-main').val('');
+        $('.emojionearea-editor').val('');
         console.log("sendText-->");
         console.log("$scope.typedMessage: " + $scope.typedMessage);
         var api;

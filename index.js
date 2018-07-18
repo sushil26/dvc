710ce9a824bbd6a623893b49939394a28d640e2d
LogeswariGuru <logeswari.careator@gmail.com>
2018-07-18 16:24:02 +0530

    index.js check

--- index.js
+++ index.js
@@ -203,9 +203,11 @@ io.sockets.on('connection', function (socket) {
         console.log("data.deleteSessionId: " + data.deleteSessionId);
         console.log("sockets[data.deleteSessionId]: " + sockets.valueOf(data.deleteSessionId));
         console.log("peerTrackForVideo[data.deleteSessionId]: " + peerTrackForVideo[data.deleteSessionId]);
-        console.log(" channels['some-global-ch-name'][data.deleteSessionId]: " +  channels['some-global-ch-name'][data.deleteSessionId]);
+        console.log(" channels['some-global-ch-name'][data.deleteSessionId]: " +  channels[some-global-ch-name][data.deleteSessionId]);
+        console.log("   socket.channels[some-global-ch-name] : " +    socket.channels[some-global-ch-name] );
         //delete tempSock.channels[channel];
-       delete channels['some-global-ch-name'][data.deleteSessionId];
+       
+       delete channels[some-global-ch-name][data.deleteSessionId];
        delete  sockets[sockets.valueOf(data.deleteSessionId)];
         delete sockets[data.deleteSessionId];
         // delete sockets[tempSock.id];

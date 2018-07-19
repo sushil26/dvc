5452412a8fa9666a086332a87fdcb61d0a4da4e1
LogeswariGuru <logeswari.careator@gmail.com>
2018-07-19 20:31:58 +0530

    url restriction added

--- public/js/csigsocket.js
+++ public/js/csigsocket.js
@@ -290,6 +290,7 @@ function checkPassword() {
   console.log("<--checkPassword");
 }
 
+
 function chatNavigation() {
   console.log("chatNavigation-->");
   window.location.href = "https://norecruits.com/careatorApp/#!/dashboard/profile";
@@ -1744,6 +1745,22 @@ signaling_socket.on('comm_logoutNotifyToUserById', function (data) {
   // }
 
 })
+
+window.onbeforeunload = function (event) {
+  
+  if(stuff.length > 5){
+    var message = 'Sure you want to leave?';
+    if (typeof event == 'undefined') {
+      event = window.event;
+    }
+    if (event) {
+      event.returnValue = message;
+    }
+    console.log("message: "+message);
+    return message;
+  }
+
+}
 
 
 

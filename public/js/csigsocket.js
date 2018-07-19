3c23be1313e0c70b096c2e78736fd319a347982b
LogeswariGuru <logeswari.careator@gmail.com>
2018-07-19 15:18:23 +0530

    session url call

--- public/js/csigsocket.js
+++ public/js/csigsocket.js
@@ -1711,6 +1711,7 @@ $(".back-to-top").click(function () {
 signaling_socket.on('comm_logoutNotifyToUserById', function (data) {
   console.log("***comm_logoutNotifyToUserById-->: " + JSON.stringify(data));
   console.log("localStorage.getItem(careatorEmail): "+localStorage.getItem("careatorEmail"));
+  console.log(" localStorage.getItem(sessionUrlId): "+ localStorage.getItem("sessionUrlId"));
   if (data.email == localStorage.getItem("careatorEmail") || data.email == localStorage.getItem("careator_remoteEmail")) {
     console.log("Start to remove the session");
     // localStorage.removeItem("email");
@@ -1745,6 +1746,8 @@ signaling_socket.on('comm_logoutNotifyToUserById', function (data) {
   }
   if(data.sessionURL== localStorage.getItem("sessionUrlId"))
   {
+    console.log("Start to remove the session based on sessionURL");
+    localStorage.removeItem("sessionUrlId");
     signaling_socket.emit("disconnectSession", {
       deleteSessionId: queryLink,
       owner: peerNew_id

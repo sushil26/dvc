6e6ce62bb29705a43d6a216e0dcb6cd161780c2d
LogeswariGuru <logeswari.careator@gmail.com>
2018-07-22 14:56:16 +0530

    added some nw alert

--- public/js/csigsocket.js
+++ public/js/csigsocket.js
@@ -387,7 +387,7 @@ signaling_socket.on("disconnectSessionReply", function (data) {
   if (queryLink == data.deleteSessionId && peerNew_id == data.owner) {
     console.log("Ready for redirect-->");
     // localStorage.setItem("redirctRequired", true);
-    alert("Sorry your link is not alive");
+    //alert("Sorry your link is not alive");
     close();
     //window.location.href = "https://norecruits.com";
   } else if (queryLink == data.deleteSessionId && peerNew_id != data.owner) {
@@ -591,7 +591,8 @@ signaling_socket.on("connect", function () {
       }
       else {
         alert("Sorry your link is not alive");
-        window.location.href = "https://norecruits.com";
+        close();
+        // window.location.href = "https://norecruits.com";
       }
     }
 

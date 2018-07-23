56847153d47a0958d7930794f11f57e93a305994
LogeswariGuru <logeswari.careator@gmail.com>
2018-07-23 11:49:52 +0530

    disconnectsession reply to all

--- public/js/csigsocket.js
+++ public/js/csigsocket.js
@@ -388,7 +388,7 @@ signaling_socket.on("disconnectSessionReply", function (data) {
     console.log("Ready for redirect-->");
     // localStorage.setItem("redirctRequired", true);
     //alert("Sorry your link is not alive");
-    //close();
+    close();
     //window.location.href = "https://norecruits.com";
   } else if (queryLink == data.deleteSessionId && peerNew_id != data.owner) {
     console.log("remote notification that host disconnect the session-->");

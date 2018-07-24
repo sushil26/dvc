ddef041eed88c04aaedfe7b195291000201a1863
LogeswariGuru <logeswari.careator@gmail.com>
2018-07-24 11:33:30 +0530

    custom alert for you already loggedin

--- public/js/csigsocket.js
+++ public/js/csigsocket.js
@@ -286,10 +286,9 @@ function checkCredential() {
         console.log("err.responseJSON: " + JSON.stringify(err.responseJSON.message));
         if (err.responseJSON.message == 'You already logged in, please logout your old session in-order to login') {
           console.log("You already logged in, please logout your old session in-order to login");
+          alert(err.responseJSON.message);
           document.getElementById('notify_msg_content').innerHTML = err.responseJSON.message;
           $("#notify_msg_button").trigger("click");
-         // alert(err.responseJSON.message);
-
           window.location.href = "/";
         }
         else {

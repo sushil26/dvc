a2bc9925f74fcfe8cfe0d7ea49d03aa95dff361c
shaiksajans <shaiksajans@gmail.com>
2018-07-25 06:51:01 +0530

    24 july

--- config/controllers/careator.js
+++ config/controllers/careator.js
@@ -759,6 +759,7 @@ module.exports.careatorSingleUserInsert = function (req, res) {
         "empId": req.body.empId,
         "email": req.body.empEmail,
         "password": req.body.empPass,
+        "Designation":req.body.empPass,empDesignation,
         "sessionRandomId": sessionRandomId,
         "videoRights": req.body.videoRights,
         "chatRights": req.body.chatRights,

da625f08481b5e9036092bc819ed427b65ff24f1
LogeswariGuru <logeswari.careator@gmail.com>
2018-07-26 14:29:44 +0530

    text seen flag update

--- public/careatorApp/controllers/chatCtrl.js
+++ public/careatorApp/controllers/chatCtrl.js
@@ -689,7 +689,7 @@ careatorApp.controller("chatCtrl", function (
       $scope.getChatRecords();
     } else if (data.freshInsert == undefined) {
       console.log("$scope.individualData: " + JSON.stringify($scope.individualData));
-      if ($scope.individualData._id == data.id) {
+      if ($scope.individualData!=undefined && $scope.individualData._id == data.id) {
         console.log("2)start pushing message");
         var id = data.id;
         console.log("id: " + id);
@@ -715,7 +715,7 @@ careatorApp.controller("chatCtrl", function (
           sendTime: data.sendTime
         });
         $scope.scrollDown();
-      } else if ($scope.individualData._id != data.id) {
+      } else if ( $scope.individualData==undefined && $scope.individualData._id != data.id) {
         console.log("Notify the Unseen message count");
         if (data.senderId != userData.userId) {
           var index = $scope.allChatRecordsReceiverId.indexOf(data.id);

0c57863511d69102f254b4b72d52314de26a065b
shaiksajans <shaiksajans@gmail.com>
2018-07-26 15:01:56 +0530

    24 july

--- public/js/csigsocket.js
+++ public/js/csigsocket.js
@@ -444,23 +444,9 @@ function checkPassword() {
 
 
 
-
-
-
-var w;
 function chatNavigation() {
   console.log("chatNavigation-->");
-  // window.location.href = "https://norecruits.com/careatorApp/#!/dashboard/profile";
-  if (!w || w.closed) {
-    w = window.location.href("https://norecruits.com/careatorApp/#!/dashboard/profile");
-  } else {
-    console.log('Window is already opened');
-    // alert("window is already opened");
-    $("#closeConfirmationButton2").trigger("click");
-  }
-  function focust2() {
-    w.focus();
-  }
+  window.location.href = "https://norecruits.com/careatorApp/#!/dashboard/profile";
 }
 
 

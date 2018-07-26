c2d9f5b2a412f1003bc8df99cca5db5236e8a45a
shaiksajans <shaiksajans@gmail.com>
2018-07-26 08:07:08 +0530

    24 july

--- public/careatorApp/controllers/chatCtrl.js
+++ public/careatorApp/controllers/chatCtrl.js
@@ -768,6 +768,8 @@ careatorApp.controller("chatCtrl", function (
             userName: localStorage.getItem("userName"),
             empId: localStorage.getItem("empId"),
             userId: localStorage.getItem("userId"),
+            userPic: localStorage.getItem("profilePicPath"),
+
             restrictedTo: restrictedArray
           };
           if (localStorage.getItem("videoRights") == "yes") {

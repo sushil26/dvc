2aa4632308cca2242a94022a9d34e9c9ee658e67
LogeswariGuru <logeswari.careator@gmail.com>
2018-07-27 11:55:48 +0530

    group open that perticular person notification will not show

--- config/controllers/careator.js
+++ config/controllers/careator.js
@@ -1770,7 +1770,7 @@ module.exports.textSeenFlagUpdate_toGroupChat = function (req, res) {
             "userId": req.body.seenBy,
             "unseenCount": 0,
         }
-        console.log("obj: " + JSON.stringify(obj));
+        console.log("*obj: " + JSON.stringify(obj));
         careatorChat.update({ "group_id": req.params.group_id, "groupMembers.userId": req.body.seenBy }, { "$set": { "groupMembers.$.unseenCount": 0 } }, function (err, updateddata) {
             if (err) {
                 console.log("err: " + JSON.stringify(err));

33698860f697c54724bdc178521378cb223746b4
LogeswariGuru <logeswari.careator@gmail.com>
2018-07-27 15:51:39 +0530

    group open that perticular person notification will not show

--- public/careatorApp/controllers/chatCtrl.js
+++ public/careatorApp/controllers/chatCtrl.js
@@ -699,7 +699,7 @@ careatorApp.controller("chatCtrl", function (
       else {
         console.log("Need to notify");
         console.log("")
-        if (($scope.individualData != undefined && $scope.individualData._id != data.id) || ($scope.individualData == undefined)) {
+        if (($scope.individualData != undefined && $scope.individualData._id != data.id) || $scope.allGroupIds.indexOf(data.group_id)>=0) {
           console.log("UnseenCount added to group");
           var index = $scope.allGroupIds.indexOf(data.group_id);
           console.log("index: " + index);

f8ff1cfa066954ac68ef71f4420913f8a6d4c994
LogeswariGuru <logeswari.careator@gmail.com>
2018-07-28 14:28:07 +0530

    leave and join added

--- config/controllers/careator.js
+++ config/controllers/careator.js
@@ -38,17 +38,9 @@ module.exports.RemoteJoinCheck = function (req, res) {
             "password": password
         }
         console.log("obj: " + JSON.stringify(obj));
-        careatorMaster.find({
-            "sessionURL": url,
-            "invite": {
-                $elemMatch: {
-                    "remoteEmailId": remote_careatorEmail,
-                    "password": password
-                }
-            }
-        }).toArray(function (err, findData) {
-            console.log("findData: " + JSON.stringify(findData));
-            console.log("findData.length: " + findData.length);
+        careatorMaster.find({ "sessionURL": url }).toArray(function (err, sessionURLFindData) {
+            console.log("sessionURLFindData: " + JSON.stringify(sessionURLFindData));
+            console.log("sessionURLFindData.length: " + sessionURLFindData.length);
             if (err) {
                 responseData = {
                     status: false,
@@ -56,35 +48,56 @@ module.exports.RemoteJoinCheck = function (req, res) {
                 };
                 res.status(400).send(responseData);
             } else {
-                if (findData.length > 0) {
-                    var joinEmails = findData[0].joinEmails;
-                    console.log("joinEmails.indexOf({email:remote_careatorEmail}): " + joinEmails.indexOf({ "email": remote_careatorEmail }));
-                    if (joinEmails.indexOf({ "email": remote_careatorEmail })<0) {
-                        careatorMaster.update({ "sessionURL": url }, { $pull: { "leftEmails": { "email": remote_careatorEmail } }, $push: { "joinEmails": { "email": remote_careatorEmail } } }, function (err, data) {
-                            if (err) {
-                                responseData = {
-                                    status: false,
-                                    message: "Process failed"
-                                };
-                                res.status(400).send(responseData);
+                if (sessionURLFindData.length > 0) {
+                    careatorMaster.find({ "sessionURL": url, "invite": { $elemMatch: { "remoteEmailId": remote_careatorEmail, "password": password } } }).toArray(function (err, findData) {
+                        console.log("findData: " + JSON.stringify(findData));
+                        console.log("findData.length: " + findData.length);
+                        if (err) {
+                            responseData = {
+                                status: false,
+                                message: "Process failed"
+                            };
+                            res.status(400).send(responseData);
+                        } else {
+                            if (findData.length > 0) {
+                                var joinEmails = findData[0].joinEmails;
+                                console.log("joinEmails.indexOf({email:remote_careatorEmail}): " + joinEmails.indexOf({ "email": remote_careatorEmail }));
+                                if (joinEmails.indexOf({ "email": remote_careatorEmail }) < 0) {
+                                    careatorMaster.update({ "sessionURL": url }, { $pull: { "leftEmails": { "email": remote_careatorEmail } }, $push: { "joinEmails": { "email": remote_careatorEmail } } }, function (err, data) {
+                                        if (err) {
+                                            responseData = {
+                                                status: false,
+                                                message: "Process failed"
+                                            };
+                                            res.status(400).send(responseData);
+                                        } else {
+                                            responseData = {
+                                                status: true,
+                                                sessionData: "79ea520a-3e67-11e8-9679-97fa7aeb8e97",
+                                                message: "Login Successfully"
+                                            };
+                                            res.status(200).send(responseData);
+                                        }
+                                    })
+                                }
+                                else {
+                                    responseData = {
+                                        status: false,
+                                        message: "Sorry tusing his credential already user participating in confeence"
+                                    };
+                                    res.status(400).send(responseData);
+                                }
                             } else {
                                 responseData = {
-                                    status: true,
-                                    sessionData: "79ea520a-3e67-11e8-9679-97fa7aeb8e97",
-                                    message: "Login Successfully"
+                                    status: false,
+                                    message: "Credential Mismatch"
                                 };
-                                res.status(200).send(responseData);
+                                res.status(400).send(responseData);
                             }
-                        })
-                    }
-                    else{
-                        responseData = {
-                            status: false,
-                            message: "Sorry tusing his credential already user participating in confeence"
-                        };
-                        res.status(400).send(responseData);
-                    }
-                } else {
+                        }
+                    })
+                }
+                else {
                     responseData = {
                         status: false,
                         message: "Credential Mismatch Or Your URL not alive"
@@ -93,7 +106,8 @@ module.exports.RemoteJoinCheck = function (req, res) {
                 }
             }
         })
-    } else {
+    }
+    else {
         responseData = {
             status: false,
             message: "Empty value found"

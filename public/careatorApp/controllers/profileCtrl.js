a3e86cf6b1a9ed456f15b8216b83c30766013af6
shaiksajans <shaiksajans@gmail.com>
2018-07-30 14:48:27 +0530

    24 july

--- public/careatorApp/controllers/profileCtrl.js
+++ public/careatorApp/controllers/profileCtrl.js
@@ -156,37 +156,4 @@ careatorApp.controller('profileCtrl', function ($scope, $state, careatorHttpFact
     }
 
 
-
-    $scope.speed = function () {
-        console.log("speeeeeeeeeeeeed");
-        var imageAddr = "./css/test.jpg" + "?n=" + Math.random();
-        var startTime, endTime;
-        var downloadSize = 191*1024;
-        var download = new Image();
-        download.onload = function () {
-            endTime = (new Date()).getTime();
-            showResults();
-        }
-        startTime = (new Date()).getTime();
-        download.src = imageAddr;
-   
-
-        function showResults() {
-            var duration = (endTime - startTime) / 1000;
-            var bitsLoaded = downloadSize * 8;
-            var speedBps = (bitsLoaded / duration).toFixed(2);
-            var speedKbps = (speedBps / 1024).toFixed(2);
-            var speedMbps = (speedKbps / 1024).toFixed(2);
-            // alert("Your connection speed is: \n" +
-            //     speedBps + " bps\n" +
-            //     speedKbps + " kbps\n" +
-            //     speedMbps + " Mbps\n");
-            $scope.speedinmbps=speedMbps;
-        }
-
-    }
-    setInterval(function(){
-        $scope.speed();
-      }, 5000)
-
 })

1062fd285bb0a7931c4754d3582352a26d090716
shaiksajans <shaiksajans@gmail.com>
2018-08-01 00:39:03 +0530

    24 july

--- public/vcApp.js
+++ public/vcApp.js
@@ -1,5 +1,5 @@
 
-var app = angular.module('vcApp', ['ui.router', 'mwl.calendar', 'ui.bootstrap', 'ngCookies', 'ngAnimate','ngImgCrop','angular-loading-bar']);
+var app = angular.module('vcApp', ['ui.router', 'mwl.calendar', 'ui.bootstrap', 'ngCookies', 'ngAnimate','ngImgCrop','angular-loading-bar','angularMoment']);
 
 app.config(['cfpLoadingBarProvider', function(cfpLoadingBarProvider) {
   // cfpLoadingBarProvider.includeBar = true;

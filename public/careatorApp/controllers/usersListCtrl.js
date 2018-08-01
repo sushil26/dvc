33b6ed387a91692bb41ccc96b94e317e739876bc
shaiksajans <shaiksajans@gmail.com>
2018-08-01 14:43:42 +0530

    24 july

--- public/careatorApp/controllers/usersListCtrl.js
+++ public/careatorApp/controllers/usersListCtrl.js
@@ -1,5 +1,36 @@
-careatorApp.controller('usersListCtrl', function ($scope, $state, careatorHttpFactory) {
+careatorApp.controller('usersListCtrl', function ($scope, $state, careatorHttpFactory, SweetAlert) {
     console.log("usersListCtrl==>");
+
+    var vm = this;
+    vm.alert = function () {
+        //alert implementation here
+        SweetAlert.swal("I'm a fancy Alert");
+    }
+    vm.confirm = function () {
+        SweetAlert.swal({
+                title: "Are you sure?", //Bold text
+                text: "Your will not be able to recover this imaginary file!", //light text
+                type: "warning", //type -- adds appropiriate icon
+                showCancelButton: true, // displays cancel btton
+                confirmButtonColor: "#DD6B55",
+                confirmButtonText: "Yes, delete it!",
+                closeOnConfirm: false, //do not close popup after click on confirm, usefull when you want to display a subsequent popup
+                closeOnCancel: false
+            },
+            function (isConfirm) { //Function that triggers on user action.
+                if (isConfirm) {
+                    SweetAlert.swal("Deleted!");
+                } else {
+                    SweetAlert.swal("Your file is safe!");
+                }
+            }
+
+        )
+    }
+
+
+
+
     $scope.getAllEmployee = function () {
         console.log("getAllEmployee-->");
         var api = "https://norecruits.com/careator/careator_getAllEmp";
@@ -64,26 +95,54 @@ careatorApp.controller('usersListCtrl', function ($scope, $state, careatorHttpFa
 
     ////////////////Delete User/////////////////////////
     $scope.deleteUser = function (id) {
-        $("#deleteConfirmationButton").trigger("click");
+        // $("#deleteConfirmationButton").trigger("click");
+        var vm = this;
+        vm.alert = function () {
+            //alert implementation here
+            SweetAlert.swal("I'm a fancy Alert");
+        }
+        vm.confirm = function () {
+            SweetAlert.swal({
+                    title: "Are you sure?", //Bold text
+                    text: "Your will not be able to recover this imaginary file!", //light text
+                    type: "warning", //type -- adds appropiriate icon
+                    showCancelButton: true, // displays cancel btton
+                    confirmButtonColor: "#DD6B55",
+                    confirmButtonText: "Yes, delete it!",
+                    closeOnConfirm: false, //do not close popup after click on confirm, usefull when you want to display a subsequent popup
+                    closeOnCancel: false
+                },
+                function (isConfirm) { //Function that triggers on user action.
+                    if (isConfirm) {
+                        SweetAlert.swal("Deleted!");
+                        var api = "https://norecruits.com/careator_userDelete/userDeleteById/" + id;
+                        careatorHttpFactory.get(api).then(function (data) {
+                            console.log("data--" + JSON.stringify(data.data));
+                            var checkStatus = careatorHttpFactory.dataValidation(data);
+                            console.log("data--" + JSON.stringify(data.data));
+                            if (checkStatus) {
+                                console.log(data.data.message);
+                                $scope.getAllEmployee();
+                            } else {
+                                console.log("Sorry");
+                                console.log(data.data.message);
+                            }
+                        })
+                        console.log("<--statusChange");
+
+                    } else {
+                        SweetAlert.swal("Your file is safe!");
+                    }
+                }
+
+            )
+        }
+
+
 
         console.log("deleteUser-->");
         console.log("Obj ID  " + id);
-        $scope.userDelete = function () {
-            var api = "https://norecruits.com/careator_userDelete/userDeleteById/" + id;
-            careatorHttpFactory.get(api).then(function (data) {
-                console.log("data--" + JSON.stringify(data.data));
-                var checkStatus = careatorHttpFactory.dataValidation(data);
-                console.log("data--" + JSON.stringify(data.data));
-                if (checkStatus) {
-                    console.log(data.data.message);
-                    $scope.getAllEmployee();
-                } else {
-                    console.log("Sorry");
-                    console.log(data.data.message);
-                }
-            })
-            console.log("<--statusChange");
-        }
+
     }
 
     $scope.resetLoginFlag = function (id) {

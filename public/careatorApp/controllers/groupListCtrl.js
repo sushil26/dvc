9a6257255315c9b7dbc2ecdcaa4819df31823162
shaiksajans <shaiksajans@gmail.com>
2018-08-01 16:07:03 +0530

    24 july

--- public/careatorApp/controllers/groupListCtrl.js
+++ public/careatorApp/controllers/groupListCtrl.js
@@ -1,4 +1,4 @@
-careatorApp.controller('groupListCtrl', function ($scope, $state, $rootScope, $filter, $window, careatorHttpFactory) {
+careatorApp.controller('groupListCtrl', function ($scope, $state,careatorHttpFactory,SweetAlert) {
     console.log("groupListCtrl==>");
     $scope.getGroupList = function () {
         console.log("getGroupList-->");
@@ -23,27 +23,90 @@ careatorApp.controller('groupListCtrl', function ($scope, $state, $rootScope, $f
     $scope.getGroupList();
 
     $scope.statusChange = function (id, status) {
-        console.log("statusChange-->");
-        console.log("id: " + id + " status: " + status);
-        var obj = {
-            "id": id,
-            "status": status
+        if (status='active') {
+            SweetAlert.swal({
+                title: "Are you sure to Activate the Group?", //Bold text
+                text: "User will be able to Use !", //light text
+                type: "warning", //type -- adds appropiriate icon
+                showCancelButton: true, // displays cancel btton
+                confirmButtonColor: "#DD6B55",
+                confirmButtonText: "Yes, Activate it!",
+                closeOnConfirm: false, //do not close popup after click on confirm, usefull when you want to display a subsequent popup
+                closeOnCancel: false
+                },
+                function (isConfirm) { //Function that triggers on user action.
+                    if (isConfirm) {
+                        SweetAlert.swal("Activated!");
+                        console.log("statusChange-->");
+                        console.log("id: " + id + " status: " + status);
+                        var obj = {
+                            "id": id,
+                            "status": status
+                        }
+                        var api = "https://norecruits.com/careator/groupStatusChangeById";
+                        console.log("api: " + api);
+                        careatorHttpFactory.post(api, obj).then(function (data) {
+                            console.log("data--" + JSON.stringify(data.data));
+                            var checkStatus = careatorHttpFactory.dataValidation(data);
+                            console.log("data--" + JSON.stringify(data.data));
+                            if (checkStatus) {
+                                $scope.getGroupList();
+                                console.log(data.data.message);
+                            } else {
+                                console.log("Sorry");
+                                console.log(data.data.message);
+                            }
+                        })
+                        console.log("<--statusChange");
+                    } else {
+                        SweetAlert.swal("You did not Activate the user!");
+                    }
+                }
+
+            )
         }
-        var api = "https://norecruits.com/careator/groupStatusChangeById";
-        console.log("api: " + api);
-        careatorHttpFactory.post(api, obj).then(function (data) {
-            console.log("data--" + JSON.stringify(data.data));
-            var checkStatus = careatorHttpFactory.dataValidation(data);
-            console.log("data--" + JSON.stringify(data.data));
-            if (checkStatus) {
-                $scope.getGroupList();
-                console.log(data.data.message);
-            } else {
-                console.log("Sorry");
-                console.log(data.data.message);
-            }
-        })
-        console.log("<--statusChange");
+        if (status='inactive') {
+            SweetAlert.swal({
+                title: "Are you sure to Deactivate the Group ?", //Bold text
+                text: "User will not be able to use !", //light text
+                type: "warning", //type -- adds appropiriate icon
+                showCancelButton: true, // displays cancel btton
+                confirmButtonColor: "#DD6B55",
+                confirmButtonText: "Yes, Deactivate!",
+                closeOnConfirm: false, //do not close popup after click on confirm, usefull when you want to display a subsequent popup
+                closeOnCancel: false
+                },
+                function (isConfirm) { //Function that triggers on user action.
+                    if (isConfirm) {
+                        SweetAlert.swal("Deactivated!");
+                        console.log("statusChange-->");
+                        console.log("id: " + id + " status: " + status);
+                        var obj = {
+                            "id": id,
+                            "status": status
+                        }
+                        var api = "https://norecruits.com/careator/groupStatusChangeById";
+                        console.log("api: " + api);
+                        careatorHttpFactory.post(api, obj).then(function (data) {
+                            console.log("data--" + JSON.stringify(data.data));
+                            var checkStatus = careatorHttpFactory.dataValidation(data);
+                            console.log("data--" + JSON.stringify(data.data));
+                            if (checkStatus) {
+                                $scope.getGroupList();
+                                console.log(data.data.message);
+                            } else {
+                                console.log("Sorry");
+                                console.log(data.data.message);
+                            }
+                        })
+                        console.log("<--statusChange");
+                    } else {
+                        SweetAlert.swal("Users cant use this group!");
+                    }
+                }
+
+            )
+        }
     }
     /////////////////Redirect page with data (id)///////////////////////
     $scope.editGroup = function (index) {

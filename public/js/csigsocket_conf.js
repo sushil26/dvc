56db0cc45f3ab56fc11320bbb8ae6dc61e6e174c
shaiksajans <shaiksajans@gmail.com>
2018-08-03 17:13:09 +0530

    24 july3

--- public/js/csigsocket_conf.js
+++ public/js/csigsocket_conf.js
@@ -1020,7 +1020,7 @@ signaling_socket.on("addPeer", function (config) {
       $("#" + peer_id + "remoteContainer").removeClass(
         "portfolio-items col-xs-4 col-sm-4 col-md-4 col-lg-3"
       );
-      // $("#portfolio .portfolio-items").css({"position":"absolute"})
+      $("#portfolio .portfolio-items").css({"position":"absolute"})
 
       $("#" + peer_id + "Remote").css({
         height: "82vh"
@@ -1048,7 +1048,7 @@ signaling_socket.on("addPeer", function (config) {
 
       console.log("add id videoElem111");
       $("#btnrestore").css({"display":"none"})
-      // $("#portfolio .portfolio-items").css({"position":"relative"})
+      $("#portfolio .portfolio-items").css({"position":"relative"})
       $("#" + peer_id + "remoteVideoElement").removeClass("fullscr");
       $("#" + peer_id + "remoteContainer").addClass(
         "portfolio-items col-xs-4 col-sm-4 col-md-4 col-lg-3"

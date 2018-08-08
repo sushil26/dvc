ad212d0772d884b59e4c3e06d52c932aa7fd0879
LogeswariGuru <logeswari.careator@gmail.com>
2018-08-08 12:05:16 +0530

    cIndex test

---
+++ config/cControllers/cRouter.js
@@ -0,0 +1,75 @@
+var user = require('./controllers/user');
+var event = require('./controllers/event');
+var quickMsg = require('./controllers/quickMsg');
+var image = require('./controllers/image');
+var adminAction = require('./controllers/adminAction');
+var school = require('./controllers/school');
+var careator = require('./controllers/careator');
+var careator_event = require('./controllers/careator_event');
+var careatorImgUpload = require('./controllers/careatorImgUploads.js');
+
+
+var record = require('./controllers/record');
+var capture = require('./controllers/capture');
+
+module.exports = function (app) {
+
+    app.post('/careator/pswdGenerate', careator.pswdGenerate);
+    app.post('/careator/pswdCheck', careator.pswdCheck);
+    app.post('/careator/pswdCheckForSesstion', careator.pswdCheckForSesstion);
+    app.post('/careator/emailInvite', careator.emailInvite);
+    app.post('/careator/RemoteJoinCheck', careator.RemoteJoinCheck);
+    app.post('/careator/setCollection', careator.setCollection);
+    app.post('/chatHistory/getChatByUrl', careator.getChatByUrl);
+    app.get('/chatHistory/getHistory', careator.getHistory);
+    app.get('/chatHistory/getHistoryByEmailId/:email', careator.getHistoryByEmailId);
+    app.post('/careator_profile/chatStatusUpdateById/:id', careator.chatStatusUpdateById);
+    app.get('/careator_adminBasicData/getAdminObjectId', careator.getAdminObjectId);
+    app.post('/careator/careatorMasterInsert', careator.careatorMasterInsert);
+    app.post('/careator/careatorSingleUserInsert', careator.careatorSingleUserInsert);
+    app.get('/careator/careator_getAllEmp', careator.careator_getAllEmp);
+    app.post('/careator/statusChangeById', careator.statusChangeById);
+    app.post('/careator/groupStatusChangeById', careator.groupStatusChangeById);
+    app.get('/careator_getChatListRecordById/getChatListRecordById/:id', careator.getChatListRecordById);
+    app.get('/careator_userDelete/userDeleteById/:id', careator.userDeleteById);
+    app.get('/careator_groupDelete/groupDeleteById/:id', careator.groupDeleteById);
+    app.post('/careator/userEditById/:id', careator.userEditById);
+    app.post('/careator/groupEditById/:id', careator.groupEditById);
+    app.get('/careator_getUser/careator_getUserById/:id', careator.careator_getUserById);
+    app.get('/careator_getGroup/careator_getGroupById/:id', careator.careator_getGroupById);
+    app.get('/careator/getChatRights_emp', careator.getChatRights_emp);
+    app.get('/careator/getVideoRights_emp', careator.getVideoRights_emp);
+    app.get('/careator/careator_getChatVideo_emp', careator.careator_getChatVideo_emp);
+    app.post('/careator/careator_chat_creteGroup', careator.careator_chat_creteGroup);
+    app.post('/careator_groupUpdate/groupUpdateById/:id', careator.groupUpdateById);
+    app.post('/careator_restrictedTo/restrictedTo/:id', careator.restrictedTo);
+    app.post('/careator_restrictedToSave/restrictedToSave/:id', careator.restrictedToSave);
+    app.post('/careator_removeRestrictedUserById/removeRestrictedUserById/:id', careator.removeRestrictedUserById);
+    app.get('/careator_getChatsById/getChatsById/:id', careator.getChatsById);
+    app.get('/careator_loggedin/getLoggedinSessionURLById/:id', careator.getLoggedinSessionURLById);
+    app.post('/careator_reset/resetLoginFlagsById/:id', careator.resetLoginFlagsById);
+    app.post('/careator_textSeenFlagUpdate/textSeenFlagUpdate/:id', careator.textSeenFlagUpdate);
+    app.post('/careator_textSeenFlagUpdate_toGroupChat/textSeenFlagUpdate_toGroupChat/:group_id', careator.textSeenFlagUpdate_toGroupChat);
+    app.post('/careator_eventSchedule/careator_sendEventSchedule', careator_event.careator_sendEventSchedule);
+    app.get('/careator_eventSchedule/careator_eventGetById/:id', careator_event.careator_eventGetById);
+    app.get("/careator_getToDate/careator_getToDate", careator_event.careator_getToDate);
+    //app.post('/careator/careator_video_creteGroup', careator.careator_video_creteGroup);
+    //app.post('/careator/careator_chatVideo_creteGroup', careator.careator_chatVideo_creteGroup);
+
+    app.get('/careator_chatGroupList/careator_getChatGroupListById/:id', careator.careator_getChatGroupListById);
+    app.get('/careator_chatGroupList/careator_getChatGroupList', careator.careator_getChatGroupList);
+    app.post('/careator_getEmp/careator_getChatRightsAllemp/:id', careator.careator_getChatRightsAllemp);
+    app.get('/careator_getEmp/careator_getChatRightsAllemp_byLoginId/:id', careator.careator_getChatRightsAllemp_byLoginId);
+    app.post('/careator_individualText/individualText', careator.individualText);
+    app.get('/careator_individualTextRead/individualTextReadById/:sId/:rId', careator.individualTextReadById);
+    app.get('/careator_groupTextRead/groupTextReadByGroupId/:group_id', careator.groupTextReadByGroupId);
+    app.post('/careator_groupText/groupText', careator.groupText);
+    //app.get('/careator_groupTextRead/groupTextReadById/:sId/:rId', careator.groupTextReadById); /* Note: this api methos is not defining on the careator.js */
+
+    app.post('/careator_comm_profileImgUpload/comm_profileImgUpload', careatorImgUpload.comm_profileImgUpload);
+    app.post('/careator_comm_profileImgUpdateById/comm_profileImgUpdateById/:id', careator.comm_profileImgUpdateById);
+
+  
+}
+
+  

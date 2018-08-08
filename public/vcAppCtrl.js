aa4447a4eba577ddbf7dd58bf271553e38884e6a
shaiksajans <shaiksajans@gmail.com>
2018-08-08 05:58:36 +0530

    video

--- public/vcAppCtrl.js
+++ public/vcAppCtrl.js
@@ -1,5 +1,111 @@
 app.controller("vcAppCtrl", function ($scope, $rootScope, httpFactory, $window,sessionAuthFactory, $uibModal, SweetAlert) {
   console.log("controller==>");
+
+  $scope.checkCredential =function(email, Password) {
+    console.log("checkCredential-->");
+    $('#myEmailModal').modal('hide');
+    var password = document.getElementById("careatorPswd").value;
+    var careatorEmail = document.getElementById("careatorEmail").value;
+    var obj = {
+      "password": Password,
+      "careatorEmail": email
+    };
+    console.log("obj: " + JSON.stringify(obj));
+    var api = "careator/pswdCheck";
+    console.log("api: " + api);
+    if (password != "" && careatorEmail != "") {
+
+
+      httpFactory.post(api, obj).then(function (data) {
+        var checkStatus = httpFactory.dataValidation(data);
+        console.log("data--" + JSON.stringify(data.data));
+        console.log("checkStatus" + checkStatus);
+        console.log("data.message: " + data.data.message);
+        if (checkStatus) {
+          var datas = data.data;
+          console.log("data.message: " + data.data.message);
+          console.log("data: " + JSON.stringify(data))
+
+          localStorage.setItem("careatorEmail", careatorEmail);
+          localStorage.setItem("userName", data.data.name);
+          localStorage.setItem("empId", data.data.empId);
+          localStorage.setItem("email", data.data.email);
+          localStorage.setItem("userId", data.data._id);
+          localStorage.setItem("sessionPassword", password);
+          localStorage.setItem("sessionRandomId", data.data.sessionRandomId);
+          localStorage.setItem("sessionEnc", data.sessionData);
+          userName = localStorage.getItem("userName");
+          if (data.data.videoRights == 'yes') {
+            localStorage.setItem("videoRights", 'yes');
+            document.getElementById("videoConfStart").style.display = "inline";
+            document.getElementById("scheduleVC").style.display = "inline";
+            $("#buttonpage").css({
+              "min-height": "auto"
+            });
+          }
+          if (data.data.chatRights == 'yes') {
+            localStorage.setItem("chatRights", 'yes');
+            document.getElementById("chatConfStart").style.display = "inline";
+          }
+          if (data.data.chatStatus) {
+            localStorage.setItem("chatStatus", data.data.chatStatus);
+          }
+          if (data.data.restrictedTo) {
+            console.log("data.data.restrictedTo: " + JSON.stringify(data.data.restrictedTo));
+            var restrictedTo = data.data.restrictedTo;
+            var restrictedArray = [];
+            for (var x = 0; x < restrictedTo.length; x++) {
+              restrictedArray.push(restrictedTo[x].userId);
+            }
+  
+            console.log("restrictedArray: " + restrictedArray);
+            localStorage.setItem("restrictedTo", restrictedArray);
+          }
+          if (data.data.profilePicPath) {
+            localStorage.setItem("profilePicPath", data.data.profilePicPath);
+          }
+  
+          console.log("userName: " + userName);
+          document.getElementById("videoConferenceUrl").style.display = "block";
+          // $('#myPasswordModal').modal('hide');
+          window.location.href = "https://norecruits.com/careatorApp/#!/dashboard/profile";
+        },
+        error: function (err) {
+          console.log("err: " + JSON.stringify(err));
+          console.log("err.responseText: " + JSON.stringify(err.responseText));
+          console.log("err.responseJSON: " + JSON.stringify(err.responseJSON.message));
+          if (err.responseJSON.message == "You've already logged in. To log in again, please reset your session") {
+  
+            checkObj = {
+              "password": password,
+              "careatorEmail": careatorEmail
+            }
+            console.log("You already logged in, please logout your old session in-order to login");
+            document.getElementById('notify_msg_content').innerHTML = err.responseJSON.message;
+            document.getElementById('resetBtn').style.display = 'inline';
+            $("#notify_msg_button").trigger("click");
+             
+            resetId = err.responseJSON.data.id;
+  
+          } else {
+            // alert(err.responseJSON.message);
+            $("#alertButton").trigger("click");
+             
+            var x =
+              document.getElementById('alertcontent').innerHTML = err.responseJSON.message + "<br>Please try with correct password or contact Admin";
+          }
+          document.getElementById("videoConferenceUrl").style.display = "none";
+          localStorage.removeItem("careatorEmail");
+          userName = "";
+        }
+      });
+    } else {
+      console.log("password trigger again-->");
+      console.log("Password empty");
+    }
+    console.log("<--checkCredential");
+  }
+
   //document.getElementById('mobile-nav').style.display='none';
   var loginModal; /* ### Note: get login modal instance on this variable ###*/
   var userName;

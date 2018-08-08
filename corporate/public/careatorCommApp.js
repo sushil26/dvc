d5192f0873cce01655708491dc7dece696fa21a7
LogeswariGuru <logeswari.careator@gmail.com>
2018-08-08 15:50:19 +0530

    login popup

--- corporate/public/careatorCommApp.js
+++ corporate/public/careatorCommApp.js
@@ -1,11 +1,11 @@
 var careatorApp = angular.module('careatorCommApp', ['ui.router', 'angularjs-dropdown-multiselect', 'ngCookies', 'ngImgCrop']);
 
-careatorApp.config(['cfpLoadingBarProvider', function (cfpLoadingBarProvider) {
-    // cfpLoadingBarProvider.includeBar = true;
-    cfpLoadingBarProvider.includeSpinner = true;
-    // cfpLoadingBarProvider.parentSelector = '#loading-bar-container';
-    // cfpLoadingBarProvider.spinnerTemplate = '<div><span class="fa fa-spinner">Loading...</div>';
-}])
+// careatorApp.config(['cfpLoadingBarProvider', function (cfpLoadingBarProvider) {
+//     // cfpLoadingBarProvider.includeBar = true;
+//     cfpLoadingBarProvider.includeSpinner = true;
+//     // cfpLoadingBarProvider.parentSelector = '#loading-bar-container';
+//     // cfpLoadingBarProvider.spinnerTemplate = '<div><span class="fa fa-spinner">Loading...</div>';
+// }])
 careatorApp.config(function ($stateProvider) {
 
     $stateProvider

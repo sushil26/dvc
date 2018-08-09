d4a17ac7bf91a9c5ce321842a1774585db431961
LogeswariGuru <logeswari.careator@gmail.com>
2018-08-09 16:13:41 +0530

    dashboard and logout added into index.html based on login

--- corporate/public/careatorCommAppCtrl.js
+++ corporate/public/careatorCommAppCtrl.js
@@ -313,6 +313,7 @@ careatorApp.controller("careatorCommAppCtrl", function ($scope, $state, careator
             localStorage.removeItem("profilePicPath");
             localStorage.removeItem("sessionRandomId");
             careatorSessionAuth.clearAccess("userData");
+            $scope.userData = careatorSessionAuth.getAccess("userData");
             // $scope.doRedirect();
         }
         // else if (data.userId == $scope.userData.userId && data.email == $scope.userData.email && data.sessionRandomId == $scope.userData.sessionRandomId) {

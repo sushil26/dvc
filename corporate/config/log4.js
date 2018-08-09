c09c15979c66d7f091edb5669d0a136669ca7224
LogeswariGuru <logeswari.careator@gmail.com>
2018-08-09 17:50:23 +0530

    logger for userlogin

--- corporate/config/log4.js
+++ corporate/config/log4.js
@@ -17,7 +17,7 @@ const log4js = require('log4js');
 const log4js_extend = require("log4js-extend");
 
 log4js.configure({
-  appenders: { cheese: { type: 'file', filename: './logs/loggedinUsers.log' } },
+  appenders: { cheese: { type: 'file', filename: './logs/debug.log' } },
   categories: { default: { appenders: ['cheese'], level: 'all' } }
 });
 
@@ -35,9 +35,9 @@ log4js_extend(log4js, {
 Object.defineProperty(exports, "LOG", {
             value:logger,
 });
-logger.trace('Entering cheese testing');
-logger.debug('Got cheese.');
-logger.info('Cheese is Gouda.');
-logger.warn('Cheese is quite smelly.');
-logger.error('Cheese is too ripe!');
-logger.fatal('Cheese was breeding ground for listeria.');
+// logger.trace('Entering cheese testing');
+// logger.debug('Got cheese.');
+// logger.info('Cheese is Gouda.');
+// logger.warn('Cheese is quite smelly.');
+// logger.error('Cheese is too ripe!');
+// logger.fatal('Cheese was breeding ground for listeria.');

c4851f642d3b4abcef836964f41d9abc14dbf917
LogeswariGuru <logeswari.careator@gmail.com>
2018-08-09 17:53:21 +0530

    logger for userlogin

--- corporate/config/log4.js
+++ corporate/config/log4.js
@@ -17,7 +17,7 @@ const log4js = require('log4js');
 const log4js_extend = require("log4js-extend");
 
 log4js.configure({
-  appenders: { cheese: { type: 'file', filename: './logs/debug.log' } },
+  appenders: { cheese: { type: 'file', filename: './logs/loginDetails.log' } },
   categories: { default: { appenders: ['cheese'], level: 'all' } }
 });
 

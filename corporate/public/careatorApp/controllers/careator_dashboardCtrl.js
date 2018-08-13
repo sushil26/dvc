bcc7af4d6d688a100f03c7958f413b8179bbc712
shaiksajans <shaiksajans@gmail.com>
2018-08-13 14:37:32 +0530

    video

--- corporate/public/careatorApp/controllers/careator_dashboardCtrl.js
+++ corporate/public/careatorApp/controllers/careator_dashboardCtrl.js
@@ -485,6 +485,7 @@ careatorApp.controller('careator_dashboardCtrl', function ($scope, $rootScope, $
                 function (isConfirm) { //Function that triggers on user action.
                     if (isConfirm) {
                         w.focus();
+                        window.close();
                     } else {
                         SweetAlert.swal({
 

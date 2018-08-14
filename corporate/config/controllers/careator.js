e550ce18319bc46066165c617fd35fc85adda65f
LogeswariGuru <logeswari.careator@gmail.com>
2018-08-14 15:21:43 +0530

    date

--- corporate/config/controllers/careator.js
+++ corporate/config/controllers/careator.js
@@ -182,12 +182,10 @@ module.exports.RemoteJoinCheck_schedule = function (req, res) {
                                 res.status(400).send(responseData);
                             } else {
                                 if (findData.length > 0) {
-                                    console.log("findData[0].startsAt: "+findData[0].startsAt);
                                     var date = new Date();
                                     var newFormatedDate_currentDate = new Date(date.getFullYear(),date.getMonth(), date.getDate(), date.getHours(), date.getMinutes());
-                                    var sd = new Date(findData[0].startsAt);
-                                    console.log("sd: " + sd);
-                                    //var newFormate_sd = new date(sd.getFullYear(),sd.getMonth(), sd.getDate(), sd.getHours(), sd.getMinutes());
+                                    //var sd = findData[0].startsAt;
+                                    var newFormate_sd = new Date(findData[0].startsAt.getFullYear(),findData[0].startsAt.getMonth(), findData[0].startsAt.getDate(), findData[0].startsAt.getHours(), findData[0].startsAt.getMinutes());
                                     console.log("newFormatedDate_currentDate: " + newFormatedDate_currentDate);
                                     console.log("newFormate_sd: " + newFormate_sd);
                                     if (newFormatedDate_currentDate <= newFormate_sd) {

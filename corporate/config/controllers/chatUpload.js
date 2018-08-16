6f0f61c8cf7aea3e36297439f93c8ccf4c382105
LogeswariGuru <logeswari.careator@gmail.com>
2018-08-16 17:06:58 +0530

    chatfile

--- corporate/config/controllers/chatUpload.js
+++ corporate/config/controllers/chatUpload.js
@@ -75,34 +75,34 @@ module.exports.chatFileUpload = function (req, res) {
 }
 
 module.exports.getChatFileUpload = function (req, res) {
-    console.log("*getChatFileUpload-->");
+    console.log("getChatFileUpload-->");
     console.log("req.params.id: "+req.params.id);
 
-    // var gfs = Grid(conn.db);
-    // var output = '';
-    // var readStream = gfs.createReadStream({
-    //     "_id": ObjectId(req.params.id) // this id was stored in db when inserted a video stream above
-    // });
-    // readStream.on("data", function (chunk) {
-    //     console.log("chunk: "+chunk);
-    //     output += chunk;
-    // });
-    // // base64.decode(output, function (err, output) {
-    // //     console.log('output');
-    // //     // dump contents to console when complete
-
-    // // });
-    // readStream.on("end", function () {
-    //     console.log("Final Output");
-    //     responseData = {
-    //         status: true,
-    //         message: "get successful",
-    //         data: output
-    //     };
-    //     res.status(200).send(responseData);
-    //     //console.log(output);
+    var gfs = Grid(conn.db);
+    var output = '';
+    var readStream = gfs.createReadStream({
+        "_id": ObjectId(req.params.id) // this id was stored in db when inserted a video stream above
+    });
+    readStream.on("data", function (chunk) {
+        console.log("chunk: "+chunk);
+        output += chunk;
+    });
+    // base64.decode(output, function (err, output) {
+    //     console.log('output');
+    //     // dump contents to console when complete
 
     // });
+    readStream.on("end", function () {
+        console.log("Final Output");
+        responseData = {
+            status: true,
+            message: "get file successful",
+            data: output
+        };
+        res.status(200).send(responseData);
+        //console.log(output);
+
+    });
 
     console.log("<--getRecordVideo");
 }

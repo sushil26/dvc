a73fc4486d48a9409dc93d32be37ba4bd5458915
LogeswariGuru <logeswari.careator@gmail.com>
2018-08-16 18:29:36 +0530

    chatfile

--- corporate/config/controllers/chatUpload.js
+++ corporate/config/controllers/chatUpload.js
@@ -9,75 +9,88 @@ const path = require('path');
 const ABSPATH = path.dirname(process.mainModule.filename); // Absolute path to our app directory
 Grid.mongo = mongoose.mongo;
 var blobs = [];
+const chatFileDirectory = process.cwd() + '/public/chatFiles/';
 
 module.exports.chatFileUpload = function (req, res) {
     console.log("chatFileUpload-->");
     if (!req.files)
         return res.status(400).send('No files were uploaded.');
+    console.log("req.files.sampleFile: " + req.files.img);
+    let myFile = req.files.img;
+    console.log("path--" + chatFileDirectory);
+    var fileArr = myFile.name.split(".");
+    var fileName = "";
+    for (var i = 0; i < fileArr.length - 1; i++) {
+        fileName = fileName + fileArr[i]
+    }
+    fileName = fileName + "_" + general.date() + "." + fileArr[fileArr.length - 1];
+    console.log("fileName--" + fileName)
 
-    var userDataFile = req.files.img;
-    // console.log("userDataFile: " + userDataFile);
-    // console.log("req.body.logo: "+req.body.img);
-    var chatFile = req.files.img
-    console.log("chatFile: " + JSON.stringify(chatFile));
-    //console.log("chatFile.data: " + JSON.stringify(chatFile.data));
+    myFile.mv(chatFileDirectory + fileName, function (err) {
+        if (err) {
+            console.log(require('util').inspect(err));
+            var responseData = {
+                "status": true,
+                "message": "date stored unsuccessfully",
+                "data": { "err": err }
+            }
+            res.status(500).send(responseData);
 
-
-    var gfs = Grid(conn.db);
-    var writeStream = gfs.createWriteStream({
-        filename: req.files.img.name,
-        mode: 'w',
-        content_type: req.files.img.mimetype,
-        metadata: userDataFile
-    });
-    var buffer = new Buffer(chatFile.data);
-    //console.log("buffer: " + buffer);
-    //var response = streamifier.createReadStream(buffer).pipe(writeStream);  // returns response which is having all information regarding saved byte string
-    //var lastInsertedFileId = response._store.fileId;
-    console.log("lastInsertedFileId: "+lastInsertedFileId);
-     var response = fs.createReadStream(req.files.img.path).pipe(writestream);
-     var lastInsertedFileId = response._store.fileId;
-
-    // var getValue = chatFile;
-    // //var byte_string = videoBase64.substr(23);//The base64 has a imageURL
-    // //var buffer = new Buffer(byte_string);   //new Buffer(b64string, 'base64');  you can use base64 encoding with creating new buffer string
-    // // var buffer = new Buffer(chatFile);   //new Buffer(b64string, 'base64');  you can use base64 encoding with creating new buffer string
-    // // var response = streamifier.createReadStream(buffer).pipe(writeStream);  // returns response which is having all information regarding saved byte string
-    // var lastInsertedFileId = response._store.fileId;  // now you can store it into another document for future use.
-    // console.log(lastInsertedFileId);
-
-    writeStream.on('close', function (file) {
-        console.log(file.filename + "written to db");
-        var responseData;
-       // console.log("req.body.id: " + req.body.id);
-
-        // var queryId = {
-        //     "_id": ObjectId(req.body.eventId)
-        // }
-        // console.log("queryId: " + JSON.stringify(queryId));
-        var setData = {
-            "vcRecordId": lastInsertedFileId
         }
-       // console.log("setData: " + JSON.stringify(setData));
-        // event.update({ "_id": ObjectId(req.body.eventId), 'vcRecordId': { $exists: false } }, { $set: { "vcRecordId": lastInsertedFileId } }, function (err, data) {
-        //     var io = req.app.get('socketio');
-        //     io.emit('eventUpdatedForHistory', {});
-        //     console.log("data: " + JSON.stringify(data));
-        // })
-        responseData = {
-            status: true,
-            errorCode: 200,
-            message: "insert Successfull and Failed to send mail",
-            data:lastInsertedFileId
-        };
-        res.status(200).send(responseData);
-    })
+        else {
+            var gfs = Grid(conn.db);
+            var writeStream = gfs.createWriteStream({
+                filename: fileName,
+                mode: 'w',
+                content_type: req.files.img.mimetype,
+                metadata: userDataFile
+            });
+
+            console.log("lastInsertedFileId: " + lastInsertedFileId);
+            var response = fs.createReadStream( "/public/schoolLogo/" + fileName).pipe(writestream);
+            var lastInsertedFileId = response._store.fileId;
+
+
+            writeStream.on('close', function (file) {
+                console.log(file.filename + "written to db");
+                var responseData;
+                var setData = {
+                    "vcRecordId": lastInsertedFileId
+                }
+
+                responseData = {
+                    status: true,
+                    errorCode: 200,
+                    message: "insert Successfull and Failed to send mail",
+                    data: lastInsertedFileId
+                };
+                res.status(200).send(responseData);
+            })
+            var responseData = {
+                "status": true,
+                "message": "date stored successfully",
+                "data": { "filePath": "/schoolLogo/" + fileName }
+            }
+            res.status(200).send(responseData);
+        }
+    });
+
+
+
+
+
+    // var userDataFile = req.files.img;
+
+    // var chatFile = req.files.img
+    // console.log("chatFile: " + JSON.stringify(chatFile));
+
+
     console.log("<--recordVideo");
 }
 
 module.exports.getChatFileUpload = function (req, res) {
     console.log("getChatFileUpload-->");
-    console.log("req.params.id: "+req.params.id);
+    console.log("req.params.id: " + req.params.id);
 
     var gfs = Grid(conn.db);
     var output = '';
@@ -85,7 +98,7 @@ module.exports.getChatFileUpload = function (req, res) {
         "_id": ObjectId(req.params.id) // this id was stored in db when inserted a video stream above
     });
     readStream.on("data", function (chunk) {
-        console.log("chunk: "+chunk);
+        console.log("chunk: " + chunk);
         output += chunk;
     });
     // base64.decode(output, function (err, output) {

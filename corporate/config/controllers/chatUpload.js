5458e158a87a14065368e0955ee8583db886bb5d
LogeswariGuru <logeswari.careator@gmail.com>
2018-08-16 18:13:46 +0530

    chatfile

--- corporate/config/controllers/chatUpload.js
+++ corporate/config/controllers/chatUpload.js
@@ -19,7 +19,7 @@ module.exports.chatFileUpload = function (req, res) {
     // console.log("userDataFile: " + userDataFile);
     // console.log("req.body.logo: "+req.body.img);
     var chatFile = req.files.img
-    //console.log("chatFile: " + JSON.stringify(chatFile));
+    console.log("chatFile: " + JSON.stringify(chatFile));
     //console.log("chatFile.data: " + JSON.stringify(chatFile.data));
 
 

88e14695bc1694aa06c07a44bc5be3db01d815ca
LogeswariGuru <logeswari.careator@gmail.com>
2018-08-21 15:36:29 +0530

    status change api done

--- corporate/config/controllers/cAdminAction.js
+++ corporate/config/controllers/cAdminAction.js
@@ -433,6 +433,7 @@ module.exports.getOrg_admin_byOrgId = function (req, res) {
                         };
                         res.status(400).send(responseData);
                     } else {
+                        console.log("data[0]: "+JSON.stringify(data[0]));
                         data[0].pswd =  adminPassword[0].pswd;
                         responseData = {
                             status: true,

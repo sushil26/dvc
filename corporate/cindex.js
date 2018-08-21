b64e18c1a38a5673512e7d0fc674fe55b0fc40ad
LogeswariGuru <logeswari.careator@gmail.com>
2018-08-21 10:13:03 +0530

    chatfile

--- corporate/cindex.js
+++ corporate/cindex.js
@@ -4,12 +4,11 @@ var fileUpload = require('express-fileupload');
 var multer = require('multer');
 var ObjectId = require("mongodb").ObjectID;
 var app = express();
-// app.use(bodyParser.urlencoded({ extended: true }));
-// app.use(bodyParser.json({
-//     limit: '100mb'
-// }));
-app.use(bodyParser.json({limit: '50mb'}));
-app.use(bodyParser.urlencoded({ extended: true}));
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json({
+    limit: '100mb'
+}));
+
 // app.use(bodyParser.json({limit: "100mb"}));
 // app.use(bodyParser.urlencoded({limit: "100mb", extended: true, parameterLimit:50000}));
 

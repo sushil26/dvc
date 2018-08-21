fca92ed2df8e3e53fab881932616bad20bd723cb
shaiksajans <shaiksajans@gmail.com>
2018-08-21 14:19:05 +0530

    admin page

--- corporate/public/careatorApp/controllers/adminListCtrl.js
+++ corporate/public/careatorApp/controllers/adminListCtrl.js
@@ -1,7 +1,7 @@
 careatorApp.controller('adminListCtrl', function ($scope, $state, careatorHttpFactory, SweetAlert) {
     console.log("adminListCtrl==>");
 
-    $scope.getAllSchool = function () {
+    $scope.getAllAdmin = function () {
         console.log("getAllSchool-->");
         api = $scope.propertyJson.C_getAllAdmin;
         console.log("api: " + api);
@@ -12,12 +12,14 @@ careatorApp.controller('adminListCtrl', function ($scope, $state, careatorHttpFa
             if (checkStatus) {
                 console.log("data.data.data: " + JSON.stringify(data.data.data));
                 console.log("data.data.message: " + data.data.message);
+                $scope.allAdminList = data.data.data;
             }
             else {
                 console.log("data.data.message: " + data.data.message);
             }
         })
     }
+    $scope.getAllAdmin();
 
     $scope.C_getAllOrg = function(){
         console.log("-->");

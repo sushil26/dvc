60b401c1e68aa9fae4722af67b38114e31dc5603
LogeswariGuru <logeswari.careator@gmail.com>
2018-08-22 10:24:26 +0530

    userName defined

--- corporate/public/careatorApp/controllers/vc4allEmpScheduleCtrl.js
+++ corporate/public/careatorApp/controllers/vc4allEmpScheduleCtrl.js
@@ -63,7 +63,7 @@ careatorApp.controller('vc4allEmpScheduleCtrl', function ($scope, $q, $timeout, 
   }
   $scope.getToDateByEachSec();
 
-  $interval($scope.getToDateByEachSec(), 1000);
+  // $interval($scope.getToDateByEachSec(), 1000);
 
   $scope.eventGet = function () {
     console.log("eventGet-->");
@@ -212,33 +212,34 @@ careatorApp.controller('vc4allEmpScheduleCtrl', function ($scope, $q, $timeout, 
 
   }
 
-  // function getSocketUrlFromServer() {
-  //   console.log("getSocketUrlFromServer-->");
-  //   var dfd = $q.defer();
-  //   var SIGNALING_SERVER = "https://norecruits.com";
-  //   signaling_socket = io(SIGNALING_SERVER);
-  //   signaling_socket.on('connect', function () {
-  //     console.log("signaling_socket connect-->");
 
-  //     signaling_socket.on('message', function (config) {
-  //       console.log("signaling_socket message-->");
+  function getSocketUrlFromServer() {
+    console.log("getSocketUrlFromServer-->");
+    var dfd = $q.defer();
+    var SIGNALING_SERVER = "https://norecruits.com";
+    signaling_socket = io(SIGNALING_SERVER);
+    signaling_socket.on('connect', function () {
+      console.log("signaling_socket connect-->");
 
-  //       queryLink = config.queryId;
-  //       peerNew_id = config.peer_id;
-  //       var dt = $scope.todayDate;
-  //       var dy = dt.getDay().toString();
-  //       var fy = dt.getFullYear().toString();
-  //       var m = dt.getMonth().toString();
-  //       var hr = dt.getHours().toString();
-  //       var date = dy.concat(fy, m, hr);
-  //       urlDate = date;
+      signaling_socket.on('message', function (config) {
+        console.log("signaling_socket message-->");
 
-  //       var url = "https://norecruits.com/talenkart_scheduleConf/" + peerNew_id + "/" + urlDate;
-  //       dfd.resolve(url);
-  //     })
-  //   })
-  //   return dfd.promise;
-  // }
+        queryLink = config.queryId;
+        peerNew_id = config.peer_id;
+        var dt = $scope.todayDate;
+        var dy = dt.getDay().toString();
+        var fy = dt.getFullYear().toString();
+        var m = dt.getMonth().toString();
+        var hr = dt.getHours().toString();
+        var date = dy.concat(fy, m, hr);
+        urlDate = date;
+
+        var url = "https://norecruits.com/talenkart_scheduleConf/" + peerNew_id + "/" + urlDate;
+        dfd.resolve(url);
+      })
+    })
+    return dfd.promise;
+  }
    
  
 
@@ -248,80 +249,80 @@ careatorApp.controller('vc4allEmpScheduleCtrl', function ($scope, $q, $timeout, 
     var queryLink = null;
     var peerNew_id = null;
     var url;
-    // $timeout(function () {
-    //   getSocketUrlFromServer().then(function (url) {
-    //     console.log("Back to function call-->");
-    //     console.log("url: " + url);
-    //     var api = "https://norecruits.com/careator_eventSchedule/careator_sendEventSchedule";
-    //     console.log("api: " + api);
-    //     var obj = {
-    //       "senderId": $scope.userData.userId,
-    //       "senderName": $scope.userData.firstName+" "+$scope.userData.lastName,
-    //       "senderEmail": $scope.userData.email,
-    //       "title": title,
-    //       "reason": reason,
-    //       "invitingTo": emailList,
-    //       "formatedStartTime": formatedStartTime,
-    //       "formatedEndTime": formatedEndTime,
-    //       "startsAt": sd,
-    //       "endsAt": ed,
-    //       "primColor": "red",
-    //       "url": url,
-    //       "date": dateForEvent,
-    //     }
-    //     console.log("obj: " + JSON.stringify(obj));
-    //     careatorHttpFactory.post(api, obj).then(function (data) {
-    //       var checkStatus = careatorHttpFactory.dataValidation(data);
-    //       console.log("data--" + JSON.stringify(data.data));
-    //       if (checkStatus) {
-    //         if (data.data.failedToSend.length == 0) {
-    //           // alert("Successfully sent the event");
-    //           SweetAlert.swal({
-    //             title: "Invited Successfully",
-    //             type: "success",
-    //             text: "Successfully sent the event",
-    //           })
+    $timeout(function () {
+      getSocketUrlFromServer().then(function (url) {
+        console.log("Back to function call-->");
+        console.log("url: " + url);
+        var api = "https://norecruits.com/careator_eventSchedule/careator_sendEventSchedule";
+        console.log("api: " + api);
+        var obj = {
+          "senderId": $scope.userData.userId,
+          "senderName": $scope.userData.firstName+" "+$scope.userData.lastName,
+          "senderEmail": $scope.userData.email,
+          "title": title,
+          "reason": reason,
+          "invitingTo": emailList,
+          "formatedStartTime": formatedStartTime,
+          "formatedEndTime": formatedEndTime,
+          "startsAt": sd,
+          "endsAt": ed,
+          "primColor": "red",
+          "url": url,
+          "date": dateForEvent,
+        }
+        console.log("obj: " + JSON.stringify(obj));
+        careatorHttpFactory.post(api, obj).then(function (data) {
+          var checkStatus = careatorHttpFactory.dataValidation(data);
+          console.log("data--" + JSON.stringify(data.data));
+          if (checkStatus) {
+            if (data.data.failedToSend.length == 0) {
+              // alert("Successfully sent the event");
+              SweetAlert.swal({
+                title: "Invited Successfully",
+                type: "success",
+                text: "Successfully sent the event",
+              })
 
-    //         } else {
-    //           SweetAlert.swal({
-    //             title: "Failed",
-    //             type: "warning",
-    //             text: "Failed to send" + JSON.stringify(data.data.failedToSend.length)
-    //           })
-    //           // alert("Failed to send " + JSON.stringify(data.data.failedToSend.length));
-    //         }
+            } else {
+              SweetAlert.swal({
+                title: "Failed",
+                type: "warning",
+                text: "Failed to send" + JSON.stringify(data.data.failedToSend.length)
+              })
+              // alert("Failed to send " + JSON.stringify(data.data.failedToSend.length));
+            }
 
-    //         // vm.events.splice(0, 1);
-    //         var eventPostedData = data.data.data;
-    //         var objData = {
-    //           "senderId": $scope.userData.userId,
-    //           "senderName": $scope.userData.firstName+" "+$scope.userData.lastName,
-    //           "senderEmail": $scope.userData.email,
-    //           "title": title,
-    //           "reason": reason,
-    //           "invitingTo": emailList,
-    //           "formatedStartTime": formatedStartTime,
-    //           "formatedEndTime": formatedEndTime,
-    //           "startsAt": sd,
-    //           "endsAt": ed,
-    //           "primColor": "red",
-    //           "url": url,
-    //           "date": dateForEvent,
-    //         }
-    //         ownerEvents.push(objData);
-    //         vm.events.push(objData);
-    //       } else {
-    //         // alert("Event Send Failed");
-    //         SweetAlert.swal({
-    //           title: "Failed",
-    //           type: "warning",
-    //           text: "Failed to send"
-    //         })
+            // vm.events.splice(0, 1);
+            var eventPostedData = data.data.data;
+            var objData = {
+              "senderId": $scope.userData.userId,
+              "senderName": $scope.userData.firstName+" "+$scope.userData.lastName,
+              "senderEmail": $scope.userData.email,
+              "title": title,
+              "reason": reason,
+              "invitingTo": emailList,
+              "formatedStartTime": formatedStartTime,
+              "formatedEndTime": formatedEndTime,
+              "startsAt": sd,
+              "endsAt": ed,
+              "primColor": "red",
+              "url": url,
+              "date": dateForEvent,
+            }
+            ownerEvents.push(objData);
+            vm.events.push(objData);
+          } else {
+            // alert("Event Send Failed");
+            SweetAlert.swal({
+              title: "Failed",
+              type: "warning",
+              text: "Failed to send"
+            })
 
-    //       }
-    //     })
-    //   });
-    // }, 0);
+          }
+        })
+      });
+    }, 0);
     console.log("<--eventSend");
     // var url = document.getElementById('linkToShare').innerHTML;
   }

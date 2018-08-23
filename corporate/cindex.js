903d46cadfe1ad5f64f72721dc9de98368df5c03
LogeswariGuru <logeswari.careator@gmail.com>
2018-08-23 15:57:28 +0530

    group inactive alert

--- corporate/cindex.js
+++ corporate/cindex.js
@@ -567,7 +567,7 @@ io.sockets.on('connection', function (socket) {
                         "sessionRandomId": data.sessionRandomId
                     }
                     console.log("queryObj: "+JSON.stringify(queryObj));
-                    loginDetails.update(queryObj, { $set: { "login": false, "logoutDate": date, "logout": true } }, { upsert: true }, function (err, logoutData) {
+                    loginDetails.update(queryObj, { $set: { "login": false, "logoutDate": date, "logout": true } }, function (err, logoutData) {
                         if (err) {
                             console.log("errr: " + JSON.stringify(err));
                         }

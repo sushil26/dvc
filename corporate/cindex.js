5fff572a75030ff47d0f6417b4b9877322e96c1a
LogeswariGuru <logeswari.careator@gmail.com>
2018-08-28 15:22:31 +0530

    logout

--- corporate/cindex.js
+++ corporate/cindex.js
@@ -588,7 +588,7 @@ io.sockets.on('connection', function (socket) {
                 var queryObj = {
                     "sessionRandomId": data.sessionRandomId
                 }
-                loginDetails.update(queryObj, { $set: { "login": false, "logoutDate": date, "logout": true } }, function (err, logoutData) {
+                loginDetails.update(queryObj, { $set: { "login": false, "logoutDate": date, "logout": true } }, { upsert: true }, function (err, logoutData) {
                     if (err) {
                         console.log("errr: " + JSON.stringify(err));
                     }

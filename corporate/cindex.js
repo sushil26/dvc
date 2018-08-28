ce321a5b540fbea42524fa6ebba394941e802c21
LogeswariGuru <logeswari.careator@gmail.com>
2018-08-28 16:00:11 +0530

    logout

--- corporate/cindex.js
+++ corporate/cindex.js
@@ -623,6 +623,7 @@ io.sockets.on('connection', function (socket) {
             if (emailTrack.indexOf(data.email) >= 0) {
                 io.sockets.emit('disconnectSessionReply', { "deleteSessionId": stuff[4], "owner": emailTrack.indexOf(datadata.email) });
             }
+            io.sockets.emit('comm_logoutNotifyToUserById', { "userId": data.userId, "email": data.email, "sessionURL": data.undisconnectedSession[x], "sessionRandomId": data.sessionRandomId }) /* ### Note: Send quick message view notification to event sender(who's user id is matched with this userId) ### */
             var url = data.undisconnectedSession[x];
             var stuff = url.split("/");
             console.log("stuff: " + JSON.stringify(stuff));
@@ -654,7 +655,7 @@ io.sockets.on('connection', function (socket) {
             }
             else {
                 console.log("updateData: " + JSON.stringify(updateData));
-                io.sockets.emit('comm_logoutNotifyToUserById', { "userId": data.userId, "email": data.email,  "sessionRandomId": data.sessionRandomId }) /* ### Note: Send quick message view notification to event sender(who's user id is matched with this userId) ### */
+              
             }
         })
 

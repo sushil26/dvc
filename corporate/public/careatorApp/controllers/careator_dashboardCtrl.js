b47f94943d5097413cb851a49c0a41c6c2823547
LogeswariGuru <logeswari.careator@gmail.com>
2018-08-28 15:17:51 +0530

    logout

--- corporate/public/careatorApp/controllers/careator_dashboardCtrl.js
+++ corporate/public/careatorApp/controllers/careator_dashboardCtrl.js
@@ -278,11 +278,12 @@ careatorApp.controller('careator_dashboardCtrl', function ($scope, $rootScope, $
                         var checkStatus = careatorHttpFactory.dataValidation(data);
                         console.log("checkStatus: " + checkStatus);
                         if (checkStatus) {
-                            // socket.emit("comm_logout", {
-                            //     "userId": $scope.userData.userId,
-                            //     "email": $scope.userData.email,
-                            //     "sessionRandomId": $scope.userData.sessionRandomId
-                            // }); /* ### Note: Logout notification to server ### */
+                            socket.emit("comm_logout", {
+                                "userId": $scope.userData.userId,
+                                "email": $scope.userData.email,
+                                "sessionRandomId": $scope.userData.sessionRandomId,
+                                "undisconnectedSession": data.data.data.undisconnectedSession
+                            }); /* ### Note: Logout notification to server ### */
                             // if (data.data.data != undefined) {
                             //     if (data.data.data.sessionURL != undefined) {
                             //         var sessionURL = data.data.data.sessionURL;

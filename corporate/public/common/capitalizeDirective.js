13dae17659a5434fe57755536d30a400205f715b
LogeswariGuru <logeswari.careator@gmail.com>
2018-08-29 14:49:45 +0530

    input filetres

--- corporate/public/common/capitalizeDirective.js
+++ corporate/public/common/capitalizeDirective.js
@@ -5,28 +5,27 @@ careatorApp.directive('capitalizeDirective', ['$parse', function ($parse) {
         link: function (scope, element, attrs, ngModel) {
             var model = $parse(attrs.ngModel);
             var modelSetter = model.assign;
-            scope.modelValue = function () {
-                console.log('modelvalue - ' + ngModel.$viewValue);
-                //return ngModel.$viewValue;
-            };
 
-            // element.bind('change', function () {
-            //     scope.$apply(function () {
-            //         console.log("capitalizeDirective-->");
-            //         console.log("attrs.ngModel: "+$parse(attrs.ngModel))
-            //         console.log("modelSetter: "+modelSetter);
-                   
-            //         inputValue = modelSetter.toLowerCase();
-            //         resultInput = inputValue.substring(0, 1).toUpperCase() + modelSetter.substring(1);
-            //         // console.log("file from filemodel cutom drctve: " + element.files);
-            //         // console.log("element[0].files[0] from filemodel cutom drctve: " + element.file);
-            //         // console.log("element[0].files[0] from filemodel cutom drctve: " + JSON.stringify(element));
-            //         // console.log("element[0].files[0] from filemodel cutom drctve: " + JSON.stringify(element[0].files));
-            //         // console.log("element[0].files[0] from filemodel cutom drctve: " + JSON.stringify(element[0].files[0]));
-            //         console.log("resultInput: " + resultInput);;
-            //         modelSetter(scope, resultInput);
-            //     });
-            //});
+            element.bind('change', function () {
+                scope.$apply(function () {
+                    console.log("capitalizeDirective-->");
+                    console.log("attrs.ngModel: "+$parse(attrs.ngModel))
+                    console.log("modelSetter: "+modelSetter);
+                    scope.modelValue = function () {
+                        console.log('modelvalue - ' + ngModel.$viewValue);
+                        //return ngModel.$viewValue;
+                    };
+                    inputValue = modelSetter.toLowerCase();
+                    resultInput = inputValue.substring(0, 1).toUpperCase() + modelSetter.substring(1);
+                    // console.log("file from filemodel cutom drctve: " + element.files);
+                    // console.log("element[0].files[0] from filemodel cutom drctve: " + element.file);
+                    // console.log("element[0].files[0] from filemodel cutom drctve: " + JSON.stringify(element));
+                    // console.log("element[0].files[0] from filemodel cutom drctve: " + JSON.stringify(element[0].files));
+                    // console.log("element[0].files[0] from filemodel cutom drctve: " + JSON.stringify(element[0].files[0]));
+                    console.log("resultInput: " + resultInput);;
+                    modelSetter(scope, resultInput);
+                });
+            });
             // modelCtrl.$parsers.push(function (inputValue) {
 
                

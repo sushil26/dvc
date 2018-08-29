4094e535a8d2e0ae0f55694f43609a5a0833e80f
LogeswariGuru <logeswari.careator@gmail.com>
2018-08-29 14:29:02 +0530

    input filetres

--- corporate/public/common/capitalizeDirective.js
+++ corporate/public/common/capitalizeDirective.js
@@ -2,7 +2,7 @@ careatorApp.directive('capitalizeDirective', ['$parse', function ($parse) {
     return {
         restrict: 'ngModel',
         link: function (scope, element, attrs, modelCtrl) {
-            modelCtrl.$parsers.push(function (inputValue) {
+            modelCtrl.$parser.push(function (inputValue) {
                 console.log("capitalizeDirective-->");
                 if (inputValue != null) {
                     inputValue = inputValue.toLowerCase();

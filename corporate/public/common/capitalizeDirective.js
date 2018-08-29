7fb468ae68e494c6891394b16b4c85969aefe4ec
LogeswariGuru <logeswari.careator@gmail.com>
2018-08-29 14:33:44 +0530

    input filetres

--- corporate/public/common/capitalizeDirective.js
+++ corporate/public/common/capitalizeDirective.js
@@ -1,24 +1,25 @@
 careatorApp.directive('capitalizeDirective', ['$parse', function ($parse) {
     return {
-        restrict: 'ngModel',
+        restrict: 'A',
         link: function (scope, element, attrs, modelCtrl) {
-            modelCtrl.$parser.push(function (inputValue) {
-                console.log("capitalizeDirective-->");
-                if (inputValue != null) {
-                    inputValue = inputValue.toLowerCase();
-                    resultInput = inputValue.substring(0, 1).toUpperCase() + input.substring(1);
-                    console.log("resultInput: "+resultInput);;
-                    return resultInput
-                    
-                }
-                else{
-                    return inputValue
-                }
+            var model = $parse(attrs.ngModel);
+            var modelSetter = model.assign;
 
-
+            element.bind('change', function () {
+                scope.$apply(function () {
+                    console.log("capitalizeDirective-->");
+                    inputValue = modelSetter.toLowerCase();
+                    resultInput = inputValue.substring(0, 1).toUpperCase() + modelSetter.substring(1);
+                    // console.log("file from filemodel cutom drctve: " + element.files);
+                    // console.log("element[0].files[0] from filemodel cutom drctve: " + element.file);
+                    // console.log("element[0].files[0] from filemodel cutom drctve: " + JSON.stringify(element));
+                    // console.log("element[0].files[0] from filemodel cutom drctve: " + JSON.stringify(element[0].files));
+                    // console.log("element[0].files[0] from filemodel cutom drctve: " + JSON.stringify(element[0].files[0]));
+                    console.log("resultInput: " + resultInput);;
+                    modelSetter(scope, resultInput);
+                });
             });
         }
-
 
     }
 }])

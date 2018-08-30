3dbc91f4d66ac023c58f2ef112d3dfeab8d526d2
LogeswariGuru <logeswari.careator@gmail.com>
2018-08-30 11:52:58 +0530

    popup lock prevent

--- corporate/public/careatorApp/controllers/careator_dashboardCtrl.js
+++ corporate/public/careatorApp/controllers/careator_dashboardCtrl.js
@@ -482,7 +482,7 @@ careatorApp.controller('careator_dashboardCtrl', function ($scope, $rootScope, $
     });
 
     var w;
-    $scope.navigateintoBoth_CVoption = function () {
+    $scope.navigateintoBoth_CVoption = function (e) {
         console.log("navigateintoBoth_CVoption-->");
         if (!w || w.closed) {
             localStorage.setItem("careatorEmail", userData.email);
@@ -520,6 +520,7 @@ careatorApp.controller('careator_dashboardCtrl', function ($scope, $rootScope, $
                             localStorage.setItem("sessionUrlId", peerNew_id);
                             localStorage.setItem("sessionUrlId", peerNew_id);
                             // localStorage.setItem("sessionUrlId", url);
+                            e.preventDefault(); 
                             console.log("url: " + url);
                             w = window.open(url, '_blank');
                             $window.close();

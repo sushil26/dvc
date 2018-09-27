0d968cc46f052fad8d9c6bf400a94e22b042b031
LogeswariGuru <logeswari.careator@gmail.com>
2018-09-27 12:04:44 +0530

    user create edit

--- corporate/public/careatorApp/controllers/createUsersCtrl.js
+++ corporate/public/careatorApp/controllers/createUsersCtrl.js
@@ -4,8 +4,8 @@ careatorApp.controller('createUsersCtrl', function ($scope, $rootScope, $state, 
     $scope.userData = careatorSessionAuth.getAccess("userData");
     console.log(" $scope.userData : " + JSON.stringify($scope.userData));
     var orgId = $scope.userData.orgId;
-    console.log("$scope.orgDatas: "+JSON.stringify($scope.orgDatas.domain));
-    console.log("$rootScope.orgDatas: "+JSON.stringify($rootScope.orgDatas.domain));
+    //console.log("$scope.orgDatas: "+JSON.stringify($scope.orgDatas.domain));
+    console.log("$rootScope.orgDatas: "+JSON.stringify($rootScope.orgDatas));
 //console.log("$rootScope.orgDatas: "+JSON.stringify($rootScope.orgDatas));
     //$scope.domainName = $rootScope.orgDatas.domain;
 

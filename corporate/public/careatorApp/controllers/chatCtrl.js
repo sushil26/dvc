68091c80aba26e5688db7209d9cb35a2cb4447ea
LogeswariGuru <logeswari.careator@gmail.com>
2018-09-28 11:14:36 +0530

    chatfile

--- corporate/public/careatorApp/controllers/chatCtrl.js
+++ corporate/public/careatorApp/controllers/chatCtrl.js
@@ -521,7 +521,7 @@ careatorApp.controller("chatCtrl", function ($scope, $rootScope, careatorHttpFac
     $("#comment").val("");
    // $scope.typedMessage = typedMessage;
     console.log("sendText-->");
-    console.log("chatFile: "+JSON.stringify(chatFile));
+    console.log("chatFile: "+JSON.stringify($scope.chatFile));
     //  console.log("chatFile: "+$scope.chatFile);
     console.log("typedMessage: " + typedMessage);
     var api;

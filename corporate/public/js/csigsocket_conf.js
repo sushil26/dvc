a37a9d50fe23b70d081bf0295f473282eba85b75
shaiksajans <shaiksajans@gmail.com>
2018-09-28 12:27:16 +0530

    aaa

--- corporate/public/js/csigsocket_conf.js
+++ corporate/public/js/csigsocket_conf.js
@@ -1352,7 +1352,7 @@ console.log("id2**: " + id2);
         // );
 
         $("#portfolio-wrapper").append(
-          '<div id="videoElem111" class="portfolio-items col-xs-4 col-sm-4 col-md-4 col-lg-3"><div id="videosAttach"></div><div class="details"><button id="fullscreenbtn" class="btn fa fa-expand" style="float:left; margin-top: 10px; margin-left: 10px; margin-right: 10px;"></button><h4>' +
+          '<div id="videoElem111" class="portfolio-items col-xs-4 col-sm-4 col-md-4 col-lg-3"><div id="videosAttach"></div><div class="details"><h4>' +
           userName +
           "</h4> </div></div>"
         );

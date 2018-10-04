bd8ef70909dfc48b72ebbf746a8bead5611a2f6c
Rails udemy course <logeswari.g@careator.com>
2018-10-04 16:22:47 +0530

    data insert

--- corporate/cindex.js
+++ corporate/cindex.js
@@ -234,7 +234,7 @@ io.sockets.on('connection', function (socket) {
             console.log("queryObj: "+JSON.stringify(queryObj));
             var sessionurl = "https://norecruits.com/vc4all_conf/"+data.deleteSessionId+"/"+data.queryTime;
             console.log("sessionurl: "+sessionurl);
-            careatorMaster.update({"_id": data.userId, "instantConf.sessionURL": sessionurl}, { $set: { "instantConf.$.isDisconnected": "yes" } }, function (err, data) {
+            careatorMaster.update({"_id": ObjectId(data.userId), "instantConf.sessionURL": sessionurl}, { $set: { "instantConf.$.isDisconnected": "yes" } }, function (err, data) {
                 if (err) {
                     console.log("errr: " + JSON.stringify(err));
                 }

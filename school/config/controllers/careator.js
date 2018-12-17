95aa6d6a971fcb5df272c4a4782bdea9b71d07af
Rails udemy course <logeswari.g@talenkart.com>
2018-12-17 18:44:33 +0530

    with authentication

--- school/config/controllers/careator.js
+++ school/config/controllers/careator.js
@@ -682,7 +682,7 @@ module.exports.getAdminObjectId = function (req, res) {
             };
             res.status(400).send(responseData);
         } else {
-            console.log("admin: " + JSON.stringify(admin));
+            // console.log("admin: " + JSON.stringify(admin));
             responseData = {
                 status: true,
                 message: property.S0008,

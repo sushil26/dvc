5d7cadd33ae1f1addfc09a414a914ddf2fb3704b
Rails udemy course <logeswari.g@talenkart.com>
2018-12-26 14:53:55 +0530

    sc

--- corporate/config/verify_token.js
+++ corporate/config/verify_token.js
@@ -1,15 +1,19 @@
 var jwt = require('jsonwebtoken');
 const config = require('./config.json');
 
-module.exports = function(req,res,next) {
+module.exports = function (req, res, next) {
     console.log("------------------------------------->verify_token----->");
-  var token = req.body.token ;
+    var token = req.body.token;
     if (token) {
         console.log("token present");
-    // verifies secret and checks exp
-        jwt.verify(token, config.jwt_secret, function(err, decoded) {
+        // verifies secret and checks exp
+        jwt.verify(token, config.jwt_secret, function (err, decoded) {
             if (err) { //failed verification.
-                return res.json({"error": true});
+                return res.json({
+                    "status": false,
+                    "errorCode": 403,
+                    "message": "You do not allowed for this resource"
+                });
             }
             req.decoded = decoded;
             next(); //no error, proceed
@@ -18,8 +22,8 @@ module.exports = function(req,res,next) {
         console.log("token not present");
         // forbidden without token
         return res.status(403).send({
-            "error": true,
-            "status": 403,
+            "status": false,
+            "errorCode": 403,
             "message": "You do not allowed for this resource"
         });
     }
